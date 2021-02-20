3d143c869a7cee0a420063d8a7286432163fa0c5
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-02-20 14:25:10 +0800

    feat: 新增组织机构

---
+++ backend/src/main/java/io/dataease/base/domain/SysDept.java
@@ -0,0 +1,32 @@
+package io.dataease.base.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class SysDept implements Serializable {
+    private Long deptId;
+
+    private Long pid;
+
+    private Integer subCount;
+
+    private String name;
+
+    private Integer level;
+
+    private Integer deptSort;
+
+    private Boolean enabled;
+
+    private String createBy;
+
+    private String updateBy;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private static final long serialVersionUID = 1L;
+}
