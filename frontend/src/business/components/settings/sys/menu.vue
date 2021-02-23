6f3f98b1e68df0289bf57393a2e90a282d7e6f66
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-02-23 15:06:38 +0800

    feat: 新增图表选择组件

--- frontend/src/business/components/settings/sys/menu.vue
+++ frontend/src/business/components/settings/sys/menu.vue
@@ -65,7 +65,7 @@
       <el-form ref="menuForm" inline :model="form" :rules="rule" size="small" label-width="80px">
 
         <el-form-item label="菜单类型" prop="type">
-          <el-radio-group v-model="form.type" size="mini" style="width: 178px">
+          <el-radio-group v-model="form.type" size="mini" style="width: 179px">
             <el-radio-button label="0">目录</el-radio-button>
             <el-radio-button label="1">菜单</el-radio-button>
             <el-radio-button label="2">按钮</el-radio-button>
@@ -74,7 +74,7 @@
         <el-form-item v-show="form.type!== '2'" label="菜单图标" prop="icon">
           <el-popover
             placement="bottom-start"
-            width="450"
+            width="425"
             trigger="click"
             @show="$refs['iconSelect'].reset()"
           >
@@ -104,25 +104,25 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item v-if="form.type !== '2'" label="菜单标题" prop="title">
-          <el-input v-model="form.title" :style=" form.type === '0' ? 'width: 450px' : 'width: 178px'" placeholder="菜单标题" />
+          <el-input v-model="form.title" :style=" form.type === '0' ? 'width: 450px' : 'width: 179px'" placeholder="菜单标题" />
         </el-form-item>
         <el-form-item v-if="form.type === '2'" label="按钮名称" prop="title">
-          <el-input v-model="form.title" placeholder="按钮名称" style="width: 178px;" />
+          <el-input v-model="form.title" placeholder="按钮名称" style="width: 179px;" />
         </el-form-item>
         <el-form-item v-show="form.type !== '0'" label="权限标识" prop="permission">
-          <el-input v-model="form.permission" :disabled="form.iframe" placeholder="权限标识" style="width: 178px;" />
+          <el-input v-model="form.permission" :disabled="form.iframe" placeholder="权限标识" style="width: 179px;" />
         </el-form-item>
         <el-form-item v-if="form.type !== '2'" label="路由地址" prop="path">
-          <el-input v-model="form.path" placeholder="路由地址" style="width: 178px;" />
+          <el-input v-model="form.path" placeholder="路由地址" style="width: 179px;" />
         </el-form-item>
         <el-form-item label="菜单排序" prop="menuSort">
-          <el-input-number v-model.number="form.menuSort" :min="0" :max="999" controls-position="right" style="width: 178px;" />
+          <el-input-number v-model.number="form.menuSort" :min="0" :max="999" controls-position="right" style="width: 179px;" />
         </el-form-item>
         <el-form-item v-show="!form.iframe && form.type === '1'" label="组件名称" prop="componentName">
-          <el-input v-model="form.componentName" style="width: 178px;" placeholder="匹配组件内Name字段" />
+          <el-input v-model="form.componentName" style="width: 179px;" placeholder="匹配组件内Name字段" />
         </el-form-item>
         <el-form-item v-show="!form.iframe && form.type === '1'" label="组件路径" prop="component">
-          <el-input v-model="form.component" style="width: 178px;" placeholder="组件路径" />
+          <el-input v-model="form.component" style="width: 179px;" placeholder="组件路径" />
         </el-form-item>
         <el-form-item label="上级类目" prop="pid">
           <treeselect
