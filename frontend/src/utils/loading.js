7254b65bd9c279cc40c97e8be5447a4c383d99a0
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-03-04 15:05:05 +0800

    feat: 添加loading说明

--- frontend/src/utils/loading.js
+++ frontend/src/utils/loading.js
@@ -18,3 +18,12 @@ export const tryHideLoading = identification => {
   }
 }
 
+/**
+ * 说明
+ * 要在view中加loading只需要两个步骤
+ * 1.在vue页面文件中加 v-loading="$store.getters.loadingMap[$store.getters.currentPath]"
+ *     例如：views/system/user/index.vue
+ * 2.在需要loading的api中添加config {loading: true}
+ *     例如：api/system/user.js中userLists方法 需要配置loading: true
+ * 针对整个view页面，需要局部loading请在对应页面中自己添加
+ */
