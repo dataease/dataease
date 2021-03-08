8d553314faefa6659c636f3dfd8a66748cbd4d34
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-03-08 13:15:52 +0800

    feat:注释demo

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -64,29 +64,29 @@ export const constantRoutes = [
       component: () => import('@/views/dashboard/index'),
       meta: { title: '首页', icon: 'dashboard' }
     }]
-  },
-
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
   }
+
+  //   {
+  //     path: '/example',
+  //     component: Layout,
+  //     redirect: '/example/table',
+  //     name: 'Example',
+  //     meta: { title: 'Example', icon: 'example' },
+  //     children: [
+  //       {
+  //         path: 'table',
+  //         name: 'Table',
+  //         component: () => import('@/views/table/index'),
+  //         meta: { title: 'Table', icon: 'table' }
+  //       },
+  //       {
+  //         path: 'tree',
+  //         name: 'Tree',
+  //         component: () => import('@/views/tree/index'),
+  //         meta: { title: 'Tree', icon: 'tree' }
+  //       }
+  //     ]
+  //   }
 
   //   {
   //     path: '/form',
