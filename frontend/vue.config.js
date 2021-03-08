e64013b23fb4c129d39bd01c96681f776aded2a1
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-03-08 13:45:22 +0800

    feat:删除不必要依赖

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -8,30 +8,10 @@ function resolve(dir) {
 
 const name = defaultSettings.title || 'vue Admin Template' // page title
 
-// If your port is set to 80,
-// use administrator privileges to execute the command line.
-// For example, Mac: sudo npm run
-// You can change the port by the following methods:
-// port = 9528 npm run dev OR npm run dev --port = 9528
 const port = process.env.port || process.env.npm_config_port || 9528 // dev port
-
-// All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
-  publicPath: '/',
-  outputDir: 'dist',
-  assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'development',
-  productionSourceMap: false,
-  css: {
-    sourceMap: true // 开启 CSS source maps
-  },
+  productionSourceMap: true,
+  // 使用mock-server
   devServer: {
     port: port,
     open: true,
@@ -42,91 +22,131 @@ module.exports = {
     before: require('./mock/mock-server.js')
   },
   configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
     name: name,
+    devtool: 'source-map',
     resolve: {
       alias: {
         '@': resolve('src')
       }
     }
   },
-  chainWebpack(config) {
-    config.plugins.delete('preload') // TODO: need test
-    config.plugins.delete('prefetch') // TODO: need test
-
-    // set svg-sprite-loader
+  chainWebpack: config => {
+    config.module.rules.delete('svg') // 删除默认配置中处理svg,
+    // const svgRule = config.module.rule('svg')
+    // svgRule.uses.clear()
     config.module
-      .rule('svg')
-      .exclude.add(resolve('src/icons'))
-      .end()
-    config.module
-      .rule('icons')
+      .rule('svg-sprite-loader')
       .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
+      .include
+      .add(resolve('src/icons')) // 处理svg目录
       .end()
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
       .options({
         symbolId: 'icon-[name]'
       })
-      .end()
-
-    // set preserveWhitespace
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .loader('vue-loader')
-      .tap(options => {
-        options.compilerOptions.preserveWhitespace = true
-        return options
-      })
-      .end()
-
-    config
-    // https://webpack.js.org/configuration/devtool/#development
-      .when(process.env.NODE_ENV === 'development',
-        config => config.devtool('source-map')
-        // config => config.devtool('cheap-source-map')
-      )
-
-    config
-      .when(process.env.NODE_ENV !== 'development',
-        config => {
-          config
-            .plugin('ScriptExtHtmlWebpackPlugin')
-            .after('html')
-            .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
-              inline: /runtime\..*\.js$/
-            }])
-            .end()
-          config
-            .optimization.splitChunks({
-              chunks: 'all',
-              cacheGroups: {
-                libs: {
-                  name: 'chunk-libs',
-                  test: /[\\/]node_modules[\\/]/,
-                  priority: 10,
-                  chunks: 'initial' // only package third parties that are initially dependent
-                },
-                elementUI: {
-                  name: 'chunk-elementUI', // split elementUI into a single package
-                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-                },
-                commons: {
-                  name: 'chunk-commons',
-                  test: resolve('src/components'), // can customize your rules
-                  minChunks: 3, //  minimum common number
-                  priority: 5,
-                  reuseExistingChunk: true
-                }
-              }
-            })
-          config.optimization.runtimeChunk('single')
-        }
-      )
   }
+
 }
+// module.exports = {
+
+//   publicPath: '/',
+//   outputDir: 'dist',
+//   assetsDir: 'static',
+//   lintOnSave: process.env.NODE_ENV === 'development',
+//   productionSourceMap: false,
+//   css: {
+//     sourceMap: true // 开启 CSS source maps
+//   },
+//   devServer: {
+//     port: port,
+//     open: true,
+//     overlay: {
+//       warnings: false,
+//       errors: true
+//     },
+//     before: require('./mock/mock-server.js')
+//   },
+//   configureWebpack: {
+
+//     name: name,
+//     resolve: {
+//       alias: {
+//         '@': resolve('src')
+//       }
+//     }
+//   },
+//   chainWebpack(config) {
+//     config.plugins.delete('preload') // TODO: need test
+//     config.plugins.delete('prefetch') // TODO: need test
+
+//     config.module
+//       .rule('svg')
+//       .exclude.add(resolve('src/icons'))
+//       .end()
+//     config.module
+//       .rule('icons')
+//       .test(/\.svg$/)
+//       .include.add(resolve('src/icons'))
+//       .end()
+//       .use('svg-sprite-loader')
+//       .loader('svg-sprite-loader')
+//       .options({
+//         symbolId: 'icon-[name]'
+//       })
+//       .end()
+
+//     config.module
+//       .rule('vue')
+//       .use('vue-loader')
+//       .loader('vue-loader')
+//       .tap(options => {
+//         options.compilerOptions.preserveWhitespace = true
+//         return options
+//       })
+//       .end()
+
+//     config
+//       .when(process.env.NODE_ENV === 'development',
+//         config => config.devtool('source-map')
+//       )
+
+//     config
+//       .when(process.env.NODE_ENV !== 'development',
+//         config => {
+//           config
+//             .plugin('ScriptExtHtmlWebpackPlugin')
+//             .after('html')
+//             .use('script-ext-html-webpack-plugin', [{
+//               inline: /runtime\..*\.js$/
+//             }])
+//             .end()
+//           config
+//             .optimization.splitChunks({
+//               chunks: 'all',
+//               cacheGroups: {
+//                 libs: {
+//                   name: 'chunk-libs',
+//                   test: /[\\/]node_modules[\\/]/,
+//                   priority: 10,
+//                   chunks: 'initial' // only package third parties that are initially dependent
+//                 },
+//                 elementUI: {
+//                   name: 'chunk-elementUI', // split elementUI into a single package
+//                   priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+//                   test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+//                 },
+//                 commons: {
+//                   name: 'chunk-commons',
+//                   test: resolve('src/components'), // can customize your rules
+//                   minChunks: 3, //  minimum common number
+//                   priority: 5,
+//                   reuseExistingChunk: true
+//                 }
+//               }
+//             })
+//           config.optimization.runtimeChunk('single')
+//         }
+//       )
+//   }
+// }
