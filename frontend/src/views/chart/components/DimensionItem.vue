9ae93df8fbb9179d2c8c9928bfca9e116e1a32a8
junjie <junjie.xia@fit2cloud.com>
2021-03-11 15:57:54 +0800

    feat(视图): css调整

--- frontend/src/views/chart/components/DimensionItem.vue
+++ frontend/src/views/chart/components/DimensionItem.vue
@@ -2,22 +2,20 @@
   <span>
     <el-dropdown trigger="click" size="small">
       <span class="el-dropdown-link">
-        <span
-          class="item-axis"
-        >
+        <el-tag size="small" class="item-axis">
           {{ item.name }}<i class="el-icon-arrow-down el-icon--right" />
-          <span />
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item icon="el-icon-edit-outline">
-              item1
-            </el-dropdown-item>
-            <el-dropdown-item icon="el-icon-delete">
-              item2
-            </el-dropdown-item>
-          </el-dropdown-menu>
-        </span>
+        </el-tag>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item icon="el-icon-edit-outline">
+            item1
+          </el-dropdown-item>
+          <el-dropdown-item icon="el-icon-delete">
+            item2
+          </el-dropdown-item>
+        </el-dropdown-menu>
       </span>
-    </el-dropdown>
+    </el-dropdown></span>
+  </el-dropdown>
   </span>
 </template>
 
@@ -43,13 +41,10 @@ export default {
   .item-axis {
     padding: 1px 6px;
     margin: 0 3px 2px 3px;
-    border: solid 1px #eee;
-    background-color: #f1f1f1;
     text-align: left;
     height: 24px;
     line-height: 22px;
     display: inline-block;
-    color: #1890ff;
     border-radius: 4px;
     box-sizing: border-box;
     white-space: nowrap;
