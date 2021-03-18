760b05e2773e21320f1dc91421cb76e8b6653c5a
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-03-18 16:56:24 +0800

    feat: 分享页面api

---
+++ frontend/src/api/panel/share.js
@@ -0,0 +1,20 @@
+import request from '@/utils/request'
+
+export function saveShare(data) {
+  return request({
+    url: '/api/share/',
+    method: 'post',
+    loading: true,
+    data
+  })
+}
+
+export function loadShares(data) {
+  return request({
+    url: '/api/share/queryWithResourceId',
+    method: 'post',
+    loading: true,
+    data
+  })
+}
+
