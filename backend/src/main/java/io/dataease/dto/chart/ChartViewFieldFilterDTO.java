e38e60c21c4bdf033c43668a2d29ad3428c7353b
junjie <junjie.xia@fit2cloud.com>
2021-03-29 15:37:01 +0800

    feat(数据集): Spark序列化

--- backend/src/main/java/io/dataease/dto/chart/ChartViewFieldFilterDTO.java
+++ backend/src/main/java/io/dataease/dto/chart/ChartViewFieldFilterDTO.java
@@ -3,13 +3,15 @@ package io.dataease.dto.chart;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 /**
  * @Author gin
  * @Date 2021/3/25 10:31 上午
  */
 @Getter
 @Setter
-public class ChartViewFieldFilterDTO {
+public class ChartViewFieldFilterDTO implements Serializable {
     private String term;
     private String value;
 }
