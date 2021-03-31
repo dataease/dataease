63435ab2b691f030d68b0266362598901adf3b64
junjie <junjie.xia@fit2cloud.com>
2021-03-31 12:22:17 +0800

    feat(backend):spark 修改；分组场景修改

--- backend/src/main/java/io/dataease/service/spark/SparkCalc.java
+++ backend/src/main/java/io/dataease/service/spark/SparkCalc.java
@@ -16,7 +16,6 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
