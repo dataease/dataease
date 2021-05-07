6f25e1a6ef205ef79eb58ae8ba252024fa903fed
wangjiahao <1522128093@qq.com>
2021-05-07 10:23:58 +0800

    feat:flay_way

--- backend/src/main/resources/db/migration/V10__chart.sql
+++ backend/src/main/resources/db/migration/V10__chart.sql
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `chart_view`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
-ALTER TABLE `data_ease`.`chart_view`
+ALTER TABLE `chart_view`
 ADD COLUMN `snapshot` longtext NULL COMMENT '缩略图 ' AFTER `update_time`,
 ADD COLUMN `style_priority` varchar(255) NULL COMMENT '样式优先级 panel 仪表盘 view 视图' AFTER `snapshot`;
 -- chart end
