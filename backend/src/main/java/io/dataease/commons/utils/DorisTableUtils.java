ad71308564327587cbe18855671fd0e463223bd9
junjie <junjie.xia@fit2cloud.com>
2021-05-10 17:57:31 +0800

    feat(数据集):自助数据集

--- backend/src/main/java/io/dataease/commons/utils/DorisTableUtils.java
+++ backend/src/main/java/io/dataease/commons/utils/DorisTableUtils.java
@@ -2,16 +2,19 @@ package io.dataease.commons.utils;
 
 public class DorisTableUtils {
 
-    public static String dorisName(String datasetId){
+    public static String dorisName(String datasetId) {
         return "ds_" + datasetId.replace("-", "_");
     }
 
-    public static String dorisTmpName(String dorisName){
+    public static String dorisTmpName(String dorisName) {
         return "tmp_" + dorisName;
     }
 
-    public static String dorisDeleteName(String dorisName){
+    public static String dorisDeleteName(String dorisName) {
         return "delete_" + dorisName;
     }
 
+    public static String dorisFieldName(String dorisName) {
+        return "f_" + Md5Utils.md5(dorisName);
+    }
 }
