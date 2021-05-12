dae257bedf2efd9cace3f4390a1ac793c6296667
junjie <junjie.xia@fit2cloud.com>
2021-05-12 13:17:06 +0800

    feat(backend):fix

--- backend/src/main/java/io/dataease/service/dataset/DataSetTableService.java
+++ backend/src/main/java/io/dataease/service/dataset/DataSetTableService.java
@@ -229,7 +229,6 @@ public class DataSetTableService {
 
             String table = dataTableInfoDTO.getTable();
             datasourceRequest.setQuery(createQuerySQL(ds.getType(), table, fieldArray) + " LIMIT " + (page - 1) * pageSize + "," + realSize);
-            LogUtil.info(datasourceRequest.getQuery());
             try {
                 data.addAll(datasourceProvider.getData(datasourceRequest));
             } catch (Exception e) {
@@ -249,7 +248,6 @@ public class DataSetTableService {
 
             String sql = dataTableInfoDTO.getSql();
             datasourceRequest.setQuery(createQuerySQL(ds.getType(), " (" + sql + ") AS tmp ", fieldArray) + " LIMIT " + (page - 1) * pageSize + "," + realSize);
-            LogUtil.info(datasourceRequest.getQuery());
             try {
                 data.addAll(datasourceProvider.getData(datasourceRequest));
             } catch (Exception e) {
@@ -268,7 +266,6 @@ public class DataSetTableService {
             datasourceRequest.setDatasource(ds);
             String table = DorisTableUtils.dorisName(dataSetTableRequest.getId());
             datasourceRequest.setQuery(createQuerySQL(ds.getType(), table, fieldArray) + " LIMIT " + (page - 1) * pageSize + "," + realSize);
-            LogUtil.info(datasourceRequest.getQuery());
             try {
                 data.addAll(jdbcProvider.getData(datasourceRequest));
             } catch (Exception e) {
@@ -288,7 +285,6 @@ public class DataSetTableService {
             datasourceRequest.setDatasource(ds);
             String table = DorisTableUtils.dorisName(dataSetTableRequest.getId());
             datasourceRequest.setQuery(createQuerySQL(ds.getType(), table, fieldArray) + " LIMIT " + (page - 1) * pageSize + "," + realSize);
-            LogUtil.info(datasourceRequest.getQuery());
             try {
                 data.addAll(jdbcProvider.getData(datasourceRequest));
             } catch (Exception e) {
