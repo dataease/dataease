4f8f9080cff6755145e1f3282de4351ece0f1d84
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-05-20 22:14:07 +0800

    feat: 绑定日期过滤组件

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -2,6 +2,26 @@
  * Created by PanJiaChen on 16/11/18.
  */
 
+export function dateFormat(date, fmt) {
+  let ret
+  const opt = {
+    'y+': date.getFullYear().toString(), // 年
+    'M+': (date.getMonth() + 1).toString(), // 月
+    'd+': date.getDate().toString(), // 日
+    'H+': date.getHours().toString(), // 时
+    'm+': date.getMinutes().toString(), // 分
+    's+': date.getSeconds().toString() // 秒
+    // 有其他格式化字符需求可以继续添加，必须转化成字符串
+  }
+  for (const k in opt) {
+    ret = new RegExp('(' + k + ')').exec(fmt)
+    if (ret) {
+      fmt = fmt.replace(ret[1], (ret[1].length === 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, '0')))
+    }
+  }
+  return fmt
+}
+
 /**
  * Parse the time to string
  * @param {(Object|string|number)} time
