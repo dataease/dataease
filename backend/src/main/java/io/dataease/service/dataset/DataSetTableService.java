dadf7bd1f6f7062faab366692b86f6db8303fe7c
junjie <junjie.xia@fit2cloud.com>
2021-05-26 14:03:44 +0800

    feat(数据集):关联视图：两个数据集之间，不能有同一关联字段出现多次

--- backend/src/main/java/io/dataease/service/dataset/DataSetTableService.java
+++ backend/src/main/java/io/dataease/service/dataset/DataSetTableService.java
@@ -867,14 +867,15 @@ public class DataSetTableService {
     }
 
     public Boolean checkDorisTableIsExists(String id) throws Exception {
-        Datasource dorisDatasource = (Datasource) CommonBeanFactory.getBean("DorisDatasource");
-        JdbcProvider jdbcProvider = CommonBeanFactory.getBean(JdbcProvider.class);
-        DatasourceRequest datasourceRequest = new DatasourceRequest();
-        datasourceRequest.setDatasource(dorisDatasource);
-        QueryProvider qp = ProviderFactory.getQueryProvider(dorisDatasource.getType());
-        datasourceRequest.setQuery(qp.searchTable(DorisTableUtils.dorisName(id)));
-        List<String[]> data = jdbcProvider.getData(datasourceRequest);
-        return CollectionUtils.isNotEmpty(data);
+//        Datasource dorisDatasource = (Datasource) CommonBeanFactory.getBean("DorisDatasource");
+//        JdbcProvider jdbcProvider = CommonBeanFactory.getBean(JdbcProvider.class);
+//        DatasourceRequest datasourceRequest = new DatasourceRequest();
+//        datasourceRequest.setDatasource(dorisDatasource);
+//        QueryProvider qp = ProviderFactory.getQueryProvider(dorisDatasource.getType());
+//        datasourceRequest.setQuery(qp.searchTable(DorisTableUtils.dorisName(id)));
+//        List<String[]> data = jdbcProvider.getData(datasourceRequest);
+//        return CollectionUtils.isNotEmpty(data);
+        return true;
     }
 
     public void updateDatasetTableStatus(){
