5f267e77233da2b736f8b255bdb4dadebb222d37
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-05-30 20:25:58 +0800

    feat: 前端远程异步动态组件，渲染插件模块用

---
+++ frontend/src/api/system/dynamic.js
@@ -0,0 +1,22 @@
+import request from '@/utils/request'
+
+export function get(url) {
+  return request({
+    url: url,
+    method: 'get'
+  })
+}
+
+export function execute(options) {
+  if (!options || !options.url) {
+    return null
+  }
+  options.type = options.type || 'post'
+
+  return request({
+    url: options.url,
+    method: options.type,
+    loading: true,
+    data: options.data
+  })
+}
