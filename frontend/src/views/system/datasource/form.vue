a60bc5c0562eb7fd110a5c15428de6adfa77312b
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-06-22 11:54:53 +0800

    feat: 【ID1001685】【数据源】预览数据源信息的时候，点击预览页下方的编辑按钮进行编辑

--- frontend/src/views/system/datasource/form.vue
+++ frontend/src/views/system/datasource/form.vue
@@ -2,49 +2,64 @@
   <layout-content :header="formType=='add' ? $t('datasource.create') : $t('datasource.modify')">
     <template v-slot:header>
       <el-icon name="back" class="back-button" @click.native="backToList" />
-      {{ params && params.id && params.showModel && params.showModel === 'show' ? $t('datasource.show_info') : formType=='add' ? $t('datasource.create') : $t('datasource.modify') }}
+      {{ params && params.id && params.showModel && params.showModel === 'show' && !canEdit ? $t('datasource.show_info') : formType=='add' ? $t('datasource.create') : $t('datasource.modify') }}
     </template>
-    <el-form ref="dsForm" :model="form" :rules="rule" size="small" :disabled="params && params.id && params.showModel && params.showModel === 'show'" label-width="auto" label-position="right">
-      <el-form-item :label="$t('commons.name')" prop="name">
-        <el-input v-model="form.name" autocomplete="off" />
-      </el-form-item>
-      <el-form-item :label="$t('commons.description')" prop="desc">
+    <div>
 
-        <el-input v-model="form.desc" autocomplete="off" type="textarea" />
-      </el-form-item>
-      <el-form-item :label="$t('datasource.type')" prop="type">
-        <el-select v-model="form.type" :placeholder="$t('datasource.please_choose_type')" class="select-width" :disabled="formType=='modify'" @change="changeType()">
-          <el-option
-            v-for="item in allTypes"
-            :key="item.name"
-            :label="item.label"
-            :value="item.name"
-          />
-        </el-select>
-      </el-form-item>
+      <el-form ref="dsForm" :model="form" :rules="rule" size="small" :disabled="params && params.id && params.showModel && params.showModel === 'show' && !canEdit " label-width="auto" label-position="right">
+        <el-form-item :label="$t('commons.name')" prop="name">
+          <el-input v-model="form.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item :label="$t('commons.description')" prop="desc">
 
-      <el-form-item v-if="form.configuration.dataSourceType=='jdbc'" :label="$t('datasource.host')" prop="configuration.host">
-        <el-input v-model="form.configuration.host" autocomplete="off" :disabled="formType=='modify'" />
-      </el-form-item>
-      <el-form-item v-if="form.configuration.dataSourceType=='jdbc'" :label="$t('datasource.data_base')" prop="configuration.dataBase">
-        <el-input v-model="form.configuration.dataBase" autocomplete="off" :disabled="formType=='modify'" />
-      </el-form-item>
-      <el-form-item v-if="form.configuration.dataSourceType=='jdbc'" :label="$t('datasource.user_name')" prop="configuration.username">
-        <el-input v-model="form.configuration.username" autocomplete="off" :disabled="formType=='modify'" />
-      </el-form-item>
-      <el-form-item v-if="form.configuration.dataSourceType=='jdbc'" :label="$t('datasource.password')" prop="configuration.password">
-        <el-input v-model="form.configuration.password" autocomplete="off" show-password />
-      </el-form-item>
-      <el-form-item v-if="form.configuration.dataSourceType=='jdbc'" :label="$t('datasource.port')" prop="configuration.port">
-        <el-input v-model="form.configuration.port" autocomplete="off" />
-      </el-form-item>
+          <el-input v-model="form.desc" autocomplete="off" type="textarea" />
+        </el-form-item>
+        <el-form-item :label="$t('datasource.type')" prop="type">
+          <el-select v-model="form.type" :placeholder="$t('datasource.please_choose_type')" class="select-width" :disabled="formType=='modify'" @change="changeType()">
+            <el-option
+              v-for="item in allTypes"
+              :key="item.name"
+              :label="item.label"
+              :value="item.name"
+            />
+          </el-select>
+        </el-form-item>
 
-      <el-form-item v-if="!(params && params.id && params.showModel && params.showModel === 'show')">
+        <el-form-item v-if="form.configuration.dataSourceType=='jdbc'" :label="$t('datasource.host')" prop="configuration.host">
+          <el-input v-model="form.configuration.host" autocomplete="off" :disabled="formType=='modify'" />
+        </el-form-item>
+        <el-form-item v-if="form.configuration.dataSourceType=='jdbc'" :label="$t('datasource.data_base')" prop="configuration.dataBase">
+          <el-input v-model="form.configuration.dataBase" autocomplete="off" :disabled="formType=='modify'" />
+        </el-form-item>
+        <el-form-item v-if="form.configuration.dataSourceType=='jdbc'" :label="$t('datasource.user_name')" prop="configuration.username">
+          <el-input v-model="form.configuration.username" autocomplete="off" :disabled="formType=='modify'" />
+        </el-form-item>
+        <el-form-item v-if="form.configuration.dataSourceType=='jdbc'" :label="$t('datasource.password')" prop="configuration.password">
+          <el-input v-model="form.configuration.password" autocomplete="off" show-password />
+        </el-form-item>
+        <el-form-item v-if="form.configuration.dataSourceType=='jdbc'" :label="$t('datasource.port')" prop="configuration.port">
+          <el-input v-model="form.configuration.port" autocomplete="off" />
+        </el-form-item>
+
+      <!-- <el-form-item v-if="canEdit">
         <el-button @click="validaDatasource">{{ $t('commons.validate') }}</el-button>
         <el-button type="primary" @click="save">{{ $t('commons.save') }}</el-button>
       </el-form-item>
-    </el-form>
 
+      <el-form-item v-else>
+        <el-button @click="validaDatasource">{{ $t('commons.validate') }}</el-button>
+        <el-button @click="changeEdit">{{ $t('commons.edit') }}</el-button>
+      </el-form-item> -->
+      </el-form>
+      <div v-if="canEdit" slot="footer" class="dialog-footer">
+        <el-button @click="validaDatasource">{{ $t('commons.validate') }}</el-button>
+        <el-button type="primary" @click="save">{{ $t('commons.save') }}</el-button>
+      </div>
+      <div v-else slot="footer" class="dialog-footer">
+        <el-button @click="validaDatasource">{{ $t('commons.validate') }}</el-button>
+        <el-button type="primary" @click="changeEdit">{{ $t('commons.edit') }}</el-button>
+      </div>
+    </div>
   </layout-content>
 </template>
 
@@ -76,7 +91,8 @@ export default {
         'configuration.port': [{ required: true, message: this.$t('datasource.please_input_port'), trigger: 'change' }]
       },
       allTypes: [{ name: 'mysql', label: 'MySQL', type: 'jdbc' },
-        { name: 'sqlServer', label: 'SQL Server', type: 'jdbc' }]
+        { name: 'sqlServer', label: 'SQL Server', type: 'jdbc' }],
+      canEdit: false
     }
   },
 
@@ -103,8 +119,13 @@ export default {
     // }
   },
   methods: {
+    changeEdit() {
+      this.canEdit = true
+      this.formType = 'modify'
+    },
     create() {
       this.formType = 'add'
+      this.canEdit = true
     },
     edit(row) {
       this.formType = 'modify'
