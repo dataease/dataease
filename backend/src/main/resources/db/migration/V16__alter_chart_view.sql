6825383d9aadb36683a711afbceb2b527aea7dbd
junjie <junjie.xia@fit2cloud.com>
2021-07-21 18:30:13 +0800

    feat: 过滤器兼容flyway

--- backend/src/main/resources/db/migration/V16__alter_chart_view.sql
+++ backend/src/main/resources/db/migration/V16__alter_chart_view.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `chart_view` ADD COLUMN `ext_stack` LONGTEXT COMMENT '堆叠项' AFTER `y_axis`;
 
 UPDATE `chart_view` SET `ext_stack` = '[]';
+UPDATE `chart_view` SET `custom_filter` = '[]';
