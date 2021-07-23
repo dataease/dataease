01c4684e01214c36778c755886b8c6ac400f5bd2
wangjiahao <1522128093@qq.com>
2021-07-23 11:12:54 +0800

    fix:矩阵设计模式下点击一次组件也不需保存镜像

--- frontend/src/components/DeDrag/index.vue
+++ frontend/src/components/DeDrag/index.vue
@@ -1448,6 +1448,7 @@ export default {
     },
     // 记录当前样式
     recordCurStyle() {
+      debugger
       const style = {
         ...this.defaultStyle
       }
@@ -1462,6 +1463,7 @@ export default {
 
     // 记录当前样式 矩阵处理
     recordMatrixCurStyle() {
+      debugger
       const left = Math.round(this.left / this.curCanvasScale.matrixStyleWidth) * this.curCanvasScale.matrixStyleWidth
       const top = Math.round(this.top / this.curCanvasScale.matrixStyleHeight) * this.curCanvasScale.matrixStyleHeight
       const width = Math.round(this.width / this.curCanvasScale.matrixStyleWidth) * this.curCanvasScale.matrixStyleWidth
@@ -1474,7 +1476,7 @@ export default {
       style.width = width
       style.height = height
       style.rotate = this.rotate
-      this.hasMove = true
+      // this.hasMove = true
       this.$store.commit('setShapeStyle', style)
     },
     mountedFunction() {
