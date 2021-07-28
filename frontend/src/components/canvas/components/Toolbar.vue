f0447a5f9269bc2fc2e396931b87e272239f2ef4
wangjiahao <1522128093@qq.com>
2021-07-28 12:46:49 +0800

    fix:关闭仪表板编辑界面时清空画布缓存，修复过滤组件回显问题

--- frontend/src/components/canvas/components/Toolbar.vue
+++ frontend/src/components/canvas/components/Toolbar.vue
@@ -138,6 +138,8 @@ export default {
 
   methods: {
     close() {
+      // 关闭页面清理缓存
+      this.clearCanvas()
       this.$emit('close-left-panel')
       this.$nextTick(() => {
         bus.$emit('PanelSwitchComponent', { name: 'PanelMain' })
