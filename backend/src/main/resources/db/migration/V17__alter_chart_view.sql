3098ea882dbaf96cc64f674b29a10060219accb7
junjie <junjie.xia@fit2cloud.com>
2021-07-28 17:10:41 +0800

    fix: flyway兼容

--- backend/src/main/resources/db/migration/V17__alter_chart_view.sql
+++ backend/src/main/resources/db/migration/V17__alter_chart_view.sql
@@ -1,4 +1,3 @@
 ALTER TABLE `chart_view` ADD COLUMN `ext_stack` LONGTEXT COMMENT '堆叠项' AFTER `y_axis`;
 
 UPDATE `chart_view` SET `ext_stack` = '[]';
-UPDATE `chart_view` SET `custom_filter` = '[]';
