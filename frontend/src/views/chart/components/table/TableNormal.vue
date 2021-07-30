03659d9316adabaefd22d49d546bfc728f5ed1ea
junjie <junjie.xia@fit2cloud.com>
2021-07-30 19:26:15 +0800

    fix: 视图表格组件行高优化

--- frontend/src/views/chart/components/table/TableNormal.vue
+++ frontend/src/views/chart/components/table/TableNormal.vue
@@ -269,4 +269,8 @@ export default {
   .table-class>>>.body--wrapper{
     background: rgba(1,1,1,0);
   }
+  .table-class>>>.elx-cell{
+    max-height: none!important;
+    line-height: normal!important;
+  }
 </style>
