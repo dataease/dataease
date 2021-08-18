a4cc93757510d897a03ac7ae585f510226a265fe
junjie <junjie.xia@fit2cloud.com>
2021-08-18 18:04:53 +0800

    feat: 矩形树图优化

--- frontend/src/views/chart/view/ChartEdit.vue
+++ frontend/src/views/chart/view/ChartEdit.vue
@@ -436,7 +436,7 @@
                   <el-collapse-item v-show="chart.type !== 'map'" name="size" :title="$t('chart.size')">
                     <size-selector :param="param" class="attr-selector" :chart="chart" @onSizeChange="onSizeChange" />
                   </el-collapse-item>
-                  <el-collapse-item v-show="!view.type.includes('table') && !view.type.includes('text') && view.type !== 'treemap'" name="label" :title="$t('chart.label')">
+                  <el-collapse-item v-show="!view.type.includes('table') && !view.type.includes('text')" name="label" :title="$t('chart.label')">
                     <label-selector :param="param" class="attr-selector" :chart="chart" @onLabelChange="onLabelChange" />
                   </el-collapse-item>
                   <el-collapse-item v-show="!view.type.includes('table') && !view.type.includes('text')" name="tooltip" :title="$t('chart.tooltip')">
