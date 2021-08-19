36b0a2a61445ae7bc63942c7822144c01933432e
taojinlong <jinlong@fit2cloud.com>
2021-08-19 14:09:51 +0800

    fix: 检测数据源状态

--- backend/src/main/java/io/dataease/job/sechedule/Schedular.java
+++ backend/src/main/java/io/dataease/job/sechedule/Schedular.java
@@ -1,10 +1,16 @@
 package io.dataease.job.sechedule;
 
 import com.fit2cloud.quartz.anno.QuartzScheduled;
+import io.dataease.datasource.service.DatasourceService;
 import io.dataease.service.dataset.DataSetTableService;
 
+import javax.annotation.Resource;
+
 public class Schedular {
+    @Resource
     private DataSetTableService dataSetTableService;
+    @Resource
+    private DatasourceService datasourceService;
 
     @QuartzScheduled(cron = "0 0/3 * * * ?")
     public void updateDatasetTableStatus() {
@@ -13,7 +19,7 @@ public class Schedular {
 
     @QuartzScheduled(cron = "0 0/30 * * * ?")
     public void updateDatasourceStatus() {
-        dataSetTableService.updateDatasetTableStatus();
+        datasourceService.updateDatasourceStatus();
     }
 
 }
