f26adc38c9c978d9d078e12ec0fce0ff0ec6e84a
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-08-23 17:23:11 +0800

    fix: 公共链接弹出登录框

--- backend/src/main/java/io/dataease/auth/service/impl/ShiroServiceImpl.java
+++ backend/src/main/java/io/dataease/auth/service/impl/ShiroServiceImpl.java
@@ -63,11 +63,12 @@ public class ShiroServiceImpl implements ShiroService {
         filterChainDefinitionMap.put("/tokenExpired", ANON);
         filterChainDefinitionMap.put("/downline", ANON);
         filterChainDefinitionMap.put("/common-files/**", ANON);
-
+        filterChainDefinitionMap.put("/linkage/getPanelAllLinkageInfo/**", ANON);
         filterChainDefinitionMap.put("/api/auth/logout", "logout");
 
         filterChainDefinitionMap.put("/api/link/resourceDetail/**", "link");
         filterChainDefinitionMap.put("/api/link/viewDetail/**", "link");
+
 
         filterChainDefinitionMap.put("/**", "authc");
 
