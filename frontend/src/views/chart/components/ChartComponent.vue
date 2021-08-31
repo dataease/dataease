1fcc794a1c8dc9dcec2940b98e63b5e677432328
wangjiahao <1522128093@qq.com>
2021-08-31 10:29:20 +0800

    refactor:视图下钻提示优化

--- frontend/src/views/chart/components/ChartComponent.vue
+++ frontend/src/views/chart/components/ChartComponent.vue
@@ -218,7 +218,10 @@ export default {
     trackClick(trackAction) {
       const param = this.pointParam
       if (!param || !param.data || !param.data.dimensionList) {
-        this.$warning(this.$t('panel.no_drill_field'))
+        // 地图提示没有关联字段 其他没有维度信息的 直接返回
+        if (this.chart.type === 'map') {
+          this.$warning(this.$t('panel.no_drill_field'))
+        }
         return
       }
       const linkageParam = {
