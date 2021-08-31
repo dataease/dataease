e72b70d45e846276e66e3128eba96404c068fab0
taojinlong <jinlong@fit2cloud.com>
2021-08-31 11:50:03 +0800

    feat: 默认去掉数据源参数里首尾空格

--- backend/src/main/java/io/dataease/datasource/dto/PgConfigration.java
+++ backend/src/main/java/io/dataease/datasource/dto/PgConfigration.java
@@ -12,8 +12,8 @@ public class PgConfigration extends JdbcDTO {
     public String getJdbc() {
         // 连接参数先写死，后边要把编码、时区等参数放到数据源的设置中
         return "jdbc:postgresql://HOSTNAME:PORT/DATABASE"
-                .replace("HOSTNAME", getHost())
-                .replace("PORT", getPort().toString())
-                .replace("DATABASE", getDataBase());
+                .replace("HOSTNAME", getHost().trim())
+                .replace("PORT", getPort().toString().trim())
+                .replace("DATABASE", getDataBase().trim());
     }
 }
