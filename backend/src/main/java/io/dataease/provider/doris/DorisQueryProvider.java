ac0da75502f00a4246363892802dd37facfcd2b6
junjie <junjie.xia@fit2cloud.com>
2021-09-08 18:38:30 +0800

    fix: rollback

--- backend/src/main/java/io/dataease/provider/doris/DorisQueryProvider.java
+++ backend/src/main/java/io/dataease/provider/doris/DorisQueryProvider.java
@@ -772,13 +772,7 @@ public class DorisQueryProvider extends QueryProvider {
                     whereValue = String.format(DorisConstants.WHERE_BETWEEN, value.get(0), value.get(1));
                 }
             } else {
-                if (request.getDatasetTableField().getDeType() == 1) {
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    String time = simpleDateFormat.format(new Date(Long.parseLong(value.get(0))));
-                    whereValue = String.format(DorisConstants.WHERE_VALUE_VALUE, time);
-                } else {
-                    whereValue = String.format(DorisConstants.WHERE_VALUE_VALUE, value.get(0));
-                }
+                whereValue = String.format(DorisConstants.WHERE_VALUE_VALUE, value.get(0));
             }
             list.add(SQLObj.builder()
                     .whereField(whereName)
