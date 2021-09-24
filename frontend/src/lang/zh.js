2586a4b37dca2c3ed5bf31cfabfa71287678940a
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-09-24 17:27:47 +0800

    fix: 修复仪表板tab组件样式

--- frontend/src/lang/zh.js
+++ frontend/src/lang/zh.js
@@ -395,6 +395,10 @@ export default {
     cancel: '取 消',
     confirm: '确 定'
   },
+  detabs: {
+    eidttitle: '编辑标题',
+    selectview: '选择视图'
+  },
   example: {
     warning: '创建和编辑页面是不能被 keep-alive 缓存的，因为keep-alive 的 include 目前不支持根据路由来缓存，所以目前都是基于 component name 来进行缓存的。如果你想类似的实现缓存效果，可以使用 localStorage 等浏览器缓存方案。或者不要使用 keep-alive 的 include，直接缓存所有页面。详情见'
   },
