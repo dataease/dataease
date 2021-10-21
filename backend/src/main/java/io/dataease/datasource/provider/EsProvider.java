54dcf9ee1afc37e59e6114bb938884204239fe75
taojinlong <jinlong@fit2cloud.com>
2021-10-21 12:37:17 +0800

    fix: es 支持https

--- backend/src/main/java/io/dataease/datasource/provider/EsProvider.java
+++ backend/src/main/java/io/dataease/datasource/provider/EsProvider.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service("es")
 public class EsProvider extends DatasourceProvider {
@@ -177,6 +178,7 @@ public class EsProvider extends DatasourceProvider {
         try {
             String response = exexQuery(datasourceRequest, "show tables", "?format=json");
             tables = fetchTables(response);
+            tables = tables.stream().filter(table -> !table.startsWith(".")).collect(Collectors.toList());
         } catch (Exception e) {
             DataEaseException.throwException(e);
         }
