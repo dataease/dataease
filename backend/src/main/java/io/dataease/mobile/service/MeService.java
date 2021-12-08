d7f1bbbd483936b27c9b94e6a8fd19b3e995e14b
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2021-12-08 17:40:06 +0800

    feat: 新增移动端个人中心

---
+++ backend/src/main/java/io/dataease/mobile/service/MeService.java
@@ -0,0 +1,19 @@
+package io.dataease.mobile.service;
+
+import io.dataease.base.mapper.ext.MobileMeMapper;
+import io.dataease.commons.utils.AuthUtils;
+import io.dataease.mobile.dto.MeItemDTO;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class MeService {
+
+    @Resource
+    private MobileMeMapper mobileMeMapper;
+
+    public MeItemDTO personInfo() {
+        return mobileMeMapper.query(AuthUtils.getUser().getUserId());
+    }
+}
