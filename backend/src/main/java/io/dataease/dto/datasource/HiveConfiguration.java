e8bcb156cb1250b6265dfa6e9040b32bb08e7c1b
taojinlong <jinlong@fit2cloud.com>
2021-12-22 17:02:03 +0800

    fix: 降低 hive 驱动版本

--- backend/src/main/java/io/dataease/dto/datasource/HiveConfiguration.java
+++ backend/src/main/java/io/dataease/dto/datasource/HiveConfiguration.java
@@ -18,7 +18,7 @@ public class HiveConfiguration extends JdbcConfiguration {
                     .replace("PORT", getPort().toString().trim())
                     .replace("DATABASE", getDataBase().trim());
         }else {
-            return "jdbc:hive2://HOSTNAME:PORT/DATABASE?EXTRA_PARAMS"
+            return "jdbc:hive2://HOSTNAME:PORT/DATABASE;EXTRA_PARAMS"
                     .replace("HOSTNAME", getHost().trim())
                     .replace("PORT", getPort().toString().trim())
                     .replace("DATABASE", getDataBase().trim())
