b246111524c7324bd2bd0cb732df3247b45df36c
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-01-03 20:34:56 +0800

    fix: 移动端分页api

--- backend/src/main/java/io/dataease/mobile/api/DirApi.java
+++ backend/src/main/java/io/dataease/mobile/api/DirApi.java
@@ -3,20 +3,16 @@ package io.dataease.mobile.api;
 
 import io.dataease.mobile.dto.DirItemDTO;
 import io.dataease.mobile.dto.DirRequest;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@Api(tags = "移动端：目录")
 @RequestMapping("/mobile/dir")
 public interface DirApi {
 
 
-    @ApiOperation("查询")
     @PostMapping("/query")
     List<DirItemDTO> query(@RequestBody DirRequest request);
 }
