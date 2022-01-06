93ff28704d508eb32b39f200ca0504c2676f1584
taojinlong <jinlong@fit2cloud.com>
2022-01-06 15:16:50 +0800

    fix: 兼容以前的公共链接

--- frontend/src/components/canvas/custom-component/UserView.vue
+++ frontend/src/components/canvas/custom-component/UserView.vue
@@ -183,7 +183,6 @@ export default {
       filter.drill = this.drillClickDimensionList
       filter.resultCount = this.resultCount
       filter.resultMode = this.resultMode
-      filter.user = this.element.user
       filter.queryFrom = 'panel'
       return filter
     },
