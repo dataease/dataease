854f7b62ad84b4e16cd8589b0b99d1bbbdc04b51
taojinlong <jinlong@fit2cloud.com>
2022-02-05 22:07:11 +0800

    feat: 优化 mongo jdbc 参数

--- frontend/src/views/system/datasource/form.vue
+++ frontend/src/views/system/datasource/form.vue
@@ -283,7 +283,7 @@ export default {
         {name: 'ds_doris', label: 'Doris', type: 'jdbc', extraParams: 'characterEncoding=UTF-8&connectTimeout=5000&useSSL=false&allowPublicKeyRetrieval=true'},
         {name: 'ck', label: 'ClickHouse', type: 'jdbc', extraParams: ''},
         {name: 'redshift', label: 'AWS Redshift', type: 'jdbc'},
-        {name: 'mongo', label: 'MongoDB', type: 'jdbc', extraParams: ''},
+        {name: 'mongo', label: 'MongoDB', type: 'jdbc', extraParams: 'rebuildschema=true'},
         {name: 'db2', label: 'Db2', type: 'jdbc', extraParams: ''}
       ],
       schemas: [],
