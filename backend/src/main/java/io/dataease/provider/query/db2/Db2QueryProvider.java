2bd1c83e795d2512a48dce9632f0506abc9459b9
taojinlong <jinlong@fit2cloud.com>
2022-02-23 21:33:50 +0800

    fix: db2 sql 数据集抽取错误

--- backend/src/main/java/io/dataease/provider/query/db2/Db2QueryProvider.java
+++ backend/src/main/java/io/dataease/provider/query/db2/Db2QueryProvider.java
@@ -681,7 +681,7 @@ public class Db2QueryProvider extends QueryProvider {
     public String createRawQuerySQL(String table, List<DatasetTableField> fields, Datasource ds) {
         String[] array = fields.stream().map(f -> {
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("\"").append(f.getOriginName()).append("\" AS ").append(f.getDataeaseName());
+            stringBuilder.append(" \"").append(f.getOriginName()).append("\"");
             return stringBuilder.toString();
         }).toArray(String[]::new);
         if(ds!= null){
