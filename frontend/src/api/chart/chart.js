3e03239941cc3a35f69af0fb4cff5b8a257194de
junjun <junjie.xia@fit2cloud.com>
2022-02-28 11:17:26 +0800

    refactor: 视图权限

--- frontend/src/api/chart/chart.js
+++ frontend/src/api/chart/chart.js
@@ -60,9 +60,9 @@ export function checkSameDataSet(viewIdSource, viewIdTarget) {
   })
 }
 
-export function ajaxGetDataOnly(id, data) {
+export function ajaxGetDataOnly(id, panelId, data) {
   return request({
-    url: '/chart/view/getData/' + id,
+    url: '/chart/view/getData/' + id + '/' + panelId,
     method: 'post',
     loading: true,
     hideMsg: true,
