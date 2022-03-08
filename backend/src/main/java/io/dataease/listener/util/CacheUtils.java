cb0e2b6a65ca5afa809fd9c43abcbb761a32b488
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-03-08 18:26:38 +0800

    refactor: 缓存兼容redis单机模式

--- backend/src/main/java/io/dataease/listener/util/CacheUtils.java
+++ backend/src/main/java/io/dataease/listener/util/CacheUtils.java
@@ -1,30 +1,46 @@
 package io.dataease.listener.util;
 
+import io.dataease.commons.utils.CommonBeanFactory;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.Element;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
-import org.springframework.context.annotation.Configuration;
-import java.util.Date;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
-@Configuration
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class CacheUtils {
 
-    private static CacheManager manager;
+    private static CacheManager cacheManager;
 
-    @Autowired
-    public void setManager(CacheManager manager) {
-        CacheUtils.manager = manager;
+
+    private static CacheManager getCacheManager() {
+        if (cacheManager == null)
+            cacheManager = CommonBeanFactory.getBean(CacheManager.class);
+        return cacheManager;
     }
 
     public static Object get(String cacheName, Object key) {
+        if (getCacheManager() instanceof RedisCacheManager) {
+            org.springframework.cache.Cache cache = getCacheManager().getCache(cacheName);
+            if (null == cache) return null;
+            return cache.get(key);
+        }
         Element element = cache(cacheName).get(key);
         if (null == element) return null;
         return element.getObjectValue();
     }
 
     public static void put(String cacheName, Object key, Object value, Integer ttl, Integer tti) {
+        if (getCacheManager() instanceof RedisCacheManager) {
+            RedisTemplate redisTemplate = (RedisTemplate) CommonBeanFactory.getBean("redisTemplate");
+            ValueOperations valueOperations = redisTemplate.opsForValue();
+            valueOperations.setIfPresent(cacheName + "::" + key , value );
+            return;
+        }
         Element e = new Element(key, value);
         //不设置则使用xml配置
         if (ttl != null) {
@@ -33,19 +49,35 @@ public class CacheUtils {
         }
         if (tti != null)
             e.setTimeToIdle(tti);
-        cache(cacheName).put(e);
+        Cache cache = cache(cacheName);
+        if (null != cache)
+            cache.put(e);
     }
 
     public static boolean remove(String cacheName, Object key) {
+        if (getCacheManager() instanceof RedisCacheManager) {
+            org.springframework.cache.Cache cache = getCacheManager().getCache(cacheName);
+            if (null == cache) return false;
+            return cache.evictIfPresent(key);
+        }
         return cache(cacheName).remove(key);
     }
 
     public static void removeAll(String cacheName) {
+        if (getCacheManager() instanceof RedisCacheManager) {
+            org.springframework.cache.Cache cache = getCacheManager().getCache(cacheName);
+            if (null == cache) return;
+            cache.clear();
+            return;
+        }
         cache(cacheName).removeAll();
     }
 
     private static Cache cache(String cacheName) {
-        net.sf.ehcache.CacheManager cacheManager = ((EhCacheCacheManager) manager).getCacheManager();
+        if (getCacheManager() instanceof RedisCacheManager) {
+            return null;
+        }
+        net.sf.ehcache.CacheManager cacheManager = ((EhCacheCacheManager) getCacheManager()).getCacheManager();
         if (!cacheManager.cacheExists(cacheName))
             cacheManager.addCache(cacheName);
         Cache cacheManagerCache = cacheManager.getCache(cacheName);
@@ -57,6 +89,12 @@ public class CacheUtils {
         long exp = (time - System.currentTimeMillis()) / 1000;
         int intExp = (int)exp;
         removeAll("lic_info");
+        if (getCacheManager() instanceof RedisCacheManager) {
+            RedisTemplate redisTemplate = (RedisTemplate) CommonBeanFactory.getBean("redisTemplate");
+            ValueOperations valueOperations = redisTemplate.opsForValue();
+            valueOperations.set("lic_info::lic", "lic", exp, TimeUnit.SECONDS);
+            return;
+        }
         put("lic_info", "lic", "lic", intExp, intExp);
     }
 }
