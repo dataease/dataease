0ebb8b1d78adf6b2833c3da3090349d819323cf8
taojinlong <jinlong@fit2cloud.com>
2022-04-02 22:20:46 +0800

    fix: 默认授权

--- backend/src/main/java/io/dataease/service/dataset/DataSetTableService.java
+++ backend/src/main/java/io/dataease/service/dataset/DataSetTableService.java
@@ -172,6 +172,7 @@ public class DataSetTableService {
                     DataTableInfoDTO info = new DataTableInfoDTO();
                     info.setExcelSheetDataList(excelSheetDataList);
                     sheetTable.setInfo(new Gson().toJson(info));
+                    sysAuthService.copyAuth(datasetTable.getId(), SysAuthConstants.AUTH_SOURCE_TYPE_DATASET);
                     datasetTableMapper.insert(sheetTable);
                     saveExcelTableField(sheetTable.getId(), excelSheetDataList.get(0).getFields(), true);
                     datasetIdList.add(sheetTable.getId());
@@ -202,6 +203,7 @@ public class DataSetTableService {
                     info.setExcelSheetDataList(excelSheetDataList);
                     sheetTable.setInfo(new Gson().toJson(info));
                     datasetTableMapper.insert(sheetTable);
+                    sysAuthService.copyAuth(datasetTable.getId(), SysAuthConstants.AUTH_SOURCE_TYPE_DATASET);
                     saveExcelTableField(sheetTable.getId(), sheet.getFields(), true);
                     datasetIdList.add(sheetTable.getId());
                 }
