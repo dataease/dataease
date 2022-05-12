a350372ed5609a070b2870b0ab00b939fbcc3890
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-05-12 18:12:21 +0800

    feat: 仪表板条件组件设置标题位置

---
+++ frontend/src/components/widget/DeWidget/TitlePostion.vue
@@ -0,0 +1,67 @@
+<template>
+
+  <el-popover
+    placement="right"
+    trigger="click"
+  >
+    <el-col>
+      <el-form ref="tabsStyleForm" :model="styleInfo" label-width="80px" size="mini" class="demo-form-inline">
+        <el-form-item :label="$t('chart.text_h_position')" class="form-item">
+          <el-radio-group v-model="styleInfo.hPosition" size="mini" @change="styleChange">
+            <el-radio-button label="left">{{ $t('chart.text_pos_left') }}</el-radio-button>
+            <el-radio-button label="center">{{ $t('chart.text_pos_center') }}</el-radio-button>
+            <el-radio-button label="right">{{ $t('chart.text_pos_right') }}</el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item :label="$t('chart.text_v_position')" class="form-item">
+          <el-radio-group v-model="styleInfo.vPosition" size="mini" @change="styleChange">
+            <el-radio-button label="top">{{ $t('chart.text_pos_top') }}</el-radio-button>
+            <el-radio-button label="center">{{ $t('chart.text_pos_center') }}</el-radio-button>
+            <el-radio-button label="bottom">{{ $t('chart.text_pos_bottom') }}</el-radio-button>
+          </el-radio-group>
+        </el-form-item>
+
+      </el-form>
+    </el-col>
+
+    <i slot="reference" class="iconfont icon-tabs" />
+  </el-popover>
+
+</template>
+
+<script>
+export default {
+
+  name: 'TitlePosition',
+  props: {
+    styleInfo: {
+      type: Object,
+      default: null
+    }
+  },
+
+  data() {
+    return {
+      defaultPosition: {
+        hPosition: 'left',
+        vPosition: 'center'
+      }
+    }
+  },
+
+  created() {
+
+  },
+  methods: {
+
+    styleChange() {
+      this.$store.commit('recordStyleChange')
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
