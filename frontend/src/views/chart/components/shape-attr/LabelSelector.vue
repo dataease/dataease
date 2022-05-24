e94cd85d17e304da9f91fb640c217337b4f4b5e6
junjun <junjie.xia@fit2cloud.com>
2022-05-24 17:37:16 +0800

    refactor: 视图样式配置项优化

--- frontend/src/views/chart/components/shape-attr/LabelSelector.vue
+++ frontend/src/views/chart/components/shape-attr/LabelSelector.vue
@@ -17,7 +17,7 @@
           <el-form-item v-show="showProperty('color')" :label="$t('chart.text_color')" class="form-item">
             <el-color-picker v-model="labelForm.color" class="color-picker-style" :predefine="predefineColors" @change="changeLabelAttr('color')" />
           </el-form-item>
-          <el-form-item v-show="showProperty('position')" :label="$t('chart.label_position')" class="form-item">
+          <el-form-item v-show="showProperty('position') && chart.type !== 'map'" :label="$t('chart.label_position')" class="form-item">
             <el-select v-model="labelForm.position" :placeholder="$t('chart.label_position')" @change="changeLabelAttr('position')">
               <el-option v-for="option in labelPosition" :key="option.value" :label="option.name" :value="option.value" />
             </el-select>
