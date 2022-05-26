791c27910c588bfee92e4cd26145e9c9048464cf
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-05-26 21:22:09 +0800

    fix: 操作日志国际化

--- backend/src/main/java/io/dataease/service/sys/log/LogService.java
+++ backend/src/main/java/io/dataease/service/sys/log/LogService.java
@@ -22,7 +22,6 @@ import io.dataease.ext.query.GridExample;
 import io.dataease.i18n.Translator;
 import io.dataease.plugins.common.base.domain.SysLogWithBLOBs;
 import io.dataease.plugins.common.base.mapper.SysLogMapper;
-import mondrian.olap.fun.vba.Excel;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -30,12 +29,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -45,7 +39,30 @@ public class LogService {
 
     private Gson gson = new Gson();
 
-    private static Integer[] unPanelOperates = {4, 5, 8, 9, 10};
+    // 仪表板的额外操作 分享以及公共链接
+    private static Integer[] panel_ext_ope = {4, 5, 8, 9, 10};
+
+    // 驱动文件操作 上传， 删除
+    private static Integer[] driver_file_ope = {11, 3};
+
+
+
+    // 排除驱动和驱动文件的公共操作的资源类型
+    private static Integer[] COMMON_SOURCE = {1, 2,3,4,6,7,8,9};
+
+    // 增 改 删  针对公共资源的操作
+    private static Integer[] COMMON_SOURCE_OPERATE = {1 ,2 , 3};
+
+    // 授权相关操作
+    private static Integer[] AUTH_OPERATE = {6, 7};
+
+    // 授权相关资源 数据源 仪表板 数据集
+    private static Integer[] AUTH_SOURCE = {1, 2, 3};
+
+
+
+
+
 
     @Resource
     private SysLogMapper sysLogMapper;
@@ -99,28 +116,64 @@ public class LogService {
 
 
     public List<FolderItem> types() {
-        List<Integer> integers = Arrays.stream(unPanelOperates).collect(Collectors.toList());
+
+
         List<FolderItem> results = new ArrayList<>();
-        SysLogConstants.SOURCE_TYPE[] sourceTypes = SysLogConstants.SOURCE_TYPE.values();
-        SysLogConstants.OPERATE_TYPE[] operateTypes = SysLogConstants.OPERATE_TYPE.values();
-        for (int i = 0; i < sourceTypes.length; i++) {
-            SysLogConstants.SOURCE_TYPE sourceType = sourceTypes[i];
-            for (int j = 0; j < operateTypes.length; j++) {
-                SysLogConstants.OPERATE_TYPE operateType = operateTypes[j];
-                if (sourceType.getValue() > 3 && operateType.getValue() > 3){
-                    continue;
-                }
-                if (sourceType.getValue() != 3 && integers.contains(operateType.getValue())) {
-                    continue;
-                }
+
+
+        for (int i = 0; i < COMMON_SOURCE.length; i++) {
+
+            Integer sourceVal = COMMON_SOURCE[i];
+            String sourceTypeName = SysLogConstants.sourceTypeName(sourceVal);
+
+            for (int j = 0; j < COMMON_SOURCE_OPERATE.length; j++) {
+
+                Integer operateVal = COMMON_SOURCE_OPERATE[j];
+                String operateTypeName = SysLogConstants.operateTypeName(operateVal);
                 FolderItem folderItem = new FolderItem();
-                folderItem.setId(operateType.getValue() + "-" + sourceType.getValue());
-                String operateTypeName = operateType.getName();
-                String sourceTypeName = sourceType.getName();
+                folderItem.setId(operateVal + "-" + sourceVal);
                 folderItem.setName( Translator.get(operateTypeName) + Translator.get(sourceTypeName));
                 results.add(folderItem);
             }
         }
+
+
+        for (int i = 0; i < driver_file_ope.length; i++) {
+            SysLogConstants.SOURCE_TYPE sourceType = SysLogConstants.SOURCE_TYPE.DRIVER_FILE;
+            FolderItem folderItem = new FolderItem();
+            folderItem.setId(driver_file_ope[i] + "-" + sourceType.getValue());
+            String operateTypeName = SysLogConstants.operateTypeName(driver_file_ope[i]);
+            String sourceTypeName = sourceType.getName();
+            folderItem.setName( Translator.get(operateTypeName) + Translator.get(sourceTypeName));
+            results.add(folderItem);
+        }
+
+        for (int i = 0; i < AUTH_SOURCE.length; i++) {
+            Integer sourceVal = AUTH_SOURCE[i];
+            String sourceTypeName = SysLogConstants.sourceTypeName(sourceVal);
+
+            for (int j = 0; j < AUTH_OPERATE.length; j++) {
+
+                Integer operateVal = AUTH_OPERATE[j];
+                String operateTypeName = SysLogConstants.operateTypeName(operateVal);
+                FolderItem folderItem = new FolderItem();
+                folderItem.setId(operateVal + "-" + sourceVal);
+                folderItem.setName( Translator.get(operateTypeName) + Translator.get(sourceTypeName));
+                results.add(folderItem);
+            }
+        }
+
+        for (int i = 0; i < panel_ext_ope.length; i++) {
+            SysLogConstants.SOURCE_TYPE sourceType = SysLogConstants.SOURCE_TYPE.PANEL;
+            FolderItem folderItem = new FolderItem();
+            folderItem.setId(panel_ext_ope[i] + "-" + sourceType.getValue());
+            String operateTypeName = SysLogConstants.operateTypeName(panel_ext_ope[i]);
+            String sourceTypeName = sourceType.getName();
+            folderItem.setName( Translator.get(operateTypeName) + Translator.get(sourceTypeName));
+            results.add(folderItem);
+        }
+
+
 
         return results;
     }
