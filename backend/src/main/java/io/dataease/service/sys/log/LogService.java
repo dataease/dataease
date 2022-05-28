215d262bbf7fa00bed084a20018e105bfb6c8722
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-05-28 21:42:53 +0800

    fix: 暂时屏蔽视图操作日志

--- backend/src/main/java/io/dataease/service/sys/log/LogService.java
+++ backend/src/main/java/io/dataease/service/sys/log/LogService.java
@@ -48,7 +48,10 @@ public class LogService {
 
 
     // 排除驱动和驱动文件的公共操作的资源类型
-    private static Integer[] COMMON_SOURCE = {1, 2,3,4,6,7,8,9};
+    // 暂时屏蔽视图日志
+    // private static Integer[] COMMON_SOURCE = {1, 2,3,4,6,7,8,9};
+
+    private static Integer[] COMMON_SOURCE = {1, 2,3,6,7,8,9};
 
     // 增 改 删  针对公共资源的操作
     private static Integer[] COMMON_SOURCE_OPERATE = {1 ,2 , 3};
