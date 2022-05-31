e04dea30a4e1290d5b7e819afaa5565ad19b1ecb
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-05-31 23:15:16 +0800

    perf: 优化选项卡选择组件提示

--- frontend/src/lang/zh.js
+++ frontend/src/lang/zh.js
@@ -417,7 +417,8 @@ export default {
     eidttitle: '编辑标题',
     selectview: '选择视图',
     selectOthers: '选择组件',
-    availableComponents: '可选组件'
+    availableComponents: '可选组件',
+    please: '未'
   },
   example: {
     warning: '创建和编辑页面是不能被 keep-alive 缓存的，因为keep-alive 的 include 目前不支持根据路由来缓存，所以目前都是基于 component name 来进行缓存的。如果你想类似的实现缓存效果，可以使用 localStorage 等浏览器缓存方案。或者不要使用 keep-alive 的 include，直接缓存所有页面。详情见'
