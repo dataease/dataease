cbb2e8e6c713281db964120692f4a1df2ac54426
junjun <junjie.xia@fit2cloud.com>
2022-06-08 15:43:04 +0800

    fix: 修复辅助线再次编辑确认会消失的问题

--- frontend/src/views/chart/components/senior/AssistLine.vue
+++ frontend/src/views/chart/components/senior/AssistLine.vue
@@ -86,6 +86,7 @@ export default {
         } else {
           this.assistLine = []
         }
+        this.lineArr = JSON.parse(JSON.stringify(this.assistLine))
       }
     },
     changeAssistLine() {
