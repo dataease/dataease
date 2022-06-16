2ce2f9411316817ddda7729a4d2c6c53b660bc1b
wangjiahao <1522128093@qq.com>
2022-06-16 14:44:54 +0800

    fix: 插件操作加入权限控制，解决普通用户或者API能越权上传卸载插件问题

--- backend/src/main/java/io/dataease/controller/sys/SysPluginController.java
+++ backend/src/main/java/io/dataease/controller/sys/SysPluginController.java
@@ -10,6 +10,7 @@ import io.dataease.controller.sys.request.PluginStatus;
 import io.dataease.service.sys.PluginService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,6 +30,7 @@ public class SysPluginController {
 
     @ApiOperation("查询已安装插件")
     @PostMapping("/pluginGrid/{goPage}/{pageSize}")
+    @RequiresPermissions("plugin:read")
     public Pager<List<MyPlugin>> pluginGrid(@PathVariable int goPage, @PathVariable int pageSize, @RequestBody BaseGridRequest request) {
         Page<Object> page = PageHelper.startPage(goPage, pageSize, true);
         return PageUtils.setPageInfo(page, pluginService.query(request));
@@ -36,19 +38,21 @@ public class SysPluginController {
 
     @ApiOperation("安装插件")
     @PostMapping("upload")
+    @RequiresPermissions("plugin:upload")
     public Map<String, Object> localUpload(@RequestParam("file") MultipartFile file) throws Exception {
         return pluginService.localInstall(file);
     }
 
     @ApiOperation("卸载插件")
     @PostMapping("/uninstall/{pluginId}")
+    @RequiresPermissions("plugin:uninstall")
     public Boolean unInstall(@PathVariable Long pluginId) {
         return pluginService.uninstall(pluginId);
     }
 
-    @ApiOperation("切换插件状态")
-    @PostMapping("/changeStatus")
-    public Boolean changeStatus(@RequestBody PluginStatus pluginStatus) {
-        return pluginService.changeStatus(pluginStatus.getPluginId(), pluginStatus.getStatus());
-    }
+//    @ApiOperation("切换插件状态")
+//    @PostMapping("/changeStatus")
+//    public Boolean changeStatus(@RequestBody PluginStatus pluginStatus) {
+//        return pluginService.changeStatus(pluginStatus.getPluginId(), pluginStatus.getStatus());
+//    }
 }
