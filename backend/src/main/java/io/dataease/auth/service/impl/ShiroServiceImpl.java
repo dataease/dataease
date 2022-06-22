1de0d83284ce7e9eaf4f4f60b1f94dbdd6952598
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-06-22 10:46:01 +0800

    fix: 退出登录错误

--- backend/src/main/java/io/dataease/auth/service/impl/ShiroServiceImpl.java
+++ backend/src/main/java/io/dataease/auth/service/impl/ShiroServiceImpl.java
@@ -74,6 +74,7 @@ public class ShiroServiceImpl implements ShiroService {
 
 
         filterChainDefinitionMap.put("/api/auth/login", ANON);
+        filterChainDefinitionMap.put("/api/auth/logout", ANON);
         filterChainDefinitionMap.put("/api/auth/isPluginLoaded", ANON);
         filterChainDefinitionMap.put("/system/requestTimeOut", ANON);
         filterChainDefinitionMap.put("/api/auth/validateName", ANON);
