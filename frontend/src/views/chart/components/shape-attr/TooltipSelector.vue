f758517863ae592bedf5a427c0b2bb75ba23d86d
wangjiahao <1522128093@qq.com>
2022-07-08 10:29:32 +0800

    fix(视图): 修复批量设置Echarts提示背景未生效

--- frontend/src/views/chart/components/shape-attr/TooltipSelector.vue
+++ frontend/src/views/chart/components/shape-attr/TooltipSelector.vue
@@ -21,7 +21,7 @@
             <el-color-picker v-model="tooltipForm.textStyle.color" class="color-picker-style" :predefine="predefineColors" @change="changeTooltipAttr('textStyle')" />
           </el-form-item>
           <el-form-item v-show="showProperty('textStyle')" :label="$t('chart.background')" class="form-item">
-            <el-color-picker v-model="tooltipForm.backgroundColor" class="color-picker-style" :predefine="predefineColors" @change="changeTooltipAttr('textStyle')" />
+            <el-color-picker v-model="tooltipForm.backgroundColor" class="color-picker-style" :predefine="predefineColors" @change="changeTooltipAttr('backgroundColor')" />
           </el-form-item>
           <el-form-item v-show="showProperty('formatter')" class="form-item">
             <span slot="label">
