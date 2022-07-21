1112dcb763ca7e55f99900c792a7a8ce22976e81
wangjiahao <1522128093@qq.com>
2022-07-21 11:33:41 +0800

    refactor: 更新模板市场菜单

--- backend/src/main/resources/db/migration/V38__1.13.sql
+++ backend/src/main/resources/db/migration/V38__1.13.sql
@@ -38,48 +38,7 @@ INSERT INTO `my_plugin` (`name`, `store`, `free`, `cost`, `category`, `descript`
 VALUES ('Mongo 数据源插件', 'default', '0', '0', 'datasource', 'Mongo 数据源插件', '1.0-SNAPSHOT', 'DATAEASE', '0',
         '1650765903630', 'mongo-backend', 'mongobi');
 
-
-INSERT INTO `sys_menu` (
-    `menu_id`,
-    `pid`,
-    `sub_count`,
-    `type`,
-    `title`,
-    `name`,
-    `component`,
-    `menu_sort`,
-    `icon`,
-    `path`,
-    `i_frame`,
-    `cache`,
-    `hidden`,
-    `permission`,
-    `create_by`,
-    `update_by`,
-    `create_time`,
-    `update_time`
-)
-VALUES
-    (
-        41,
-        1,
-        1,
-        1,
-        '模板市场',
-        'template-market',
-        'panel/templateMarket/index',
-        13,
-        'dashboard',
-        'panel/templateMarket/index',
-        0,
-        0,
-        0,
-        '',
-        NULL,
-        NULL,
-        NULL,
-        1620444227389
-    );
+INSERT INTO `sys_menu` (`menu_id`, `pid`, `sub_count`, `type`, `title`, `name`, `component`, `menu_sort`, `icon`, `path`, `i_frame`, `cache`, `hidden`, `permission`, `create_by`, `update_by`, `create_time`, `update_time`) VALUES (202, 0, 0, 1, '模板市场', 'template-market', 'panel/templateMarket/index', 7, 'dashboard', '/templateMarket', 0, 0, 0, null, NULL, NULL, NULL, 1620444227389);
 
 INSERT INTO `system_parameter` (`param_key`, `param_value`, `type`, `sort`) VALUES ('basic.templateAccessKey', 'dataease', 'text', NULL);
 INSERT INTO `system_parameter` (`param_key`, `param_value`, `type`, `sort`) VALUES ('basic.templateMarketUlr', 'https://dataease.io/templates', 'text', 4);
