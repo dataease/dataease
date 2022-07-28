7c14840da846eb542bfbb2e361f831b176659356
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-07-28 22:11:32 +0800

    fix(仪表板-过滤器): 时间过滤组件过滤精度到毫秒的视图有误差

--- backend/src/main/java/io/dataease/provider/query/es/EsQueryProvider.java
+++ backend/src/main/java/io/dataease/provider/query/es/EsQueryProvider.java
@@ -1119,7 +1119,7 @@ public class EsQueryProvider extends QueryProvider {
                 whereValue = "'%" + value.get(0) + "%'";
             } else if (StringUtils.containsIgnoreCase(request.getOperator(), "between")) {
                 if (request.getDatasetTableField().getDeType() == 1) {
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
                     String startTime = simpleDateFormat.format(new Date(Long.parseLong(value.get(0))));
                     String endTime = simpleDateFormat.format(new Date(Long.parseLong(value.get(1))));
                     whereValue = String.format(EsSqlLConstants.WHERE_BETWEEN, startTime, endTime);
