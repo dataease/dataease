fea0d68c52df4b24db5d1e59eb9f089d445c27ea
刘  稳 <liu.wen@trs.com.cn>
2022-08-25 10:24:18 +0800

    fix:代码修改20220825测试url链接变换问题

---
+++ frontend/src/views/system/dataFill/addInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-dialog
+      title="新增数据填报"
+      :visible.sync="dialogVisible"
+      width="80%"
+      :before-close="handleClose"
+    >
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
