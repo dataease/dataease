aa5a410eb239ae506c55589af395b096c4180fa5
zl_liuhaiyong <haiyong.liu@transwarp.cn>
2022-08-31 10:41:43 +0800

    长链接取值

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,6 +13,32 @@ export default {
   components: { PluginCom },
   beforeCreate() {
 
+  },
+  mounted() {
+    const attachParams = this.getQueryVariable('attachParams')
+    console.log('attachParams,,,',attachParams)
+    localStorage.setItem('permissionId',attachParams)
+  },
+  methods: {
+    getQueryVariable(variable) {
+      // let query = window.location.search.substring(1)
+      let query = window.location.href.split('?')[1]
+      console.log('query',query)
+      let vars = []
+      if (!query) {
+        query = document.cookie
+        vars = query.split(';')
+      } else {
+        vars = query.split('&')
+      }
+      for (var i = 0; i < vars.length; i++) {
+        const pair = vars[i].split('=')
+        if (pair[0].trim() === variable) {
+          return pair[1]
+        }
+      }
+      return (false)
+    }
   }
 }
 </script>
