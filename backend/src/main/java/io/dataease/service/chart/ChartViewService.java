98055c354b72f20e5f82efe251db8207b73052e3
taojinlong <jinlong@fit2cloud.com>
2022-09-28 10:20:29 +0800

    fix: 不同的sql 数据集参数同名

--- backend/src/main/java/io/dataease/service/chart/ChartViewService.java
+++ backend/src/main/java/io/dataease/service/chart/ChartViewService.java
@@ -664,10 +664,12 @@ public class ChartViewService {
                 }
                 boolean hasParameters = false;
                 if (StringUtils.isNotEmpty(table.getSqlVariableDetails())) {
-                    List<SqlVariableDetails> sqlVariables = new Gson().fromJson(table.getSqlVariableDetails(), new TypeToken<List<SqlVariableDetails>>() {
-                    }.getType());
+                    List<SqlVariableDetails> sqlVariables = new Gson().fromJson(table.getSqlVariableDetails(), new TypeToken<List<SqlVariableDetails>>() {}.getType());
                     for (String parameter : Optional.ofNullable(request.getParameters()).orElse(new ArrayList<>())) {
                         if (sqlVariables.stream().map(SqlVariableDetails::getVariableName).collect(Collectors.toList()).contains(parameter)) {
+                            hasParameters = true;
+                        }
+                        if (parameter.contains("|DE|") && table.getId().equals(parameter.split("|DE|")[0]) && sqlVariables.stream().map(SqlVariableDetails::getVariableName).collect(Collectors.toList()).contains(parameter.split("|DE|")[1])) {
                             hasParameters = true;
                         }
                     }
@@ -1591,10 +1593,7 @@ public class ChartViewService {
     }
 
     private String handleVariable(String sql, ChartExtRequest requestList, QueryProvider qp, DataSetTableDTO table, Datasource ds) throws Exception {
-
-        List<SqlVariableDetails> sqlVariables = new Gson().fromJson(table.getSqlVariableDetails(), new TypeToken<List<SqlVariableDetails>>() {
-        }.getType());
-
+        List<SqlVariableDetails> sqlVariables = new Gson().fromJson(table.getSqlVariableDetails(), new TypeToken<List<SqlVariableDetails>>() {}.getType());
         if (requestList != null && CollectionUtils.isNotEmpty(requestList.getFilter())) {
             for (ChartExtFilterRequest chartExtFilterRequest : requestList.getFilter()) {
                 if (CollectionUtils.isEmpty(chartExtFilterRequest.getValue())) {
@@ -1605,12 +1604,22 @@ public class ChartViewService {
                 }
 
                 for (String parameter : chartExtFilterRequest.getParameters()) {
-                    List<SqlVariableDetails> parameters = sqlVariables.stream().filter(item -> item.getVariableName().equalsIgnoreCase(parameter)).collect(Collectors.toList());
-                    if (CollectionUtils.isNotEmpty(parameters)) {
-                        String filter = qp.transFilter(chartExtFilterRequest, parameters.get(0));
-                        sql = sql.replace("${" + parameter + "}", filter);
+                    if(parameter.contains("|DE")){
+                        if(!parameter.split("|DE|")[0].equals(table.getId())){
+                            continue;
+                        }
+                        List<SqlVariableDetails> parameters = sqlVariables.stream().filter(item -> item.getVariableName().equalsIgnoreCase(parameter.split("|DE|")[1])).collect(Collectors.toList());
+                        if (CollectionUtils.isNotEmpty(parameters)) {
+                            String filter = qp.transFilter(chartExtFilterRequest, parameters.get(0));
+                            sql = sql.replace("${" + parameter.split("|DE|")[1] + "}", filter);
+                        }
+                    }else {
+                        List<SqlVariableDetails> parameters = sqlVariables.stream().filter(item -> item.getVariableName().equalsIgnoreCase(parameter)).collect(Collectors.toList());
+                        if (CollectionUtils.isNotEmpty(parameters)) {
+                            String filter = qp.transFilter(chartExtFilterRequest, parameters.get(0));
+                            sql = sql.replace("${" + parameter + "}", filter);
+                        }
                     }
-
                 }
             }
         }
