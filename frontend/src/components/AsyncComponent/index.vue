f55365e6574dca74e65990e7a9f920ab1cbaf6d1
dataeaseShu <106045316+dataeaseShu@users.noreply.github.com>
2022-09-30 10:49:41 +0800

    fix: 创建数据集 拉宽 保存校验 请求数据 loading

--- frontend/src/components/AsyncComponent/index.vue
+++ frontend/src/components/AsyncComponent/index.vue
@@ -42,8 +42,105 @@ export default {
           window.SyncComponentCache = {}
         }
         let res
+        console.log(1, this.$route);
+        const urlMap = {
+            'system-role': 'SystemRole',
+            'system-dept': 'SystemDept',
+            'system-auth': 'SystemAuth',
+            'email-task': 'TaskEmail',
+            'system-dept-form': 'SystemDeptFormn',
+            'system-role-form': 'SystemRoleForm',
+            'email-task-form': 'TaskEmailForm',
+            'system-ukey': 'SystemUkey',
+            740: 'XpackThemeForm',
+        }
+
+        // if (this.$route.name === 'system-role') {
+        //   this.mode = () => import('de-plugins/src/views/xpack/role/index.vue');
+        //   return
+        // }
+
+        // if (this.$route.name === 'system-role-form') {
+        //   this.mode = () => import('de-plugins/src/views/xpack/role/form.vue');
+        //   return
+        // }
+
+        // if (this.$route.name === 'dataset') {
+        //   this.mode = () => import('de-plugins/src/views/xpack/dataset/rowpermissions/rowPermissions.vue');
+        //   return
+        // }
+
+        // if (this.$route.name === 'system-dept') {
+        //   this.mode = () => import('de-plugins/src/views/xpack/dept/index.vue');
+        //   return
+        // }
+        console.log('this.url', this.url);
+        // if (this.url.includes('LdapSetting')) {
+        //   this.mode = () => import('de-plugins/src/views/xpack/settings/ldap/index.vue');
+        //   return
+        // }
+
+        // if (this.url.includes('SsoSetting')) {
+        //   this.mode = () => import('de-plugins/src/views/xpack/settings/sso/index.vue');
+        //   return
+        // }
+
+        // if (this.url.includes('CasSetting')) {
+        //   this.mode = () => import('de-plugins/src/views/xpack/settings/cas/index.vue');
+        //   return
+        // }
+
+
+        // if (this.url.includes('DisplaySetting')) {
+        //   this.mode = () => import('de-plugins/src/views/xpack/settings/display/index.vue');
+        //   return
+        // }
+
+        // if (this.url.includes('ThemeSetting')) {
+        //   this.mode = () => import('de-plugins/src/views/xpack/settings/theme/index.vue');
+        //   return
+        // }
+
+        // if (this.url.includes('740')) {
+        //   this.mode = () => import('de-plugins/src/views/xpack/theme/XpackThemeForm.vue');
+        //   return
+        // }
+
+        // if (this.url.includes('710')) {
+        //   this.mode = () => import('de-plugins/src/views/xpack/theme/XpackThemeMenu.vue');
+        //   return
+        // }
+
+        if (this.$route.name === 'sys-task-email') {
+          this.mode = () => import('de-plugins/src/views/xpack/task/TaskList.vue');
+          return
+        }
+
+        // if (this.url.includes('61')) {
+        //   this.mode = () => import('de-plugins/src/views/xpack/task/email.vue');
+        //   return
+        // }
+
+        // if (this.url.includes('62')) {
+        //   this.mode = () => import('de-plugins/src/views/xpack/task/form.vue');
+        //   return
+        // }
+
+        // const arr = this.url.split('/');
+        // let text = arr[arr.length - 1 ]
+        // if (urlMap[text]) {
+        //   text = urlMap[text]
+        // }
+
+        // console.log(1, text);
+        
+
         if (!window.SyncComponentCache[this.url]) {
+          // window.SyncComponentCache[this.url] = get(`/static/${text}.js`)
+
+
           window.SyncComponentCache[this.url] = get(this.url)
+
           // window.SyncComponentCache[this.url] = Axios.get(this.url)
           res = await window.SyncComponentCache[this.url]
         } else {
@@ -51,7 +148,7 @@ export default {
         }
         if (res) {
           const Fn = Function
-          const dynamicCode = res.data || res
+          const dynamicCode = res
           this.mode = new Fn(`return ${dynamicCode}`)()
         }
       }
@@ -71,3 +168,8 @@ export default {
   }
 }
 </script>
+
+
+
+
+
