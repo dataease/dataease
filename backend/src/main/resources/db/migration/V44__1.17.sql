2e7a268d5504e275a1adafcb8a671209db69ec76
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-11-09 10:50:42 +0800

    perf(数据集): 优化数据集表单路由驼峰命名

--- backend/src/main/resources/db/migration/V44__1.17.sql
+++ backend/src/main/resources/db/migration/V44__1.17.sql
@@ -2,3 +2,5 @@ ALTER TABLE `sys_log`
     CHANGE COLUMN `user_id` `user_id` BIGINT NULL COMMENT '操作人',
     CHANGE COLUMN `login_name` `login_name` VARCHAR (255) NULL COMMENT '登录账号',
     CHANGE COLUMN `nick_name` `nick_name` VARCHAR (255) NULL COMMENT '姓名';
+
+UPDATE `sys_menu` SET `component` = 'dataset/Form' WHERE (`menu_id` = '800');
