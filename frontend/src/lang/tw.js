62b199323608efdd0416b36183405b27c612bb15
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-11-28 11:55:56 +0800

    fix(用户管理): 用户表单可以保存包含特殊字符.的姓名

--- frontend/src/lang/tw.js
+++ frontend/src/lang/tw.js
@@ -652,6 +652,7 @@ export default {
     input_roles: '請選擇角色',
     select_users: '請選擇用戶',
     select_gender: '請選擇性別',
+    user_name_pattern_error: 'ID只能包含字母数字下划线以及-并以字母开头！',
     special_characters_are_not_supported: '不支持特殊字符',
     mobile_number_format_is_incorrect: '手機號碼格式不正確',
     email_format_is_incorrect: '郵箱格式不正確',
