68c50614a5a933c84a17ee22625961ccc48eec21
wangjiahao <1522128093@qq.com>
2022-12-13 11:42:05 +0800

    refactor: 调整flyway

---
+++ backend/src/main/resources/db/migration/V45__1.17.1.sql
@@ -0,0 +1,4 @@
+INSERT INTO `my_plugin` (`name`, `store`, `free`, `cost`, `category`, `descript`, `version`, `creator`, `load_mybatis`,
+                         `install_time`, `module_name`, `ds_type`)
+VALUES ('人大金仓数据源插件', 'default', '0', '0', 'datasource', '人大金仓数据源插件', '1.0-SNAPSHOT', 'DATAEASE', '0',
+        '1650765903630', 'kingbase-backend', 'kingbase');
