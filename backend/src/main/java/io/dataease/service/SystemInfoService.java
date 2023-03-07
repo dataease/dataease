7e8445ba8c4d62c9b6b51ffcf12b3f080e98efb2
wangjiahao <1522128093@qq.com>
2023-03-07 17:55:19 +0800

    fix: 修复公共链接获取多余用户信息漏洞问题

--- backend/src/main/java/io/dataease/service/SystemInfoService.java
+++ backend/src/main/java/io/dataease/service/SystemInfoService.java
@@ -2,12 +2,10 @@ package io.dataease.service;
 
 import io.dataease.auth.api.dto.CurrentUserDto;
 import io.dataease.commons.utils.AuthUtils;
-import io.dataease.commons.utils.BeanUtils;
 import io.dataease.commons.utils.IPUtils;
 import io.dataease.dto.UserLoginInfoDTO;
 import io.dataease.plugins.common.base.domain.SysUser;
 import io.dataease.plugins.common.base.mapper.SysUserMapper;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -18,14 +16,18 @@ public class SystemInfoService {
     @Resource
     private SysUserMapper sysUserMapper;
 
-    public UserLoginInfoDTO getUserLoginInfo(String userId) {
-        if (StringUtils.isNotEmpty(userId)) {
-            SysUser userInfo = sysUserMapper.selectByPrimaryKey(Long.parseLong(userId));
-            CurrentUserDto userDto = new CurrentUserDto();
-            BeanUtils.copyBean(userDto, userInfo);
-            return new UserLoginInfoDTO(userDto, IPUtils.get());
+    public UserLoginInfoDTO getUserLoginInfo(Long userId) {
+        if (userId != null) {
+            SysUser userInfo = sysUserMapper.selectByPrimaryKey(userId);
+            return new UserLoginInfoDTO(userInfo.getUsername(), userInfo.getNickName(), IPUtils.get());
         }
-        return new UserLoginInfoDTO(AuthUtils.getUser(), IPUtils.get());
+        CurrentUserDto userDto = AuthUtils.getUser();
+        if (userDto != null) {
+            return new UserLoginInfoDTO(userDto.getUsername(), userDto.getNickName(), IPUtils.get());
+        } else {
+            return new UserLoginInfoDTO(null, null, IPUtils.get());
+        }
     }
+
 
 }
