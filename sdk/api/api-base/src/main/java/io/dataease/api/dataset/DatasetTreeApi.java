832c4f620210f5fa86cddb556da89542dbbc5ee1
junjun <junjie.xia@fit2cloud.com>
2023-03-21 14:29:25 +0800

    fix: rename

--- sdk/api/api-base/src/main/java/io/dataease/api/dataset/DatasetTreeApi.java
+++ sdk/api/api-base/src/main/java/io/dataease/api/dataset/DatasetTreeApi.java
@@ -1,13 +1,13 @@
 package io.dataease.api.dataset;
 
-import io.dataease.api.dataset.vo.DataSetTreeNodeVO;
+import io.dataease.api.dataset.vo.DatasetTreeNodeVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-public interface DataSetTreeApi {
+public interface DatasetTreeApi {
 
     @GetMapping("/query/{keyword}")
-    List<DataSetTreeNodeVO> query(@PathVariable("keyword") String keyword);
+    List<DatasetTreeNodeVO> query(@PathVariable("keyword") String keyword);
 }
