0dbaec5ddd6bb9d80299d35d2d4ac6d526020418
junjun <junjie.xia@fit2cloud.com>
2023-03-29 12:51:41 +0800

    feat: 数据集基本业务

---
+++ sdk/api/api-base/src/main/java/io/dataease/api/dataset/dto/DatasetNodeDTO.java
@@ -0,0 +1,67 @@
+package io.dataease.api.dataset.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class DatasetNodeDTO implements Serializable {
+
+    /**
+     * ID
+     */
+    private String id;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 父级ID
+     */
+    private String pid;
+
+    /**
+     * 当前分组处于第几级
+     */
+    private Integer level;
+
+    /**
+     * node类型：folder or dataset
+     */
+    private String nodeType;
+
+    /**
+     * 连接模式：0-直连，1-同步(包括excel、api等数据存在de中的表)
+     */
+    private Integer mode;
+
+    /**
+     * 关联关系树
+     */
+    private String info;
+
+    /**
+     * 创建人ID
+     */
+    private String createBy;
+
+    /**
+     * 创建时间
+     */
+    private Long createTime;
+
+    private String qrtzInstance;
+
+    /**
+     * 同步状态
+     */
+    private String syncStatus;
+
+    /**
+     * 最后同步时间
+     */
+    private Long lastUpdateTime;
+
+}
--- sdk/api/api-base/src/main/java/io/dataease/api/dataset/vo/DatasetTreeNodeVO.java
+++ sdk/api/api-base/src/main/java/io/dataease/api/dataset/vo/DatasetTreeNodeVO.java
@@ -1,15 +1,15 @@
 package io.dataease.api.dataset.vo;
 
+import io.dataease.api.dataset.dto.DatasetNodeDTO;
+import io.dataease.model.ITreeBase;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
-public class DatasetTreeNodeVO implements Serializable {
+public class DatasetTreeNodeVO extends DatasetNodeDTO implements Serializable, ITreeBase<DatasetTreeNodeVO> {
 
-    private Long id;
+    private List<DatasetTreeNodeVO> children;
 
-    private String name;
-
-    private String type;
 }
