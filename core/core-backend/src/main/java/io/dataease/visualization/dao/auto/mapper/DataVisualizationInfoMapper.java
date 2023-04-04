036defca75c0af515008054e0d26ac1472de20b1
ziyujiahao <1522128093@qq.com>
2023-04-04 10:06:29 +0800

    refactor: 数据大屏基础逻辑调整

---
+++ core/core-backend/src/main/java/io/dataease/visualization/dao/auto/mapper/DataVisualizationInfoMapper.java
@@ -0,0 +1,16 @@
+package io.dataease.visualization.dao.auto.mapper;
+
+import io.dataease.visualization.dao.auto.entity.DataVisualizationInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author fit2cloud
+ * @since 2023-04-04
+ */
+public interface DataVisualizationInfoMapper extends BaseMapper<DataVisualizationInfo> {
+
+}
