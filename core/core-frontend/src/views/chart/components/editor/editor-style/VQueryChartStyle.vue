3f363df3c114d7bd3040c7e23fd2c2cfd1b0b0f8
dataeaseShu <106045316+dataeaseShu@users.noreply.github.com>
2023-06-30 17:51:17 +0800

    feat: 查询组件样式配置

---
+++ core/core-frontend/src/views/chart/components/editor/editor-style/VQueryChartStyle.vue
@@ -0,0 +1,163 @@
+<script lang="tsx" setup>
+import { useI18n } from '@/hooks/web/useI18n'
+import { toRefs } from 'vue'
+import { COLOR_PANEL } from '@/views/chart/components/editor/util/chart'
+import CollapseSwitchItem from '@/components/collapse-switch-item/src/CollapseSwitchItem.vue'
+const { t } = useI18n()
+
+const state = {
+  attrActiveNames: [],
+  styleActiveNames: []
+}
+
+const props = defineProps({
+  chart: {
+    type: Object,
+    required: true
+  }
+})
+const predefineColors = COLOR_PANEL
+
+const { chart } = toRefs(props)
+// const chart = reactive({
+//   customStyle: {
+//     component: {
+//       show: false,
+//       titleShow: false,
+//       borderShow: false,
+//       borderColor: '',
+//       title: '',
+//       borderWidth: 1,
+//       bgColorShow: false,
+//       bgColor: '',
+//       btnList: []
+//     }
+//   }
+// })
+const emit = defineEmits(['onChangeComponentForm'])
+const onChangeComponentForm = val => {
+  emit('onChangeComponentForm', val)
+}
+</script>
+
+<template>
+  <el-row class="view-panel">
+    <div class="attr-style">
+      <el-row class="query-collapse-style">
+        <el-collapse v-model="state.styleActiveNames" class="style-collapse">
+          <collapse-switch-item
+            v-model="chart.customStyle.component.show"
+            :change-model="chart.customStyle.component"
+            @modelChange="onChangeComponentForm"
+            name="component"
+            :title="t('visualization.module')"
+          >
+            <el-checkbox v-model="chart.customStyle.component.titleShow">{{
+              t('chart.show') + t('chart.title')
+            }}</el-checkbox>
+            <div class="query-collapse-item">
+              <el-input
+                :disabled="!chart.customStyle.component.titleShow"
+                v-model="chart.customStyle.component.title"
+              />
+            </div>
+            <el-checkbox v-model="chart.customStyle.component.borderShow">{{
+              t('visualization.board')
+            }}</el-checkbox>
+            <div class="query-collapse-item">
+              <el-color-picker
+                v-model="chart.customStyle.component.borderColor"
+                :disabled="!chart.customStyle.component.borderShow"
+                :predefine="predefineColors"
+              />
+              <el-input-number
+                v-model="chart.customStyle.component.borderWidth"
+                :min="0"
+                :max="100"
+                :disabled="!chart.customStyle.component.borderShow"
+                controls-position="right"
+              />
+            </div>
+            <el-checkbox v-model="chart.customStyle.component.bgColorShow">{{
+              t('chart.custom_case') + t('chart.backgroundColor')
+            }}</el-checkbox>
+            <div class="query-collapse-item">
+              <el-color-picker
+                v-model="chart.customStyle.component.bgColor"
+                :disabled="!chart.customStyle.component.bgColorShow"
+                :predefine="predefineColors"
+              />
+            </div>
+            <el-divider />
+            <span> 展示按钮 </span>
+            <div class="query-collapse-item query-component">
+              <el-checkbox-group v-model="chart.customStyle.component.btnList">
+                <el-checkbox label="query"> {{ t('commons.adv_search.search') }}</el-checkbox>
+                <el-checkbox label="clear"> {{ t('commons.clear') }}</el-checkbox>
+                <el-checkbox label="reset">{{ t('commons.adv_search.reset') }}</el-checkbox>
+              </el-checkbox-group>
+            </div>
+            <span>
+              {{ t('chart.label_position') }}
+            </span>
+            <div class="query-collapse-item query-component">
+              <el-button text>
+                <template #icon>
+                  <icon name="icon_collect_filled"></icon>
+                </template>
+              </el-button>
+              <el-button text>
+                <template #icon>
+                  <icon name="icon_collect_filled"></icon>
+                </template>
+              </el-button>
+            </div>
+          </collapse-switch-item>
+        </el-collapse>
+      </el-row>
+    </div>
+  </el-row>
+</template>
+
+<style lang="less" scoped>
+.view-panel {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  border-top: 1px solid @side-outline-border-color;
+}
+
+.attr-style {
+  overflow-y: auto;
+  height: 100%;
+  width: 100%;
+
+  .query-collapse-style {
+    :deep(.ed-collapse-item__content) {
+      padding: 8px 16px;
+      .query-collapse-item {
+        padding: 8px 8px 0 22px;
+        display: flex;
+        align-items: center;
+        margin-bottom: 16px;
+
+        .ed-input-number {
+          margin-left: 8px;
+        }
+
+        .ed-color-picker__mask {
+          height: 30px;
+          width: 30px;
+        }
+      }
+
+      .query-component {
+        padding: 8px 0 0 0;
+        .ed-checkbox {
+          margin-right: 16px;
+        }
+      }
+    }
+  }
+}
+</style>
