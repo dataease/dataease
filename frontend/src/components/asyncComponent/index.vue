2554614eef1498331508330d29522a3737c82a04
dataeaseShu <106045316+dataeaseShu@users.noreply.github.com>
2023-07-05 11:53:36 +0800

    fix: 符号地图内存泄漏

--- frontend/src/components/asyncComponent/index.vue
+++ frontend/src/components/asyncComponent/index.vue
@@ -47,19 +47,24 @@ export default {
           // window.SyncComponentCache[this.url] = Axios.get(this.url)
           res = await window.SyncComponentCache[this.url]
         } else {
-          res = await window.SyncComponentCache[this.url]
+          this.mode = await window.SyncComponentCache[this.url]
+          return
         }
         if (res) {
           const Fn = Function
           const dynamicCode = res.data || res
           const component = new Fn(`return ${dynamicCode}`)()
           this.mode = component.default || component
+          window.SyncComponentCache[this.url] = this.mode
         }
       }
     }
   },
   created() {
     this.refId = uuid.v1
+  },
+  beforeDestroy() {
+    this.mode = null
   },
   methods: {
     /* chartResize() {
