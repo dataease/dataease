dca79b519e39193e9798e7c853e67fe81533fd24
junjun <junjie.xia@fit2cloud.com>
2023-07-27 10:56:25 +0800

    refactor: 视图color格式转换

--- core/core-frontend/src/views/chart/components/js/util.ts
+++ core/core-frontend/src/views/chart/components/js/util.ts
@@ -6,25 +6,31 @@ import { useMapStoreWithOut } from '@/store/modules/map'
 import { getGeoJson } from '@/api/map'
 import { toRaw } from 'vue'
 
+// 同时支持将hex和rgb，转换成rgba
 export function hexColorToRGBA(hex, alpha) {
-  const rgb = [] // 定义rgb数组
-  if (/^\#[0-9A-F]{3}$/i.test(hex)) {
-    // 判断传入是否为#三位十六进制数
-    let sixHex = '#'
-    hex.replace(/[0-9A-F]/gi, function (kw) {
-      sixHex += kw + kw // 把三位16进制数转化为六位
-    })
-    hex = sixHex // 保存回hex
-  }
-  if (/^#[0-9A-F]{6}$/i.test(hex)) {
-    // 判断传入是否为#六位十六进制数
-    hex.replace(/[0-9A-F]{2}/gi, function (kw) {
-      // eslint-disable-next-line no-eval
-      rgb.push(eval('0x' + kw)) // 十六进制转化为十进制并存如数组
-    })
-    return `rgba(${rgb.join(',')},${alpha / 100})` // 输出RGB格式颜色
+  let rgb = [] // 定义rgb数组
+  if (hex.indexOf('#') > -1) {
+    if (/^\#[0-9A-F]{3}$/i.test(hex)) {
+      // 判断传入是否为#三位十六进制数
+      let sixHex = '#'
+      hex.replace(/[0-9A-F]/gi, function (kw) {
+        sixHex += kw + kw // 把三位16进制数转化为六位
+      })
+      hex = sixHex // 保存回hex
+    }
+    if (/^#[0-9A-F]{6}$/i.test(hex)) {
+      // 判断传入是否为#六位十六进制数
+      hex.replace(/[0-9A-F]{2}/gi, function (kw) {
+        // eslint-disable-next-line no-eval
+        rgb.push(eval('0x' + kw)) // 十六进制转化为十进制并存如数组
+      })
+      return `rgba(${rgb.join(',')},${alpha / 100})` // 输出RGB格式颜色
+    } else {
+      return 'rgb(0,0,0)'
+    }
   } else {
-    return 'rgb(0,0,0)'
+    rgb = hex.match(/\d+/g)
+    return `rgba(${rgb.join(',')},${alpha / 100})`
   }
 }
 
