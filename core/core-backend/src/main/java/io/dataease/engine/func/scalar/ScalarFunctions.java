ee9d2412e8d70290e11968f5f63f1373a93b785a
junjun <junjie.xia@fit2cloud.com>
2023-08-03 13:51:31 +0800

    feat: calcite date function

--- core/core-backend/src/main/java/io/dataease/engine/func/scalar/ScalarFunctions.java
+++ core/core-backend/src/main/java/io/dataease/engine/func/scalar/ScalarFunctions.java
@@ -9,39 +9,55 @@ import java.util.Date;
 public class ScalarFunctions {
     public static String format = "yyyy-MM-dd HH:mm:ss";
 
-    public static String date_format(String date, String format) throws Exception {
-        if (StringUtils.isEmpty(date)) {
+    public static String date_format(String date, String format) {
+        try {
+            if (StringUtils.isEmpty(date)) {
+                return null;
+            }
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+            Date parse = simpleDateFormat.parse(date);
+            return simpleDateFormat.format(parse);
+        } catch (Exception e) {
             return null;
         }
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
-        Date parse = simpleDateFormat.parse(date);
-        return simpleDateFormat.format(parse);
     }
 
-    public static String str_to_date(String date, String format) throws Exception {
-        if (StringUtils.isEmpty(date)) {
+    public static String str_to_date(String date, String format) {
+        try {
+            if (StringUtils.isEmpty(date)) {
+                return null;
+            }
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+            Date parse = simpleDateFormat.parse(date);
+            return simpleDateFormat.format(parse);
+        } catch (Exception e) {
             return null;
         }
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
-        Date parse = simpleDateFormat.parse(date);
-        return simpleDateFormat.format(parse);
     }
 
-    public static Long unix_timestamp(String date) throws Exception {
-        if (StringUtils.isEmpty(date)) {
+    public static Long unix_timestamp(String date) {
+        try {
+            if (StringUtils.isEmpty(date)) {
+                return null;
+            }
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+            Date parse = simpleDateFormat.parse(date);
+            return parse.getTime();
+        } catch (Exception e) {
             return null;
         }
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
-        Date parse = simpleDateFormat.parse(date);
-        return parse.getTime();
     }
 
-    public static String from_unixtime(Long timestamp, String format) throws Exception {
-        if (ObjectUtils.isEmpty(timestamp)) {
+    public static String from_unixtime(Long timestamp, String format) {
+        try {
+            if (ObjectUtils.isEmpty(timestamp)) {
+                return null;
+            }
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+            Date date = new Date(timestamp);
+            return simpleDateFormat.format(date);
+        } catch (Exception e) {
             return null;
         }
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
-        Date date = new Date(timestamp);
-        return simpleDateFormat.format(date);
     }
 }
