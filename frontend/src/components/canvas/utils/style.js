fb6ca8accd572b79eb4f37c4f6f150f99ff2f704
ulleo <ulleo.wang@gmail.com>
2023-08-16 17:29:22 +0800

    feat: 支持ECharts排序柱状图插件 slider字体颜色与大小修改

--- frontend/src/components/canvas/utils/style.js
+++ frontend/src/components/canvas/utils/style.js
@@ -127,7 +127,10 @@ export const customAttrTrans = {
   ],
   'tooltip': {
     'textStyle': ['fontSize']
-  }
+  },
+  'slider': [
+    'fontSize'
+  ]
 }
 export const customStyleTrans = {
   'text': ['fontSize'],
@@ -375,7 +378,7 @@ export function adaptCurTheme(customStyle, customAttr, chartType) {
   }
 }
 
-export function adaptCurThemeCommonStyle(component,adaptFrom = 'them') {
+export function adaptCurThemeCommonStyle(component, adaptFrom = 'them') {
   const commonStyle = store.state.canvasStyleData.chartInfo.chartCommonStyle
   for (const key in commonStyle) {
     Vue.set(component.commonBackground, key, commonStyle[key])
@@ -383,9 +386,9 @@ export function adaptCurThemeCommonStyle(component,adaptFrom = 'them') {
   if (isFilterComponent(component.component)) {
     const filterStyle = store.state.canvasStyleData.chartInfo.filterStyle
     for (const styleKey in filterStyle) {
-      if(adaptFrom === 'copy'){
+      if (adaptFrom === 'copy') {
         Vue.set(component.style, styleKey, filterStyle[styleKey])
-      }else if (adaptFrom === 'them' && styleKey !== 'horizontal' && styleKey !== 'vertical') {
+      } else if (adaptFrom === 'them' && styleKey !== 'horizontal' && styleKey !== 'vertical') {
         // 主题变化位置属性不修改
         Vue.set(component.style, styleKey, filterStyle[styleKey])
       }
