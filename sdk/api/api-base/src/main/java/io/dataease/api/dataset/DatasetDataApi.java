5b49f5cc6366e88542275af42469720d13e24ac3
junjun <junjie.xia@fit2cloud.com>
2023-08-18 16:36:00 +0800

    feat: 数据集权限

--- sdk/api/api-base/src/main/java/io/dataease/api/dataset/DatasetDataApi.java
+++ sdk/api/api-base/src/main/java/io/dataease/api/dataset/DatasetDataApi.java
@@ -3,6 +3,7 @@ package io.dataease.api.dataset;
 import io.dataease.api.dataset.dto.DatasetTableDTO;
 import io.dataease.api.dataset.dto.PreviewSqlDTO;
 import io.dataease.api.dataset.union.DatasetGroupInfoDTO;
+import io.dataease.auth.DePermit;
 import io.dataease.dto.dataset.DatasetTableFieldDTO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +15,11 @@ import java.util.Map;
  * @Author Junjun
  */
 public interface DatasetDataApi {
+    @DePermit({"m:read"})
     @PostMapping("previewData")
     Map<String, Object> previewData(@RequestBody DatasetGroupInfoDTO datasetGroupInfoDTO) throws Exception;
 
+    @DePermit({"m:read"})
     @PostMapping("tableField")
     List<DatasetTableFieldDTO> tableField(@RequestBody DatasetTableDTO datasetTableDTO) throws Exception;
 
