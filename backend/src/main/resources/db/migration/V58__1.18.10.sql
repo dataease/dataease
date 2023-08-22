a9d7e5d1561fb8a11c41ca183eb18c62253477e3
xuwei-fit2cloud <wei@fit2cloud.com>
2023-08-22 10:16:09 +0800

    feat: 添加动态排序图flyway

--- backend/src/main/resources/db/migration/V58__1.18.10.sql
+++ backend/src/main/resources/db/migration/V58__1.18.10.sql
@@ -6,3 +6,5 @@ where `plugin_id` > 0
 INSERT INTO `my_plugin`(`name`, `store`, `free`, `cost`, `category`, `descript`, `version`, `install_type`, `creator`, `load_mybatis`, `release_time`, `install_time`, `module_name`, `icon`, `ds_type`) VALUES ('桑基图插件', 'default', 0, 0, 'view', 'AntV G2Plot 桑基图插件', '1.18.10', NULL, 'DATAEASE', 0, NULL, 1691046891296, 'view-sankey-backend', NULL, NULL);
 
 INSERT INTO `my_plugin`(`name`, `store`, `free`, `cost`, `category`, `descript`, `version`, `install_type`, `creator`, `load_mybatis`, `release_time`, `install_time`, `module_name`, `icon`, `ds_type`) VALUES ('AntV 组合图插件', 'default', 0, 0, 'view', 'AntV G2Plot 组合图插件', '1.18.10', NULL, 'DATAEASE', 0, NULL, 1691046891296, 'view-chartmix-backend', NULL, NULL);
+
+INSERT INTO `my_plugin`(`name`, `store`, `free`, `cost`, `category`, `descript`, `version`, `install_type`, `creator`, `load_mybatis`, `release_time`, `install_time`, `module_name`, `icon`, `ds_type`) VALUES ('ECharts 动态排序图插件', 'default', 0, 0, 'view', 'ECharts 动态排序图插件', '1.18.10', NULL, 'DATAEASE', 0, NULL, 1691046891296, 'view-racebar-backend', NULL, NULL);
