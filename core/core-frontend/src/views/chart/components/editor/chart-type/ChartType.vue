fecd25b306e3af4a1c08c93a0d067f1c9512027f
junjun <junjie.xia@fit2cloud.com>
2023-09-13 15:09:32 +0800

    refactor: 图表icon样式

--- core/core-frontend/src/views/chart/components/editor/chart-type/ChartType.vue
+++ core/core-frontend/src/views/chart/components/editor/chart-type/ChartType.vue
@@ -196,12 +196,15 @@ const groupActiveChange = category => {
     background: transparent;
     border-radius: 4px;
     cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     &:hover {
       border: 1px solid #3370ff;
     }
     .item-top-icon {
-      width: 86px;
-      height: 62px;
+      width: 80px;
+      height: 56px;
       color: @canvas-main-font-color;
     }
   }
