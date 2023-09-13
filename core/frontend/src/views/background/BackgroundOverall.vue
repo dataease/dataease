826513053146721a2b3e09a9c9d3ea41f8f10569
wangjiahao <1522128093@qq.com>
2023-09-13 18:20:55 +0800

    fix: 加强图片校验，防止恶意代码通过合成到图片上传到服务器中引起的攻击行为

--- core/frontend/src/views/background/BackgroundOverall.vue
+++ core/frontend/src/views/background/BackgroundOverall.vue
@@ -110,7 +110,7 @@
           <el-col style="width: 130px!important;">
             <el-upload
               action=""
-              accept=".jpeg,.jpg,.png,.gif,.svg"
+              accept=".jpeg,.jpg,.png,.gif"
               class="avatar-uploader"
               list-type="picture-card"
               :class="{disabled:uploadDisabled}"
