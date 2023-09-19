01a466691269f62038e8098614127a0ced900c91
taojinlong <jinlong@fit2cloud.com>
2023-09-19 11:39:39 +0800

    fix: sql 变量解析

--- core/core-frontend/src/locales/zh-CN.ts
+++ core/core-frontend/src/locales/zh-CN.ts
@@ -1078,7 +1078,7 @@ export default {
     select_year: '选择年',
     sql_variable_limit_1: '1、SQL 变量只能在 WHERE 条件中使用',
     sql_variable_limit_2:
-      "2、示例：select * from table_name where column_name1='${'{'}param_name1{'}'}' and column_name2 in ${'{'}param_name2{'}'}",
+      "2、示例：select * from table_name where column_name1='${'{'}param_name1{'}'}' and column_name2 in (${'{'}param_name2{'}'})",
     select_month: '选择月',
     select_date: '选择日期',
     select_time: '选择时间',
