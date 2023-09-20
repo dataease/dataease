cbd5c41a72db19cb3dd16a8d34fec1dcc3b116e0
ulleo <ulleo.wang@gmail.com>
2023-09-20 18:30:27 +0800

    feat: 仪表板/数据大屏>高级 功能设置样式调整

--- core/core-frontend/src/views/chart/components/editor/editor-senior/components/FunctionCfg.vue
+++ core/core-frontend/src/views/chart/components/editor/editor-senior/components/FunctionCfg.vue
@@ -1,5 +1,5 @@
 <script lang="tsx" setup>
-import { computed, PropType, reactive, ref, watch } from 'vue'
+import { computed, onMounted, PropType, reactive, ref, watch } from 'vue'
 import { useI18n } from '@/hooks/web/useI18n'
 import { COLOR_PANEL, DEFAULT_FUNCTION_CFG } from '@/views/chart/components/editor/util/chart'
 import { equalsAny, includesAny } from '../../util/StringUtils'
@@ -86,102 +86,119 @@ const initFieldCtrl = () => {
     })
   }
 }
-init()
+onMounted(() => {
+  init()
+})
 </script>
 
 <template>
   <div @keydown.stop @keyup.stop style="width: 100%">
-    <el-col>
-      <el-form ref="functionForm" :model="state.functionForm" label-width="80px" size="small">
-        <div v-show="showProperty('slider')">
-          <el-form-item :label="t('chart.slider')" class="form-item">
-            <el-checkbox
-              size="small"
-              v-model="state.functionForm.sliderShow"
+    <el-form ref="functionForm" :model="state.functionForm" size="small" label-position="top">
+      <div v-if="showProperty('slider')">
+        <el-form-item :label="t('chart.slider')" class="form-item form-item-checkbox">
+          <el-checkbox
+            :effect="themes"
+            size="small"
+            v-model="state.functionForm.sliderShow"
+            @change="changeFunctionCfg"
+          >
+            {{ t('chart.show') }}
+          </el-checkbox>
+        </el-form-item>
+        <div style="padding-left: 22px">
+          <el-form-item
+            :label="t('chart.slider_range') + '(%)'"
+            class="form-item range-slider form-item-checkbox"
+          >
+            <el-slider
+              :effect="themes"
+              :disabled="!state.functionForm.sliderShow"
+              v-model="state.functionForm.sliderRange"
+              style="width: 90%"
+              :min="0"
+              :max="100"
+              input-size="small"
+              range
               @change="changeFunctionCfg"
-              >{{ t('chart.show') }}</el-checkbox
-            >
-          </el-form-item>
-          <div v-show="state.functionForm.sliderShow">
-            <el-form-item
-              :label="t('chart.slider_range') + '(%)'"
-              class="form-item form-item-slider"
-            >
-              <el-slider
-                v-model="state.functionForm.sliderRange"
-                style="width: 90%"
-                :min="0"
-                :max="100"
-                input-size="small"
-                range
-                @change="changeFunctionCfg"
-              />
-            </el-form-item>
-            <el-form-item :label="t('chart.slider_bg')" class="form-item">
-              <el-color-picker
-                v-model="state.functionForm.sliderBg"
-                class="color-picker-style"
-                :predefine="state.predefineColors"
-                @change="changeFunctionCfg"
-              />
-            </el-form-item>
-            <el-form-item :label="t('chart.slider_fill_bg')" class="form-item">
-              <el-color-picker
-                v-model="state.functionForm.sliderFillBg"
-                class="color-picker-style"
-                :predefine="state.predefineColors"
-                @change="changeFunctionCfg"
-              />
-            </el-form-item>
-            <el-form-item :label="t('chart.slider_text_color')" class="form-item">
-              <el-color-picker
-                v-model="state.functionForm.sliderTextColor"
-                class="color-picker-style"
-                :predefine="state.predefineColors"
-                @change="changeFunctionCfg"
-              />
-            </el-form-item>
-          </div>
-        </div>
-        <el-form-item
-          v-show="showProperty('emptyDataStrategy')"
-          :label="t('chart.empty_data_strategy')"
-          class="form-item"
-        >
-          <el-radio-group
-            v-model="state.functionForm.emptyDataStrategy"
-            @change="changeFunctionCfg"
-          >
-            <el-radio :effect="props.themes" :label="'breakLine'">{{
-              t('chart.break_line')
-            }}</el-radio>
-            <el-radio :effect="props.themes" :label="'setZero'">{{ t('chart.set_zero') }}</el-radio>
-            <el-radio v-show="showIgnoreOption" :effect="props.themes" :label="'ignoreData'">{{
-              t('chart.ignore_data')
-            }}</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item
-          v-show="showEmptyDataFieldCtrl"
-          :label="t('chart.empty_data_field_ctrl')"
-          class="form-item"
-        >
-          <el-select
-            v-model="state.functionForm.emptyDataFieldCtrl"
-            multiple
-            :effect="props.themes"
-            @change="changeFunctionCfg"
-          >
-            <el-option
-              v-for="option in fieldOptions"
-              :key="option.value"
-              :label="option.label"
-              :value="option.value"
             />
-          </el-select>
-        </el-form-item>
-      </el-form>
-    </el-col>
+          </el-form-item>
+          <el-form-item :label="t('chart.slider_bg')" class="form-item">
+            <el-color-picker
+              :effect="themes"
+              :disabled="!state.functionForm.sliderShow"
+              v-model="state.functionForm.sliderBg"
+              class="color-picker-style"
+              :predefine="state.predefineColors"
+              @change="changeFunctionCfg"
+              is-custom
+              :trigger-width="108"
+            />
+          </el-form-item>
+          <el-form-item :label="t('chart.slider_fill_bg')" class="form-item">
+            <el-color-picker
+              :effect="themes"
+              :disabled="!state.functionForm.sliderShow"
+              v-model="state.functionForm.sliderFillBg"
+              class="color-picker-style"
+              :predefine="state.predefineColors"
+              @change="changeFunctionCfg"
+              is-custom
+              :trigger-width="108"
+            />
+          </el-form-item>
+          <el-form-item :label="t('chart.slider_text_color')" class="form-item">
+            <el-color-picker
+              :effect="themes"
+              :disabled="!state.functionForm.sliderShow"
+              v-model="state.functionForm.sliderTextColor"
+              class="color-picker-style"
+              :predefine="state.predefineColors"
+              @change="changeFunctionCfg"
+              is-custom
+              :trigger-width="108"
+            />
+          </el-form-item>
+        </div>
+      </div>
+      <el-form-item
+        v-if="showProperty('emptyDataStrategy')"
+        :label="t('chart.empty_data_strategy')"
+        class="form-item"
+      >
+        <el-radio-group
+          :effect="themes"
+          v-model="state.functionForm.emptyDataStrategy"
+          @change="changeFunctionCfg"
+        >
+          <el-radio :effect="themes" :label="'breakLine'">
+            {{ t('chart.break_line') }}
+          </el-radio>
+          <el-radio :effect="themes" :label="'setZero'">{{ t('chart.set_zero') }}</el-radio>
+          <el-radio v-if="showIgnoreOption" :effect="themes" :label="'ignoreData'">
+            {{ t('chart.ignore_data') }}
+          </el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item
+        v-if="showEmptyDataFieldCtrl"
+        :label="t('chart.empty_data_field_ctrl')"
+        class="form-item"
+      >
+        <el-select
+          :effect="themes"
+          v-model="state.functionForm.emptyDataFieldCtrl"
+          multiple
+          @change="changeFunctionCfg"
+        >
+          <el-option
+            v-for="option in fieldOptions"
+            :key="option.value"
+            :label="option.label"
+            :value="option.value"
+          />
+        </el-select>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -194,21 +211,12 @@ init()
   justify-content: space-between;
   align-items: center;
 }
-.form-item-slider :deep(.ed-form-item__label) {
-  font-size: 12px;
-  line-height: 38px;
-}
-.form-item :deep(.ed-form-item__label) {
-  font-size: 12px;
-}
+
 .ed-select-dropdown__item {
   padding: 0 20px;
 }
 span {
   font-size: 12px;
-}
-.ed-form-item {
-  margin-bottom: 6px;
 }
 
 .switch-style {
@@ -228,5 +236,17 @@ span {
   label {
     line-height: 28px;
   }
+}
+.range-slider {
+  :deep(.ed-form-item__content) {
+    padding: 0 8px;
+  }
+  :deep(.ed-slider__button-wrapper) {
+    --ed-slider-button-wrapper-size: 36px;
+    --ed-slider-button-size: 16px;
+  }
+}
+.form-item-checkbox {
+  margin-bottom: 10px !important;
 }
 </style>
