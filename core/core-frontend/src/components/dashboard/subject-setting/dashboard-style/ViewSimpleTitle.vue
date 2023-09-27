4c24353722606ec201b9eceabfe3eb8594e35292
ulleo <ulleo.wang@gmail.com>
2023-09-27 15:44:18 +0800

    feat: 去除仪表板设置>图表标题 垂直方向设置

--- core/core-frontend/src/components/dashboard/subject-setting/dashboard-style/ViewSimpleTitle.vue
+++ core/core-frontend/src/components/dashboard/subject-setting/dashboard-style/ViewSimpleTitle.vue
@@ -106,71 +106,17 @@
             </el-icon>
           </div>
         </el-tooltip>
-
-        <div class="m-divider"></div>
-
-        <el-tooltip effect="dark" placement="top">
-          <template #content>
-            {{ t('chart.text_pos_top') }}
-          </template>
-          <div
-            class="icon-btn"
-            :class="{ dark: themes === 'dark', active: titleForm.vPosition === 'top' }"
-            @click="setVPosition('top')"
-          >
-            <el-icon>
-              <Icon name="icon_top-align_outlined" />
-            </el-icon>
-          </div>
-        </el-tooltip>
-
-        <el-tooltip effect="dark" placement="top">
-          <template #content>
-            {{ t('chart.text_pos_center') }}
-          </template>
-          <div
-            class="icon-btn"
-            :class="{
-              dark: themes === 'dark',
-              active: titleForm.vPosition === 'center'
-            }"
-            @click="setVPosition('center')"
-          >
-            <el-icon>
-              <Icon name="icon_vertical-align_outlined" />
-            </el-icon>
-          </div>
-        </el-tooltip>
-
-        <el-tooltip effect="dark" placement="top">
-          <template #content>
-            {{ t('chart.text_pos_bottom') }}
-          </template>
-          <div
-            class="icon-btn"
-            :class="{
-              dark: themes === 'dark',
-              active: titleForm.vPosition === 'bottom'
-            }"
-            @click="setVPosition('bottom')"
-          >
-            <el-icon>
-              <Icon name="icon_bottom-align_outlined" />
-            </el-icon>
-          </div>
-        </el-tooltip>
       </el-space>
     </el-row>
   </el-row>
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted, reactive, ref } from 'vue'
+import { computed, reactive, ref } from 'vue'
 import { COLOR_PANEL } from '@/views/chart/components/editor/util/chart'
 import { dvMainStoreWithOut } from '@/store/modules/data-visualization/dvMain'
 import { useI18n } from '@/hooks/web/useI18n'
 import Icon from '@/components/icon-custom/src/Icon.vue'
-import { ElFormItem } from 'element-plus-secondary'
 const { t } = useI18n()
 const dvMainStore = dvMainStoreWithOut()
 
