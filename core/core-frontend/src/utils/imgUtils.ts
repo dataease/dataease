73b303348e9476eb6fa2a1965481d3dd2e1208ae
wangjiahao <1522128093@qq.com>
2023-10-12 18:06:46 +0800

    refactor: 压缩图片等优化

--- core/core-frontend/src/utils/imgUtils.ts
+++ core/core-frontend/src/utils/imgUtils.ts
@@ -1,10 +1,14 @@
 const basePath = import.meta.env.VITE_API_BASEPATH
 
 export function imgUrlTrans(url) {
-  const rawUrl = url
-    ? (basePath.endsWith('/') ? basePath.substring(0, basePath.length - 1) : basePath) + url
-    : null
-  return window.DataEaseBi
-    ? `${window.DataEaseBi.baseUrl}${rawUrl.startsWith('/api') ? rawUrl.slice(5) : rawUrl}`
-    : rawUrl
+  if (url && typeof url === 'string' && url.indexOf('static-resource') > -1) {
+    const rawUrl = url
+      ? (basePath.endsWith('/') ? basePath.substring(0, basePath.length - 1) : basePath) + url
+      : null
+    return window.DataEaseBi
+      ? `${window.DataEaseBi.baseUrl}${rawUrl.startsWith('/api') ? rawUrl.slice(5) : rawUrl}`
+      : rawUrl
+  } else {
+    return url
+  }
 }
