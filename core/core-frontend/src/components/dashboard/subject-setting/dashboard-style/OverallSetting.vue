21ba882c20dc5f4291a88665aa5909ed19498b8b
ulleo <ulleo.wang@gmail.com>
2023-10-18 18:16:30 +0800

    feat: 仪表板>整体配置>刷新频率 disable状态样式调整

--- core/core-frontend/src/components/dashboard/subject-setting/dashboard-style/OverallSetting.vue
+++ core/core-frontend/src/components/dashboard/subject-setting/dashboard-style/OverallSetting.vue
@@ -232,11 +232,6 @@ const colorButtonClick = val => {
     :deep(.ed-input-group__append) {
       padding: 0 20px;
     }
-    &.is-disabled {
-      :deep(.ed-input.is-disabled .ed-input__wrapper) {
-        box-shadow: 0 0 0 1px var(--ed-disabled-border-color) inset !important;
-      }
-    }
   }
   :deep(&.dv-dark) {
     .ed-input-group__append .ed-select {
