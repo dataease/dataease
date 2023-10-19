b16e9a2990ac6e015d7e8803362fcd84a2e1eaef
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2023-10-19 10:30:56 +0800

    perf: 第三方登录过渡页面背景

--- core/core-frontend/src/views/login/index.vue
+++ core/core-frontend/src/views/login/index.vue
@@ -15,7 +15,7 @@ import { XpackComponent } from '@/components/plugin'
 import { logoutHandler } from '@/utils/logout'
 import DeImage from '@/assets/login-desc-de.png'
 import elementResizeDetectorMaker from 'element-resize-detector'
-import PreheatImage from '@/assets/preheat.gif'
+import PreheatImage from '@/assets/preheat.png'
 const { wsCache } = useCache()
 const appStore = useAppStoreWithOut()
 const userStore = useUserStoreWithOut()
@@ -140,6 +140,16 @@ const cleanPlatformFlag = () => {
 }
 
 const preheat = ref(true)
+const svg = `
+        <path class="path" d="
+          M 30 15
+          L 28 17
+          M 25.61 25.61
+          A 15 15, 0, 0, 1, 15 30
+          A 15 15, 0, 1, 1, 27.99 7.5
+          L 15 15
+        " style="stroke-width: 4px; fill: rgba(0, 0, 0, 0)"/>
+      `
 
 onMounted(() => {
   checkPlatform()
@@ -164,7 +174,16 @@ onMounted(() => {
 </script>
 
 <template>
-  <div ref="loginContainer" v-if="preheat" class="preheat-container">
+  <div
+    v-if="preheat"
+    ref="loginContainer"
+    class="preheat-container"
+    v-loading="true"
+    element-loading-text="Loading..."
+    :element-loading-spinner="svg"
+    element-loading-svg-view-box="-10, -10, 50, 50"
+    element-loading-background="rgba(122, 122, 122, 0.8)"
+  >
     <el-image class="preheat-image" fit="cover" :src="PreheatImage" />
   </div>
   <div v-show="contentShow" class="login-background" v-loading="duringLogin">
@@ -306,8 +325,8 @@ onMounted(() => {
   position: absolute;
   z-index: 100;
   .preheat-image {
-    width: 50%;
-    height: 50%;
+    width: 98%;
+    height: 98%;
   }
 }
 .login-background {
