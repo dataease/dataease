3c78321c2879738ebdca4d8aa497b0e4b2ce2032
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2023-10-27 09:59:41 +0800

    feat: 增加参数设置

---
+++ core/core-frontend/src/views/system/parameter/map/Geometry.vue
@@ -0,0 +1,145 @@
+<template>
+  <el-container class="geometry-container">
+    <el-aside width="200px" class="geonetry-aside">
+      <div class="geo-title">
+        <span>{{ t('online_map.geometry') }}</span>
+        <span class="add-icon-span">
+          <el-icon>
+            <Icon name="icon_add_outlined"></Icon>
+          </el-icon>
+        </span>
+      </div>
+      <div class="geo-search">
+        <el-input class="m16 w100" v-model="keyworkd" clearable :placeholder="t('commons.search')">
+          <template #prefix>
+            <el-icon>
+              <Icon name="icon_search-outline_outlined"></Icon>
+            </el-icon>
+          </template>
+        </el-input>
+      </div>
+      <div class="map-tree-container">
+        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" />
+      </div>
+    </el-aside>
+    <el-main>地理信息内容区域</el-main>
+  </el-container>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { useI18n } from '@/hooks/web/useI18n'
+const { t } = useI18n()
+const keyworkd = ref('')
+
+interface Tree {
+  label: string
+  children?: Tree[]
+}
+
+const handleNodeClick = (data: Tree) => {
+  console.log(data)
+}
+
+const data: Tree[] = [
+  {
+    label: 'Level one 1',
+    children: [
+      {
+        label: 'Level two 1-1',
+        children: [
+          {
+            label: 'Level three 1-1-1'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    label: 'Level one 2',
+    children: [
+      {
+        label: 'Level two 2-1',
+        children: [
+          {
+            label: 'Level three 2-1-1'
+          }
+        ]
+      },
+      {
+        label: 'Level two 2-2',
+        children: [
+          {
+            label: 'Level three 2-2-1'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    label: 'Level one 3',
+    children: [
+      {
+        label: 'Level two 3-1',
+        children: [
+          {
+            label: 'Level three 3-1-1'
+          }
+        ]
+      },
+      {
+        label: 'Level two 3-2',
+        children: [
+          {
+            label: 'Level three 3-2-1'
+          }
+        ]
+      }
+    ]
+  }
+]
+
+const defaultProps = {
+  children: 'children',
+  label: 'label'
+}
+</script>
+
+<style lang="less" scoped>
+.geometry-container {
+  height: 100%;
+  .geonetry-aside {
+    width: 280px !important;
+    border-right: 1px solid #1f232926;
+    padding: 16px;
+    .geo-title {
+      display: flex;
+      justify-content: space-between;
+      height: 24px;
+      line-height: 24px;
+      span:first-child {
+        font-size: 16px;
+        font-weight: 500;
+        line-height: 24px;
+      }
+      .add-icon-span {
+        color: #3370ff;
+        height: 20px;
+        width: 20px;
+        i {
+          left: 2px;
+        }
+        &:hover {
+          background: #1f232926;
+          cursor: pointer;
+        }
+        border-radius: 2px;
+      }
+      margin-bottom: 16px;
+    }
+    .geo-search {
+      margin-bottom: 16px;
+    }
+  }
+}
+</style>
---
+++ core/core-frontend/src/views/system/parameter/map/OnlineMap.vue
@@ -0,0 +1,76 @@
+<template>
+  <el-container class="online-map-container">
+    <el-aside width="200px" class="online-map-aside">
+      <div class="geo-title">
+        <span>{{ t('online_map.onlinemap') }}</span>
+      </div>
+      <div class="online-form-item">
+        <div class="map-item">
+          <div class="map-item-label">
+            <span class="form-label">Key</span>
+          </div>
+        </div>
+        <div class="map-item">
+          <el-input v-model="key" />
+        </div>
+      </div>
+      <el-button type="primary" :disabled="!key" @click="saveHandler">{{
+        t('commons.save')
+      }}</el-button>
+    </el-aside>
+    <el-main>
+      <div class="de-map-container" :id="domId" />
+    </el-main>
+  </el-container>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { useI18n } from '@/hooks/web/useI18n'
+const { t } = useI18n()
+const key = ref('')
+
+const domId = ref('de-map-container')
+const saveHandler = () => {
+  console.log(111)
+}
+</script>
+
+<style lang="less" scoped>
+.de-map-container {
+  height: 100%;
+  width: 100%;
+}
+.online-map-container {
+  height: 100%;
+  .online-map-aside {
+    width: 280px !important;
+    border-right: 1px solid #1f232926;
+    padding: 16px;
+    .geo-title {
+      height: 24px;
+      line-height: 24px;
+      margin-bottom: 16px;
+      span:first-child {
+        font-size: 16px;
+        font-weight: 500;
+        line-height: 24px;
+      }
+    }
+    .online-form-item {
+      height: 64px;
+      margin-bottom: 16px;
+      .map-item {
+        height: 32px;
+        .map-item-label {
+          height: 22px;
+          line-height: 22px;
+          font-size: 14px;
+          font-weight: 400;
+          color: #1f2329;
+        }
+      }
+    }
+  }
+}
+</style>
