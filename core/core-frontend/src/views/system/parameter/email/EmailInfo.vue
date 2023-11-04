c4b64f8eccffef73dbbcce0277911153cfe489db
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2023-11-04 19:50:24 +0800

    feat: 系统参数前端页面

---
+++ core/core-frontend/src/views/system/parameter/email/EmailInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <InfoTemplate :label-tooltips="tooltips" setting-key="email" @edit="edit" />
+  <email-edit ref="editor" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import InfoTemplate from '../../common/InfoTemplate.vue'
+import EmailEdit from './EmailEdit.vue'
+const tooltips = [
+  {
+    key: '测试收件人',
+    val: '测试收件人xxxx'
+  },
+  {
+    key: 'SSL',
+    val: 'SSLxxxx'
+  },
+  {
+    key: 'TSL',
+    val: 'TSLxxxx'
+  }
+]
+
+const editor = ref()
+
+const edit = () => {
+  editor?.value.edit()
+}
+</script>
