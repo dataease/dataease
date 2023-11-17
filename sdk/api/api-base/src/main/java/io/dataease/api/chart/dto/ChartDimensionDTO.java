ba2b06334136775bf6b3b979b0a79149158987f0
junjun <junjie.xia@fit2cloud.com>
2023-11-17 16:05:12 +0800

    fix: id序列化

--- sdk/api/api-base/src/main/java/io/dataease/api/chart/dto/ChartDimensionDTO.java
+++ sdk/api/api-base/src/main/java/io/dataease/api/chart/dto/ChartDimensionDTO.java
@@ -1,5 +1,7 @@
 package io.dataease.api.chart.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 /**
@@ -7,6 +9,7 @@ import lombok.Data;
  */
 @Data
 public class ChartDimensionDTO {
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
     private String value;
 }
