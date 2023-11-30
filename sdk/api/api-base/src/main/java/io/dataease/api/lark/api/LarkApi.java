03c27f7e10fe95cf8710bc267e8f54738a1d81d0
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2023-11-30 17:04:26 +0800

    perf: 飞书设置页面重构

--- sdk/api/api-base/src/main/java/io/dataease/api/lark/api/LarkApi.java
+++ sdk/api/api-base/src/main/java/io/dataease/api/lark/api/LarkApi.java
@@ -1,5 +1,6 @@
 package io.dataease.api.lark.api;
 
+import io.dataease.api.lark.dto.LarkEnableEditor;
 import io.dataease.api.lark.dto.LarkTokenRequest;
 import io.dataease.api.lark.vo.LarkInfoVO;
 import io.dataease.api.lark.dto.LarkSettingCreator;
@@ -17,5 +18,8 @@ public interface LarkApi {
 
     @PostMapping("/token")
     String larkToken(@RequestBody LarkTokenRequest request);
+
+    @PostMapping("/switchEnable")
+    void switchEnable(@RequestBody LarkEnableEditor editor);
 
 }
