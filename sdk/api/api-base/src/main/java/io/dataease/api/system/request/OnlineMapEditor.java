5c009d5473581beb112a7edd8f9fe190e803cc13
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2023-12-20 17:55:37 +0800

    perf: 系统设置api接口文档

--- sdk/api/api-base/src/main/java/io/dataease/api/system/request/OnlineMapEditor.java
+++ sdk/api/api-base/src/main/java/io/dataease/api/system/request/OnlineMapEditor.java
@@ -1,11 +1,13 @@
 package io.dataease.api.system.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.io.Serializable;
 
+@Schema(description = "在线地图构造器")
 @Data
 public class OnlineMapEditor implements Serializable {
-
+    @Schema(description = "在线地图key", requiredMode = Schema.RequiredMode.REQUIRED)
     private String key;
 }
