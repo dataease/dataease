bbbc249a2ed6c1f37bf309e62cee34863a07f2be
junjun <junjie.xia@fit2cloud.com>
2023-12-21 11:15:27 +0800

    feat: 数据集视图接口api

--- sdk/api/api-base/src/main/java/io/dataease/api/dataset/DatasetTableApi.java
+++ sdk/api/api-base/src/main/java/io/dataease/api/dataset/DatasetTableApi.java
@@ -4,6 +4,7 @@ import com.github.xiaoymin.knife4j.annotations.ApiSupport;
 import io.dataease.api.dataset.dto.MultFieldValuesRequest;
 import io.dataease.api.dataset.engine.SQLFunctionDTO;
 import io.dataease.dto.dataset.DatasetTableFieldDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,31 +28,39 @@ public interface DatasetTableApi {
      * @return
      * @throws Exception
      */
+    @Operation(summary = "保存字段")
     @PostMapping("save")
     DatasetTableFieldDTO save(@RequestBody DatasetTableFieldDTO datasetTableFieldDTO) throws Exception;
 
+    @Operation(summary = "查询字段")
     @PostMapping("get/{id}")
     DatasetTableFieldDTO get(@PathVariable Long id);
 
+    @Operation(summary = "获取数据集字段")
     @PostMapping("listByDatasetGroup/{id}")
     List<DatasetTableFieldDTO> listByDatasetGroup(@PathVariable Long id);
 
+    @Operation(summary = "删除字段")
     @PostMapping("delete/{id}")
     void delete(@PathVariable Long id);
 
+    @Operation(summary = "获取字段分组")
     @PostMapping("listByDQ/{id}")
     Map<String, List<DatasetTableFieldDTO>> listByDQ(@PathVariable Long id);
 
-    @GetMapping ("listWithPermissions/{id}")
+    @Operation(summary = "获取字段")
+    @GetMapping("listWithPermissions/{id}")
     List<DatasetTableFieldDTO> listFieldsWithPermissions(@PathVariable Long id);
 
-
+    @Operation(summary = "获取枚举值")
     @PostMapping("multFieldValuesForPermissions")
     List<String> multFieldValuesForPermissions(@RequestBody MultFieldValuesRequest multFieldValuesRequest) throws Exception;
 
+    @Operation(summary = "获取计算字段函数")
     @PostMapping("getFunction")
     List<SQLFunctionDTO> getFunction();
 
+    @Operation(summary = "删除视图计算字段", hidden = true)
     @PostMapping("deleteByChartId/{id}")
     void deleteByChartId(@PathVariable Long id);
 }
