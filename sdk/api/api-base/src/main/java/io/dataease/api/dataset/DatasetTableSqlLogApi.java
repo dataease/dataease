e5308b07382783c5806b87dd1afd52a3cfb76490
junjun <junjie.xia@fit2cloud.com>
2024-01-10 10:26:12 +0800

    feat: 数据集视图接口api

--- sdk/api/api-base/src/main/java/io/dataease/api/dataset/DatasetTableSqlLogApi.java
+++ sdk/api/api-base/src/main/java/io/dataease/api/dataset/DatasetTableSqlLogApi.java
@@ -2,6 +2,7 @@ package io.dataease.api.dataset;
 
 import com.github.xiaoymin.knife4j.annotations.ApiSupport;
 import io.dataease.api.dataset.dto.SqlLogDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,12 +16,15 @@ import java.util.List;
 @Tag(name = "数据集管理:SQL日志")
 @ApiSupport(order = 977)
 public interface DatasetTableSqlLogApi {
+    @Operation(summary = "保存")
     @PostMapping("save")
     void save(@RequestBody SqlLogDTO sqlLogDTO) throws Exception;
 
+    @Operation(summary = "查看SQL片段执行记录")
     @PostMapping("listByTableId")
     List<SqlLogDTO> listByTableId(@RequestBody SqlLogDTO sqlLogDTO) throws Exception;
 
+    @Operation(summary = "删除日志")
     @PostMapping("deleteByTableId/{id}")
     void deleteByTableId(@PathVariable String id) throws Exception;
 }
