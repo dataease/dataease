214fb7ca687842cb7bd917367f0a29d9abef5161
zhaoqian <qian.zhao@fit2cloud.com>
2024-01-12 15:31:12 +0800

    fix：修复工作台模板中心查看全部时无模板展示问题

--- core/core-backend/src/main/resources/db/desktop/V2.3__ddl.sql
+++ core/core-backend/src/main/resources/db/desktop/V2.3__ddl.sql
@@ -12,3 +12,7 @@ CREATE TABLE `visualization_watermark` (
 INSERT INTO `visualization_watermark` (`id`, `version`, `setting_content`, `create_by`, `create_time`) VALUES ('system_default', '1.0', '{\"enable\":false,\"enablePanelCustom\":true,\"type\":\"custom\",\"content\":\"水印\",\"watermark_color\":\"#DD1010\",\"watermark_x_space\":12,\"watermark_y_space\":36,\"watermark_fontsize\":15}', 'admin', NULL);
 
 INSERT INTO `core_sys_setting` (`id`, `pkey`, `pval`, `type`, `sort`) VALUES (9, 'basic.frontTimeOut', '60', 'text', 1);
+
+ALTER TABLE `core_opt_recent` CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+ALTER TABLE `core_opt_recent` MODIFY COLUMN `resource_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '资源名称' AFTER `resource_id`;
+
