5eefb53ef6105615339e115f818787045f8ba922
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-01-30 18:46:06 +0800

    feat(X-Pack): 日志保留时间

--- core/core-frontend/src/views/system/parameter/basic/BasicEdit.vue
+++ core/core-frontend/src/views/system/parameter/basic/BasicEdit.vue
@@ -102,6 +102,15 @@ const closeLoading = () => {
 const edit = list => {
   state.settingList = list.map(item => {
     const pkey = item.pkey
+    if (pkey === 'basic.logLiveTime') {
+      rule[pkey.split('.')[1]] = [
+        {
+          required: true,
+          message: t('common.require'),
+          trigger: ['blur', 'change']
+        }
+      ]
+    }
     item['label'] = `setting_${pkey}`
     item['pkey'] = pkey.split('.')[1]
     let pval = item.pval
@@ -171,8 +180,21 @@ defineExpose({
             v-model="state.form.frontTimeOut"
             autocomplete="off"
             step-strictly
-            class="text-left"
+            class="text-left edit-all-line"
             :min="1"
+            :placeholder="t('common.inputText')"
+            controls-position="right"
+            type="number"
+          />
+        </div>
+        <div v-else-if="item.pkey === 'logLiveTime'">
+          <el-input-number
+            v-model="state.form[item.pkey]"
+            autocomplete="off"
+            step-strictly
+            class="text-left edit-all-line"
+            :min="1"
+            :max="4000"
             :placeholder="t('common.inputText')"
             controls-position="right"
             type="number"
@@ -214,6 +236,9 @@ defineExpose({
   }
   .is-error {
     margin-bottom: 40px !important;
+  }
+  .edit-all-line {
+    width: 552px !important;
   }
 }
 .setting-hidden-item {
--- sdk/common/src/main/java/io/dataease/constant/XpackSettingConstants.java
+++ sdk/common/src/main/java/io/dataease/constant/XpackSettingConstants.java
@@ -3,5 +3,6 @@ package io.dataease.constant;
 public class XpackSettingConstants {
 
     public static final String AUTO_CREATE_USER = "basic.autoCreateUser";
+    public static final String LOG_LIVE_TIME = "basic.logLiveTime";
     public static final String Front_Time_Out = "basic.frontTimeOut";
 }
