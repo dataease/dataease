950a4de34e413881a527c78812e12cf47bd0eb33
wangjiahao <1522128093@qq.com>
2024-02-20 14:11:31 +0800

    refactor(仪表板): 视图明细数据字体大小调整 #7729

--- core/frontend/src/components/canvas/customComponent/UserView.vue
+++ core/frontend/src/components/canvas/customComponent/UserView.vue
@@ -1111,8 +1111,8 @@ export default {
         tableChart.customAttr.color.tableFontColor = '#7c7e81'
         tableChart.customAttr.color.enableTableCrossBG = false
         tableChart.customAttr.size.showTableHeader = true
-        tableChart.customAttr.size.tableTitleFontSize = 12
-        tableChart.customAttr.size.tableItemFontSize = 12
+        tableChart.customAttr.size.tableTitleFontSize = 14
+        tableChart.customAttr.size.tableItemFontSize = 14
       }
       tableChart.customAttr.size.tableColumnFreezeHead = 0
       tableChart.customAttr.size.tableRowFreezeHead = 0
