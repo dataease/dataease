ac88bf9fefa040d5c0fb1ce8520491fdfd24f9a8
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-03-06 16:35:31 +0800

    fix: 定时报告统一错误提示

--- core/backend/src/main/java/io/dataease/service/chart/ViewExportExcel.java
+++ core/backend/src/main/java/io/dataease/service/chart/ViewExportExcel.java
@@ -1,14 +1,8 @@
 package io.dataease.service.chart;
 
-import com.google.gson.internal.LinkedTreeMap;
-import io.dataease.plugins.common.exception.DataEaseException;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.ObjectUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Service;
-
 import com.google.gson.Gson;
-
+import com.google.gson.internal.LinkedTreeMap;
+import com.google.gson.reflect.TypeToken;
 import io.dataease.auth.annotation.DePermissionProxy;
 import io.dataease.commons.model.excel.ExcelSheetModel;
 import io.dataease.commons.utils.ExcelUtils;
@@ -18,16 +12,19 @@ import io.dataease.dto.PermissionProxy;
 import io.dataease.dto.chart.ChartViewDTO;
 import io.dataease.dto.panel.PanelGroupDTO;
 import io.dataease.plugins.common.dto.chart.ChartViewFieldDTO;
+import io.dataease.plugins.common.exception.DataEaseException;
 import io.dataease.plugins.common.request.chart.ChartExtFilterRequest;
 import io.dataease.plugins.common.util.SpringContextUtil;
 import io.dataease.service.panel.PanelGroupService;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.lang.reflect.Type;
 import java.util.*;
 import java.util.stream.Collectors;
-
-import com.google.gson.reflect.TypeToken;
 
 @Service
 public class ViewExportExcel {
@@ -79,7 +76,7 @@ public class ViewExportExcel {
             chartExtRequest.setFilter(chartExtFilterRequests);
             chartExtRequest.setResultCount((int) resultCount);
             chartExtRequest.setResultMode(resultMode);
-            if(tableInfoViewIds.contains(entry.getKey())){
+            if (tableInfoViewIds.contains(entry.getKey())) {
                 chartExtRequest.setGoPage(1L);
                 chartExtRequest.setPageSize(1000000L);
                 chartExtRequest.setExcelExportFlag(true);
@@ -116,13 +113,14 @@ public class ViewExportExcel {
         List<ChartViewFieldDTO> fields = gson.fromJson(gson.toJson(objectFields), fieldTokenType);
         List<String> heads = new ArrayList<>();
         List<String> headKeys = new ArrayList<>();
-        fields.forEach(field -> {
-            if (ObjectUtils.isNotEmpty(field.getName()) && ObjectUtils.isNotEmpty(field.getDataeaseName())) {
-                heads.add(field.getName());
-                headKeys.add(field.getDataeaseName());
-            }
-        });
-
+        if (CollectionUtils.isNotEmpty(fields)) {
+            fields.forEach(field -> {
+                if (ObjectUtils.isNotEmpty(field.getName()) && ObjectUtils.isNotEmpty(field.getDataeaseName())) {
+                    heads.add(field.getName());
+                    headKeys.add(field.getDataeaseName());
+                }
+            });
+        }
         Object objectTableRow = chart.get("tableRow");
         List<Map<String, Object>> tableRow = (List<Map<String, Object>>) objectTableRow;
 
