0ade71cc0e287635e3b0c941e97674900aff5ef0
ulleo <ulleo.wang@gmail.com>
2024-03-28 16:01:25 +0800

    fix(视图): 修复AntV组合图插件维度/指标下拉菜单没有对齐问题

--- extensions/dataease-extensions-view/view-chartmix/view-chartmix-frontend/src/components/views/QuotaExtItem.vue
+++ extensions/dataease-extensions-view/view-chartmix/view-chartmix-frontend/src/components/views/QuotaExtItem.vue
@@ -33,7 +33,7 @@
           <el-dropdown-item v-show="chart.type ==='chart-mix'">
             <el-dropdown placement="right-start" size="mini" style="width: 100%" @command="switchChartType">
               <span class="el-dropdown-link inner-dropdown-menu">
-                <span>
+                <span class="inline-menu-text">
                   <i class="el-icon-s-data"/>
                   <span>{{ $t('chart.chart_type') }}</span>
                 </span>
@@ -49,7 +49,7 @@
           <el-dropdown-item v-show="chart.type !== 'table-info'" :divided="chart.type === 'chart-mix'">
             <el-dropdown placement="right-start" size="mini" style="width: 100%" @command="summary">
               <span class="el-dropdown-link inner-dropdown-menu">
-                <span>
+                <span class="inline-menu-text">
                   <i class="el-icon-notebook-2"/>
                   <span>{{ $t('chart.summary') }}</span>
                   <span class="summary-span-item">({{ $t('chart.' + item.summary) }})</span>
@@ -83,7 +83,7 @@
               @command="quickCalc"
             >
               <span class="el-dropdown-link inner-dropdown-menu">
-                <span>
+                <span class="inline-menu-text">
                   <i class="el-icon-s-grid"/>
                   <span>{{ $t('chart.quick_calc') }}</span>
                   <span class="summary-span-item">({{
@@ -114,7 +114,7 @@
               @command="sort"
             >
               <span class="el-dropdown-link inner-dropdown-menu">
-                <span>
+                <span class="inline-menu-text">
                   <i class="el-icon-sort"/>
                   <span>{{ $t('chart.sort') }}</span>
                   <span class="summary-span-item">({{ $t('chart.' + item.sort) }})</span>
@@ -390,6 +390,11 @@ span {
 .summary-span-item {
   margin-left: 4px;
   color: #878d9f;
+}
+.inline-menu-text {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 
 .summary-span {
