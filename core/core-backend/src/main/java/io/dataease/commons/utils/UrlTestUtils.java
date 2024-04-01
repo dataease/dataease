db4af7c39302db0e19d4ddf13189547284dbe0d0
wangjiahao <1522128093@qq.com>
2024-04-01 18:11:16 +0800

    refactor(工作台): 智能助手增加对话框放大功能

---
+++ core/core-backend/src/main/java/io/dataease/commons/utils/UrlTestUtils.java
@@ -0,0 +1,34 @@
+package io.dataease.commons.utils;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class UrlTestUtils {
+
+    public static boolean testUrlWithTimeOut(String urlString, int timeOutMillSeconds) {
+        try {
+            URL url = new URL(urlString);
+            URLConnection co = url.openConnection();
+            co.setConnectTimeout(timeOutMillSeconds);
+            co.connect();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean isURLAvailable(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD");
+            int responseCode = connection.getResponseCode();
+            return responseCode == HttpURLConnection.HTTP_OK;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+}
