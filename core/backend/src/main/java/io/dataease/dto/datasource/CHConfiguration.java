e96a2fb81915b73f0c96743834b412ce08bfb202
an5er <1318861442@qq.com>
2024-04-07 15:04:32 +0800

    fix: 限制 clickhouse 非法参数

--- core/backend/src/main/java/io/dataease/dto/datasource/CHConfiguration.java
+++ core/backend/src/main/java/io/dataease/dto/datasource/CHConfiguration.java
@@ -5,12 +5,17 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.lang3.StringUtils;
 
+import java.net.URLDecoder;
+import java.util.List;
+import java.util.Arrays;
+
 @Getter
 @Setter
 public class CHConfiguration extends JdbcConfiguration {
 
     private String driver = "ru.yandex.clickhouse.ClickHouseDriver";
     private String extraParams = "";
+    private List<String> illegalParameters = Arrays.asList("localFile");
 
     public String getJdbc() {
         if(StringUtils.isEmpty(extraParams.trim())){
@@ -19,12 +24,18 @@ public class CHConfiguration extends JdbcConfiguration {
                     .replace("PORT", getPort().toString().trim())
                     .replace("DATABASE", getDataBase().trim());
         }else {
+            for (String illegalParameter : getIllegalParameters()) {
+                if (getExtraParams().toLowerCase().contains(illegalParameter.toLowerCase()) ||
+                        URLDecoder.decode(getExtraParams()).toLowerCase().contains(illegalParameter.toLowerCase())) {
+                    throw new RuntimeException("Illegal parameter: " + illegalParameter);
+                }
+            }
+
             return "jdbc:clickhouse://HOSTNAME:PORT/DATABASE?EXTRA_PARAMS"
                     .replace("HOSTNAME", getHost().trim())
                     .replace("PORT", getPort().toString().trim())
                     .replace("DATABASE", getDataBase().trim())
                     .replace("EXTRA_PARAMS", getExtraParams().trim());
         }
-
     }
 }
