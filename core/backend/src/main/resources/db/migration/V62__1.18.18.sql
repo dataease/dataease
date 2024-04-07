eed822e46d7b7d0663cb29592f7eac0fa3cbfa85
taojinlong <jinlong@fit2cloud.com>
2024-04-07 15:30:36 +0800

    feat: 新增数据导出中心功能

--- core/backend/src/main/resources/db/migration/V62__1.18.18.sql
+++ core/backend/src/main/resources/db/migration/V62__1.18.18.sql
@@ -18,19 +18,3 @@ ALTER TABLE `panel_link_mapping`
     ADD COLUMN `require_ticket` tinyint(1) NOT NULL DEFAULT 0 AFTER `uuid`;
 
 INSERT INTO `system_parameter` (`param_key`, `param_value`, `type`, `sort`) VALUES ('ai.baseUrl', 'https://maxkb.fit2cloud.com/ui/chat/5baa787163381fa2', 'text', 100);
-
-
-DROP TABLE IF EXISTS `export_task`;
-CREATE TABLE `export_task` (
-   `id`            varchar(255)   NOT NULL,
-   `user_id`     bigint(20)       NOT NULL ,
-   `file_name` varchar(2048) DEFAULT NULL,
-   `file_size` DOUBLE DEFAULT NULL,
-   `export_from` varchar(255) DEFAULT NULL,
-   `export_status` varchar(255) DEFAULT NULL,
-   `export_from_type` varchar(255) DEFAULT NULL,
-   `export_time` bigint(20) DEFAULT NULL,
-   `export_pogress` varchar(255)  DEFAULT NULL,
-   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;
-
