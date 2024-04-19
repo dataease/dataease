0543cf35dab724aa43c22f74909fc07b0fa98fa8
dataeaseShu <tong.shu@fit2cloud.com>
2024-04-19 10:04:32 +0800

    feat(嵌入式): div嵌入支持新增仪表板和新增数据大屏

--- core/core-frontend/src/router/embedded.ts
+++ core/core-frontend/src/router/embedded.ts
@@ -2,7 +2,22 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import type { App } from 'vue'
 
-export const routes: AppRouteRecordRaw[] = []
+export const routes: AppRouteRecordRaw[] = [
+  {
+    path: '/dvCanvas',
+    name: 'dvCanvas',
+    hidden: true,
+    meta: {},
+    component: () => import('@/views/data-visualization/index.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    hidden: true,
+    meta: {},
+    component: () => import('@/views/dashboard/index.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
