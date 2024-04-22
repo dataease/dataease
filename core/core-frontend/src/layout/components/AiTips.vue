489560dc7572fbab5b93f7da3264822c33173c5a
wangjiahao <1522128093@qq.com>
2024-04-22 18:38:36 +0800

    refactor(工作台): 文案修改

--- core/core-frontend/src/layout/components/AiTips.vue
+++ core/core-frontend/src/layout/components/AiTips.vue
@@ -21,7 +21,9 @@ onMounted(() => {
   >
     <div class="ai-popper-tips-content">
       <p class="title">DataEase 智能客服</p>
-      <p class="constant">你好，卧室DatEase智能客服<br />点击一下开，启高效解答模式~<br />&nbsp;</p>
+      <p class="constant">
+        你好，我是DataEase智能客服<br />点击一下，开启高效解答模式~<br />&nbsp;
+      </p>
       <div class="bottom">
         <el-button size="middle" @click="confirm"> 我知道了 </el-button>
       </div>
