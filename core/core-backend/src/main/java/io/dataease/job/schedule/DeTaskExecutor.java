367961e67a2bb209a54dba63a8c634e0192089d0
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-05-06 18:02:28 +0800

    perf(X-Pack): 定时报告

--- core/core-backend/src/main/java/io/dataease/job/schedule/DeTaskExecutor.java
+++ core/core-backend/src/main/java/io/dataease/job/schedule/DeTaskExecutor.java
@@ -14,11 +14,18 @@ import java.util.Date;
 @Component
 public class DeTaskExecutor {
 
+    protected static final String IS_TEMP_TASK = "isTempTask";
+
     @Resource
     private ScheduleManager scheduleManager;
 
     @XpackInteract(value = "xpackTaskExecutor", replace = true)
     public boolean execute(Long taskId) {
+        return false;
+    }
+
+    @XpackInteract(value = "xpackTaskExecutor", replace = true)
+    public boolean executeTemplate(Long taskId) {
         return false;
     }
 
@@ -31,9 +38,27 @@ public class DeTaskExecutor {
         TriggerKey triggerKey = new TriggerKey(key, key);
         JobDataMap jobDataMap = new JobDataMap();
         jobDataMap.put("taskId", taskId);
+        jobDataMap.put(IS_TEMP_TASK, false);
         Date end = null;
         if (ObjectUtils.isNotEmpty(endTime)) end = new Date(endTime);
         scheduleManager.addOrUpdateCronJob(jobKey, triggerKey, DeXpackScheduleJob.class, cron, new Date(startTime), end, jobDataMap);
+    }
+
+    public void fireNow(Long taskId) throws Exception{
+        String key = taskId.toString();
+        JobKey jobKey = new JobKey(key, key);
+        scheduleManager.fireNow(jobKey);
+    }
+
+    public void addTempTask(Long taskId, Long startTime) {
+        String key = taskId.toString();
+        JobKey jobKey = new JobKey(key, key);
+        TriggerKey triggerKey = new TriggerKey(key, key);
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put(IS_TEMP_TASK, true);
+        String cron = CronUtils.cron();
+        jobDataMap.put("taskId", taskId);
+        scheduleManager.addOrUpdateCronJob(jobKey, triggerKey, DeXpackScheduleJob.class, cron, new Date(startTime), null, jobDataMap);
     }
 
     public void removeTask(Long taskId) {
