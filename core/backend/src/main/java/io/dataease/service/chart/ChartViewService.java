7150744379d584ed8c6e34d0e82403feccf2063d
junjun <junjie.xia@fit2cloud.com>
2024-05-10 10:28:46 +0800

    fix(视图): 修复同环比差值百分比分母没取绝对值的问题

--- core/backend/src/main/java/io/dataease/service/chart/ChartViewService.java
+++ core/backend/src/main/java/io/dataease/service/chart/ChartViewService.java
@@ -1430,7 +1430,7 @@ public class ChartViewService {
                                         item[dataIndex] = null;
                                     } else {
                                         item[dataIndex] = new BigDecimal(cValue)
-                                                .divide(new BigDecimal(lastValue), 8, RoundingMode.HALF_UP)
+                                                .divide(new BigDecimal(lastValue).abs(), 8, RoundingMode.HALF_UP)
                                                 .subtract(new BigDecimal(1))
                                                 .setScale(8, RoundingMode.HALF_UP)
                                                 .toString();
