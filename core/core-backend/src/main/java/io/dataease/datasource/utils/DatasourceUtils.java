d19476ad076b0b5fe041a902b2b14fcf7da37147
junjun <junjie.xia@fit2cloud.com>
2024-05-11 17:31:45 +0800

    fix(数据集): 修复数据源无效状态下，依旧查询数据没报错的问题

---
+++ core/core-backend/src/main/java/io/dataease/datasource/utils/DatasourceUtils.java
@@ -0,0 +1,26 @@
+package io.dataease.datasource.utils;
+
+import io.dataease.dataset.dto.DatasourceSchemaDTO;
+import io.dataease.exception.DEException;
+import io.dataease.i18n.Translator;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Map;
+
+/**
+ * @Author Junjun
+ */
+public class DatasourceUtils {
+    public static void checkDsStatus(Map<Long, DatasourceSchemaDTO> dsMap) {
+        if (ObjectUtils.isEmpty(dsMap)) {
+            DEException.throwException(Translator.get("i18n_datasource_delete"));
+        }
+        for (Map.Entry<Long, DatasourceSchemaDTO> next : dsMap.entrySet()) {
+            DatasourceSchemaDTO ds = next.getValue();
+            if (StringUtils.isNotEmpty(ds.getStatus()) && "Error".equalsIgnoreCase(ds.getStatus())) {
+                DEException.throwException(Translator.get("i18n_invalid_ds"));
+            }
+        }
+    }
+}
