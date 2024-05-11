67711b41be60b52be6d519a3b0bb968027bc840f
fit2cloudrd <reaperww@gmail.com>
2024-05-11 15:27:17 +0800

    fix: 移除desktop版本中不需要的SQL

--- core/core-backend/src/main/resources/db/desktop/V2.6__ddl.sql
+++ core/core-backend/src/main/resources/db/desktop/V2.6__ddl.sql
@@ -1,6 +1,3 @@
-ALTER TABLE `xpack_share`
-    ADD COLUMN `auto_pwd` tinyint(1) NOT NULL DEFAULT 1 COMMENT '自动生成密码';
-
 ALTER TABLE `data_visualization_info`
     ADD COLUMN `version` int NULL DEFAULT 3 COMMENT '可视化资源版本';
 update data_visualization_info set version = 2;
