7137b19365eaf115e00bb7621e147512ef429b1a
wangjiahao <1522128093@qq.com>
2024-05-11 14:59:39 +0800

    fix(仪表板): 仪表板支持前端缓存，异常退出可恢复

---
+++ core/core-frontend/src/components/visualization/CanvasCacheDialog.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-dialog
+    ref="canvasCacheDialogRef"
+    :title="dialogInfo.title"
+    :append-to-body="true"
+    v-model="dialogShow"
+    width="540px"
+    trigger="click"
+  >
+    <el-row style="height: 20px">
+      <el-col :span="3">
+        <Icon name="warn-tree" style="width: 20px; height: 20px; float: right" />
+      </el-col>
+      <el-col :span="21">
+        <span style="font-size: 13px; margin-left: 10px; font-weight: bold; line-height: 20px">
+          {{ dialogInfo.tips }}
+        </span>
+      </el-col>
+    </el-row>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button size="mini" @click="doUseCache(false)">否 </el-button>
+        <el-button type="primary" size="mini" @click="doUseCache(true)">是 </el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useI18n } from '@/hooks/web/useI18n'
+import { dvMainStoreWithOut } from '@/store/modules/data-visualization/dvMain'
+const dvMainStore = dvMainStoreWithOut()
+const dialogShow = ref(false)
+const { t } = useI18n()
+import { useCache } from '@/hooks/web/useCache'
+const { wsCache } = useCache()
+const emits = defineEmits(['doUseCache'])
+
+const dialogInfo = {
+  resourceId: null,
+  title: '',
+  tips: ''
+}
+
+const dialogInit = initInfo => {
+  const canvasTypeName = initInfo.canvasType === 'dataV' ? '数据大屏' : '仪表板'
+  dialogInfo.resourceId = initInfo.resourceId
+  dialogInfo.title = '存在未保存的' + canvasTypeName
+  dialogInfo.tips =
+    '检查到上次有' + canvasTypeName + '未能正常保存，是否使用上次未保存的' + canvasTypeName + '？'
+  dialogShow.value = true
+}
+
+const doUseCache = flag => {
+  emits('doUseCache', flag)
+  dialogShow.value = false
+}
+
+defineExpose({
+  dialogInit
+})
+</script>
+
+<style lang="less" scoped></style>
