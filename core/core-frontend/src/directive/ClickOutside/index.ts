52ad8d92a19e8458a2820f3375fedd3e3296cc6a
dataeaseShu <tong.shu@fit2cloud.com>
2024-05-14 10:31:20 +0800

    refactor: 删除import 'element-plus-secondary/es/css  #9624

--- core/core-frontend/src/directive/ClickOutside/index.ts
+++ core/core-frontend/src/directive/ClickOutside/index.ts
@@ -2,7 +2,9 @@ export const vClickOutside = {
   beforeMount(el, binding) {
     el.clickOutsideEvent = function (event) {
       if (!(el === event.target || el.contains(event.target))) {
-        binding.value(event)
+        if (typeof binding.value === 'function') {
+          binding.value(event)
+        }
       }
     }
     document.addEventListener('click', el.clickOutsideEvent)
