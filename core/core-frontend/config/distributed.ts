be6979da5724647538c2a978969c8dac967fa800
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-06-07 15:26:58 +0800

    chore(X-Pack): 默认以分离式xpack前端模式运行

---
+++ core/core-frontend/config/distributed.ts
@@ -0,0 +1,32 @@
+import pkg from '../package.json'
+import viteCompression from 'vite-plugin-compression'
+
+export default {
+  plugins: [
+    viteCompression({
+      // gzip静态资源压缩配置
+      verbose: true, // 是否在控制台输出压缩结果
+      disable: false, // 是否禁用压缩
+      threshold: 10240, // 启用压缩的文件大小限制
+      algorithm: 'gzip', // 采用的压缩算法
+      ext: '.gz' // 生成的压缩包后缀
+    })
+  ],
+  build: {
+    rollupOptions: {
+      external: id => /de-xpack/.test(id),
+      output: {
+        // 用于命名代码拆分时创建的共享块的输出命名
+        chunkFileNames: `assets/chunk/[name]-${pkg.version}-${pkg.name}.js`,
+        assetFileNames: `assets/[ext]/[name]-${pkg.version}-${pkg.name}.[ext]`,
+        entryFileNames: `js/[name]-${pkg.version}-${pkg.name}.js`,
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
+    },
+    sourcemap: false
+  }
+}
--- core/core-frontend/vite.config.ts
+++ core/core-frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import common from './config/common';
 import base from './config/base';
+import distributed from './config/distributed';
 import dev from './config/dev';
 import lib from './config/lib';
 import pages from './config/pages';
@@ -12,6 +13,9 @@ export default defineConfig(({mode}) => {
 
   if (mode === 'lib') {
     return mergeConfig(common , lib)
+  }
+  if (mode === 'distributed') {
+    return mergeConfig(common, mergeConfig(distributed, pages))
   }
 
   return mergeConfig(common, mergeConfig(base, pages))
