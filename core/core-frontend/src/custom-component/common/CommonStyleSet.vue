a727d424c7630a420936e85cab9d9061235e5b78
wangjiahao <1522128093@qq.com>
2024-06-12 16:02:15 +0800

    refactor(数据大屏): 优化跑马灯的滚动速度计算方式，以px为单位同时兼顾缩放的效果

--- core/core-frontend/src/custom-component/common/CommonStyleSet.vue
+++ core/core-frontend/src/custom-component/common/CommonStyleSet.vue
@@ -316,16 +316,16 @@ const fontFamilyList = [
 
 const scrollSpeedList = [
   { name: '停止', value: 0 },
-  { name: '1', value: 80 },
-  { name: '2', value: 60 },
-  { name: '3', value: 40 },
-  { name: '4', value: 30 },
-  { name: '5', value: 20 },
-  { name: '6', value: 15 },
-  { name: '7', value: 10 },
-  { name: '8', value: 8 },
-  { name: '9', value: 6 },
-  { name: '10', value: 3 }
+  { name: '10', value: 10 },
+  { name: '20', value: 20 },
+  { name: '30', value: 30 },
+  { name: '40', value: 40 },
+  { name: '50', value: 50 },
+  { name: '60', value: 60 },
+  { name: '70', value: 70 },
+  { name: '80', value: 80 },
+  { name: '90', value: 90 },
+  { name: '100', value: 100 }
 ]
 
 const opacitySizeList = [
