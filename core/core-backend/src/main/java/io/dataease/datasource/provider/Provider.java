40ecd125b49388c95aa70629d26f984071340b5d
junjun <junjie.xia@fit2cloud.com>
2024-06-20 15:41:06 +0800

    pref: 数据源接口

--- core/core-backend/src/main/java/io/dataease/datasource/provider/Provider.java
+++ core/core-backend/src/main/java/io/dataease/datasource/provider/Provider.java
@@ -1,7 +1,28 @@
 package io.dataease.datasource.provider;
 
+import io.dataease.api.dataset.dto.DatasetTableDTO;
+import io.dataease.api.ds.vo.TableField;
+import io.dataease.datasource.dao.auto.entity.CoreDatasource;
+import io.dataease.datasource.request.DatasourceRequest;
+import io.dataease.exception.DEException;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @Author Junjun
  */
 public abstract class Provider {
+    public abstract List<String> getSchema(DatasourceRequest datasourceRequest);
+
+    public abstract List<DatasetTableDTO> getTables(DatasourceRequest datasourceRequest);
+
+    public abstract Connection getConnection(CoreDatasource coreDatasource) throws DEException;
+
+    public abstract String checkStatus(DatasourceRequest datasourceRequest) throws Exception;
+
+    public abstract Map<String, Object> fetchResultField(DatasourceRequest datasourceRequest) throws DEException;
+
+    public abstract List<TableField> fetchTableField(DatasourceRequest datasourceRequest) throws DEException;
 }
