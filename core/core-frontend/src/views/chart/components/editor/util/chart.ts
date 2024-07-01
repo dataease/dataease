55cb2f383ba167e18ef9f83159a3f8f90ac942c0
jianneng-fit2cloud <jianneng.pan@fit2cloud.com>
2024-07-01 11:44:46 +0800

    fix(图表-K线图): 添加K线图图标

--- core/core-frontend/src/views/chart/components/editor/util/chart.ts
+++ core/core-frontend/src/views/chart/components/editor/util/chart.ts
@@ -1147,13 +1147,6 @@ export const CHART_TYPE_CONFIGS = [
         value: 'area-stack',
         title: t('chart.chart_area_stack'),
         icon: 'area-stack'
-      },
-      {
-        render: 'antv',
-        category: 'trend',
-        value: 'stock-line',
-        title: 'K 线图',
-        icon: 'line'
       }
     ]
   },
@@ -1245,6 +1238,13 @@ export const CHART_TYPE_CONFIGS = [
         value: 'progress-bar',
         title: t('chart.chart_progress_bar'),
         icon: 'progress-bar'
+      },
+      {
+        render: 'antv',
+        category: 'trend',
+        value: 'stock-line',
+        title: 'K 线图',
+        icon: 'stock-line'
       }
     ]
   },
