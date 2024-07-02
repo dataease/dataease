325cd6cda55cda500bd125853540e074ddae4023
junjun <junjie.xia@fit2cloud.com>
2024-07-02 13:36:49 +0800

    refactor: 数据源接口抽象

--- sdk/extensions/extensions-datasource/src/main/java/io/dataease/extensions/datasource/factory/ProviderFactory.java
+++ sdk/extensions/extensions-datasource/src/main/java/io/dataease/extensions/datasource/factory/ProviderFactory.java
@@ -1,16 +1,22 @@
 package io.dataease.extensions.datasource.factory;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import io.dataease.extensions.datasource.provider.Provider;
+import io.dataease.extensions.datasource.utils.SpringContextUtil;
+import io.dataease.extensions.datasource.vo.DatasourceConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Author Junjun
  */
-public class ProviderFactory implements ApplicationContextAware {
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+public class ProviderFactory {
 
+    public static Provider getProvider(String type) {
+        List<String> list = Arrays.stream(DatasourceConfiguration.DatasourceType.values()).map(DatasourceConfiguration.DatasourceType::getType).toList();
+        if (list.contains(type)) {
+            return SpringContextUtil.getApplicationContext().getBean("calciteProvider", Provider.class);
+        }
+        return SpringContextUtil.getApplicationContext().getBean(type + "DsProvider", Provider.class);
     }
-
 }
