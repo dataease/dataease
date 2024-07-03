fc1e4eb965837b897bab08d093c3d6e9ae10aff2
ulleo <ulleo.wang@gmail.com>
2024-07-03 17:41:49 +0800

    feat(X-Pack): [数据填报]表单绑定数据源需为必填

--- core/frontend/src/views/dataFilling/form/create.vue
+++ core/frontend/src/views/dataFilling/form/create.vue
@@ -722,6 +722,18 @@ export default {
                 }
               }
             }
+          } else {
+            if (f.settings.optionDatasource === undefined ||
+                  f.settings.optionTable === undefined ||
+                  f.settings.optionColumn === undefined) {
+              this.selectItem(f.id)
+              this.$message({
+                message: this.$t('data_fill.form.option_list_datasource_cannot_empty'),
+                type: 'error',
+                showClose: true
+              })
+              return
+            }
           }
         }
       }
