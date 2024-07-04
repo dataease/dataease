fa13a8d644eebf0f93178841c49f68a770f7f388
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-07-04 17:53:52 +0800

    fix(仪表板): 公共链接修改url参数回车无效 #10778

--- core/core-frontend/src/pages/index/App.vue
+++ core/core-frontend/src/pages/index/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import configGlobal from '@/components/config-global/src/ConfigGlobal.vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
 </script>
 <template>
   <config-global>
-    <router-view />
+    <router-view :key="route.path" />
   </config-global>
 </template>
