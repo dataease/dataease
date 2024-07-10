01692d71dbf6a746ac043aca3d422869dfe6263c
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-07-10 15:04:45 +0800

    fix(X-Pack): 配置动态url前缀的场景下无法进行cas/oid认证

--- core/core-frontend/src/utils/logout.ts
+++ core/core-frontend/src/utils/logout.ts
@@ -21,13 +21,21 @@ export const logoutHandler = (justClean?: boolean) => {
   if (router.currentRoute.value.fullPath) {
     queryRedirectPath = router.currentRoute.value.fullPath as string
   }
+  let pathname = window.location.pathname
+  if (pathname) {
+    if (pathname.includes('oidcbi/') || pathname.includes('casbi/')) {
+      pathname = pathname.replace('oidcbi/', '')
+      pathname = pathname.replace('casbi/', '')
+    }
+    pathname = pathname.substring(0, pathname.length - 1)
+  }
   if (wsCache.get('out_auth_platform') === 'cas') {
     const uri = window.location.href
-    window.location.href = '/casbi/cas/logout?service=' + uri
+    window.location.href = pathname + '/casbi/cas/logout?service=' + uri
     return
   }
   if (wsCache.get('out_auth_platform') === 'oidc') {
-    window.location.href = '/oidcbi/oidc/logout'
+    window.location.href = pathname + '/oidcbi/oidc/logout'
     return
   }
   if (wsCache.get('custom_auth_logout_url')) {
