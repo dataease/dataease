eaec3cb2a5d1e74a40a8e5d0f31e7ae5bfc514f8
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-07-12 15:47:57 +0800

    perf(X-Pack): 默认登录方式-在默认非普通登录方式场景下增加admin-login路由

--- core/core-frontend/src/permission.ts
+++ core/core-frontend/src/permission.ts
@@ -22,7 +22,7 @@ const { start, done } = useNProgress()
 
 const { loadStart, loadDone } = usePageLoading()
 
-const whiteList = ['/login', '/de-link', '/chart-view', '/notSupport'] // 不重定向白名单
+const whiteList = ['/login', '/de-link', '/chart-view', '/notSupport', '/admin-login', '/401'] // 不重定向白名单
 const embeddedWindowWhiteList = ['/dvCanvas', '/dashboard', '/preview', '/dataset-embedded-form']
 const embeddedRouteWhiteList = ['/dataset-embedded', '/dataset-form', '/dataset-embedded-form']
 router.beforeEach(async (to, from, next) => {
