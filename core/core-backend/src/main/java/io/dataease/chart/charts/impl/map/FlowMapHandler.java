399cf3e8323b6b4c90c684877b42aed9b4548513
jianneng-fit2cloud <jianneng.pan@fit2cloud.com>
2024-07-25 12:12:07 +0800

    fix(图表-流向地图): 修复在不设置起点终点名称时报错的问题

--- core/core-backend/src/main/java/io/dataease/chart/charts/impl/map/FlowMapHandler.java
+++ core/core-backend/src/main/java/io/dataease/chart/charts/impl/map/FlowMapHandler.java
@@ -7,6 +7,9 @@ import io.dataease.extensions.view.dto.ChartViewDTO;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 @Component
 public class FlowMapHandler extends GroupChartHandler {
     @Getter
@@ -15,8 +18,8 @@ public class FlowMapHandler extends GroupChartHandler {
     public AxisFormatResult formatAxis(ChartViewDTO view) {
         var result = super.formatAxis(view);
         var xAxis = result.getAxisMap().get(ChartAxis.xAxis);
-        xAxis.addAll(view.getFlowMapStartName());
-        xAxis.addAll(view.getFlowMapEndName());
+        xAxis.addAll(Optional.ofNullable(view.getFlowMapStartName()).orElse(new ArrayList<>()));
+        xAxis.addAll(Optional.ofNullable(view.getFlowMapStartName()).orElse(new ArrayList<>()));
         result.getAxisMap().put(ChartAxis.xAxis, xAxis);
         return result;
     }
