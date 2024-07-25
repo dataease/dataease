3c8496b4fc2d3e75e3c4cd28947775aaa2c4a1a1
wangjiahao <1522128093@qq.com>
2024-07-25 18:18:03 +0800

    refactor(仪表板、数据大屏): 清理未使用边框

--- core/core-backend/src/main/resources/db/migration/V2.10__ddl.sql
+++ core/core-backend/src/main/resources/db/migration/V2.10__ddl.sql
@@ -1,1 +1,2 @@
 INSERT INTO `core_sys_setting` (`id`, `pkey`, `pval`, `type`, `sort`) VALUES (10, 'basic.exportFileLiveTime', '30', 'text', 2);
+delete from visualization_background where id ='dark_1';
