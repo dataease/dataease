3c308e53f0812b795b0a9b6313d621c3ab573d2c
jianneng-fit2cloud <jianneng.pan@fit2cloud.com>
2024-07-29 19:21:50 +0800

    style(图表-符号地图): 根据数据自适应缩放

--- core/core-frontend/src/views/chart/components/js/panel/charts/map/symbolic-map.ts
+++ core/core-frontend/src/views/chart/components/js/panel/charts/map/symbolic-map.ts
@@ -139,30 +139,8 @@ export class SymbolicMap extends L7ChartView<Scene, L7Config> {
         }
       })
     })
-    scene.once('loaded', () => {
-      this.autoZoom(symbolicLayer, scene)
-    })
-    return new L7Wrapper(scene, configList)
-  }
 
-  /**
-   * 根据数据自动缩放大小及位置
-   * @param symbolicLayer
-   * @param scene
-   */
-  autoZoom = (symbolicLayer, scene) => {
-    const roamMap = flag => {
-      return flag ? scene.zoomIn() : scene.zoomOut()
-    }
-    const resetZoom = () => {
-      symbolicLayer.fitBounds()
-    }
-    symbolicLayer && resetZoom()
-    // 自动放大两级
-    let index = 2
-    while (index--) {
-      roamMap(true)
-    }
+    return new L7Wrapper(scene, configList)
   }
 
   /**
@@ -199,7 +177,7 @@ export class SymbolicMap extends L7ChartView<Scene, L7Config> {
           }
         })
       : []
-    const pointLayer = new PointLayer()
+    const pointLayer = new PointLayer({ autoFit: true })
       .source(data, {
         parser: {
           type: 'json',
