236bd6093f1e3b700a93ef3b9f157b34b374c096
dataeaseShu <tong.shu@fit2cloud.com>
2024-08-15 16:50:48 +0800

    feat(xpack): 血缘分析

--- core/core-frontend/src/components/icon-custom/src/Icon.vue
+++ core/core-frontend/src/components/icon-custom/src/Icon.vue
@@ -3,6 +3,7 @@ import { computed } from 'vue'
 import { propTypes } from '@/utils/propTypes'
 
 import _401 from '@/assets/svg/401.svg'
+import icon_data_visualization from '@/assets/svg/icon_data-visualization.svg'
 import icon_notification_filled from '@/assets/svg/icon_notification_filled.svg'
 import calculate from '@/assets/svg/calculate.svg'
 import _403 from '@/assets/svg/403.svg'
@@ -677,6 +678,7 @@ import icon_file_doc_colorful from '@/assets/svg/icon_file-doc_colorful.svg'
 import icon_font from '@/assets/svg/icon_font.svg'
 const iconMap = {
   '401': _401,
+  icon_data_visualization,
   icon_notification_filled,
   '403': _403,
   'API-ds': APIDs,
