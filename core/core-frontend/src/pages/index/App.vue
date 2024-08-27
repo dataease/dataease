6227f1b565e9983db054117d2d41753fd45b26e4
dataeaseShu <tong.shu@fit2cloud.com>
2024-08-27 09:47:04 +0800

    fix(仪表板): 图表-导出-点击「数据导出中心」-无响应，右侧未弹出「数据导出中心」

--- core/core-frontend/src/pages/index/App.vue
+++ core/core-frontend/src/pages/index/App.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useEmitt } from '@/hooks/web/useEmitt'
 import configGlobal from '@/components/config-global/src/ConfigGlobal.vue'
 import { useRoute } from 'vue-router'
+import ExportExcel from '@/views/visualized/data/dataset/ExportExcel.vue'
 const route = useRoute()
+const exportExcelRef = ref()
+const exportExcelCenter = params => {
+  exportExcelRef.value.init(params)
+}
+useEmitt({
+  name: 'data-export-center',
+  callback: exportExcelCenter
+})
 </script>
 <template>
   <config-global>
     <router-view :key="route.path" />
+    <ExportExcel ref="exportExcelRef"></ExportExcel>
   </config-global>
 </template>
