99971a0c71c82f257a94487c86b561bb19d4d58d
dataeaseShu <tong.shu@fit2cloud.com>
2024-09-14 16:49:27 +0800

    fix: 删除注释

--- core/core-frontend/config/dev.ts
+++ core/core-frontend/config/dev.ts
@@ -8,7 +8,7 @@ export default {
       },
       // 使用 proxy 实例
       '/api': {
-        target: 'https://de2.fit2cloud.com',
+        target: 'http://localhost:8100',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, 'de2api')
       }
