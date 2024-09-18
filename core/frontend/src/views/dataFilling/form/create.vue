8a0c03636eb93a69bb102c9a63773d0c73b224d9
ulleo <ulleo.wang@gmail.com>
2024-09-18 17:55:48 +0800

    feat(X-Pack): 【数据填报】提交表单数据页面，下拉框支持快速搜索

--- core/frontend/src/views/dataFilling/form/create.vue
+++ core/frontend/src/views/dataFilling/form/create.vue
@@ -980,6 +980,7 @@ export default {
                       :placeholder="item.settings.placeholder"
                       style="width: 100%"
                       size="small"
+                      filterable
                       :multiple="item.settings.multiple"
                       clearable
                     >
