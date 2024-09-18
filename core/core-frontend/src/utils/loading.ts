32b801e010dea27002bc05b5a67503452e42d4fe
dataeaseShu <tong.shu@fit2cloud.com>
2024-09-18 15:51:33 +0800

    fix(仪表板): v2 公共链接视图明细导出优化 #11487

--- core/core-frontend/src/utils/loading.ts
+++ core/core-frontend/src/utils/loading.ts
@@ -16,7 +16,7 @@ export const tryHideLoading = identification => {
 /**
  * 说明
  * 要在view中加loading只需要两个步骤
- * 1.在vue页面文件中加 v-loading="requestStore.loadingMap[requestStore.currentPath]"
+ * 1.在vue页面文件中加 v-loading="requestStore.loadingMap[permissionStore.currentPath]"
  *     例如：views/system/user/index.vue
  * 2.在需要loading的api中添加config {loading: true}
  *     例如：api/system/user.js中userLists方法 需要配置loading: true
