b3469ba6fd7d7af7e7f5b6b00e04acb74fe11c89
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-09-25 15:38:09 +0800

    perf(X-Pack): 优化定时报告flyway

--- core/core-backend/src/main/resources/db/migration/V2.10.1__ddl.sql
+++ core/core-backend/src/main/resources/db/migration/V2.10.1__ddl.sql
@@ -1,12 +1,10 @@
 ALTER TABLE `visualization_outer_params_info`
-    ADD COLUMN `required`        tinyint(1) DEFAULT 0 COMMENT '是否必填',
+    ADD COLUMN `required`        tinyint(1)   DEFAULT 0 COMMENT '是否必填',
     ADD COLUMN `default_value`   varchar(255) DEFAULT NULL COMMENT '默认值 JSON格式',
     ADD COLUMN `enabled_default` tinyint(1) NULL DEFAULT 0 COMMENT '是否启用默认值';
 update visualization_outer_params_info
 set required =0;
 
-ALTER TABLE `xpack_report_info`
-    ADD COLUMN `show_watermark` tinyint(1) NOT NULL DEFAULT 0 COMMENT '显示水印' AFTER `rid`;
 
 ALTER TABLE `visualization_link_jump_info`
     ADD COLUMN `window_size` varchar(255) NULL DEFAULT 'middle' COMMENT '窗口大小large middle small';
