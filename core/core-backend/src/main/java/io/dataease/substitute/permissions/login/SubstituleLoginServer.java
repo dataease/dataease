e755248d59543bcd668ace495f293ff735fa82e9
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-09-29 17:20:16 +0800

    perf: 社区版token机制

--- core/core-backend/src/main/java/io/dataease/substitute/permissions/login/SubstituleLoginServer.java
+++ core/core-backend/src/main/java/io/dataease/substitute/permissions/login/SubstituleLoginServer.java
@@ -5,14 +5,17 @@ import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
 import io.dataease.api.permissions.login.dto.PwdLoginDTO;
 import io.dataease.auth.bo.TokenUserBO;
+import io.dataease.auth.config.SubstituleLoginConfig;
 import io.dataease.auth.vo.TokenVO;
+import io.dataease.exception.DEException;
+import io.dataease.i18n.Translator;
 import io.dataease.utils.LogUtil;
+import io.dataease.utils.Md5Utils;
+import io.dataease.utils.RsaUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Component
 @ConditionalOnMissingBean(name = "loginServer")
@@ -21,11 +24,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class SubstituleLoginServer {
 
     @PostMapping("/login/localLogin")
-    public TokenVO localLogin(PwdLoginDTO dto) {
+    public TokenVO localLogin(@RequestBody PwdLoginDTO dto) {
+
+        String name = dto.getName();
+        name = RsaUtils.decryptStr(name);
+        String pwd = dto.getPwd();
+        pwd = RsaUtils.decryptStr(pwd);
+
+        dto.setName(name);
+        dto.setPwd(pwd);
+
+        if (!StringUtils.equals("admin", name)) {
+            DEException.throwException("仅admin账号可用");
+        }
+        if (!StringUtils.equals(pwd, SubstituleLoginConfig.getPwd())) {
+            DEException.throwException(Translator.get("i18n_login_name_pwd_err"));
+        }
         TokenUserBO tokenUserBO = new TokenUserBO();
         tokenUserBO.setUserId(1L);
         tokenUserBO.setDefaultOid(1L);
-        String md5Pwd = "83d923c9f1d8fcaa46cae0ed2aaa81b5";
+        String md5Pwd = Md5Utils.md5(pwd);
         return generate(tokenUserBO, md5Pwd);
     }
 
