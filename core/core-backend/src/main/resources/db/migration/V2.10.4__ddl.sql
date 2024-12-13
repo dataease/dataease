3b5e6c5c509471c6380de0637114995ead98d4a8
wisonic <shaochuan.wu@fit2cloud.com>
2024-12-13 15:53:32 +0800

    refactor(图表): 字段排序 DDL

--- core/core-backend/src/main/resources/db/migration/V2.10.4__ddl.sql
+++ core/core-backend/src/main/resources/db/migration/V2.10.4__ddl.sql
@@ -2,4 +2,4 @@ UPDATE `visualization_background` SET `name` = 'Board10' WHERE `id` = 'dark_1';
 UPDATE `visualization_subject` SET `name` = 'chart.light_theme' WHERE `id` = '10001';
 UPDATE `visualization_subject` SET `name` = 'chart.dark_theme' WHERE `id` = '10002';
 CREATE INDEX idx_dataset_table_task_log_A ON core_datasource_task_log(ds_id, table_name, start_time);
-ALTER TABLE core_chart_view ADD sort_priority longtext null comment '字段排序优先级';
+ALTER TABLE core_chart_view ADD COLUMN sort_priority longtext null comment '字段排序优先级';
