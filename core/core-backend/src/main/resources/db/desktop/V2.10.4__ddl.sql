5426fb697c8d603a70c50c8d033e9119653ec065
taojinlong <jinlong@fit2cloud.com>
2024-12-19 11:28:25 +0800

    fix: 优化查询数据表最新更新时间

--- core/core-backend/src/main/resources/db/desktop/V2.10.4__ddl.sql
+++ core/core-backend/src/main/resources/db/desktop/V2.10.4__ddl.sql
@@ -3,3 +3,5 @@ UPDATE `visualization_subject` SET `name` = 'chart.light_theme' WHERE `id` = '10
 UPDATE `visualization_subject` SET `name` = 'chart.dark_theme' WHERE `id` = '10002';
 ALTER TABLE core_chart_view ADD COLUMN sort_priority longtext null comment '字段排序优先级';
 DELETE FROM area where id = '156320571';
+CREATE INDEX idx_dataset_table_task_log_A ON core_datasource_task_log(ds_id, table_name, start_time);
+
