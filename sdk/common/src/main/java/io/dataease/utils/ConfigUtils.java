c3156bbbff7df9c41492842698a4aaf9e2747975
junjun <junjie.xia@fit2cloud.com>
2024-12-31 12:36:46 +0800

    chore: 将资源的固定路径优化为可读取yml配置中的路径

--- sdk/common/src/main/java/io/dataease/utils/ConfigUtils.java
+++ sdk/common/src/main/java/io/dataease/utils/ConfigUtils.java
@@ -12,11 +12,13 @@ import java.util.Objects;
  */
 public class ConfigUtils {
 
+    public static String configPath = "opt" + File.separator + "dataease2.0" + File.separator + "config" + File.separator + "application.yml";
+
     public static String getConfig(String key, String defaultValue) {
         try {
-            String filePath = System.getProperty("user.dir");
-            filePath = filePath.replace("file:", "").substring(0, filePath.lastIndexOf("resources"));
-            Resource resource = new FileSystemResource(filePath + "resources" + File.separator + "config" + File.separator + "application.yml");
+            String filePath = System.getProperty("user.home");
+            filePath = filePath.replace("file:", "");
+            Resource resource = new FileSystemResource(filePath + File.separator + configPath);
             YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
             factory.setResources(resource);
 
