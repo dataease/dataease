06e3c38481bba174feb623b1307eb40101d1f390
dataeaseShu <tong.shu@fit2cloud.com>
2025-01-15 17:52:33 +0800

    fix(数据集): 使用PostgreSQL的外部表SQL和内部表左连接，创建数据集成功

--- core/core-frontend/src/views/visualized/data/dataset/form/index.vue
+++ core/core-frontend/src/views/visualized/data/dataset/form/index.vue
@@ -891,6 +891,7 @@ const dfsFieldsTips = (arr, list, idArr) => {
   })
 }
 const confirmEditUnion = () => {
+  num = +new Date()
   const { node, parent } = fieldUnion.value
   const to = node.id
   const from = parent.id
