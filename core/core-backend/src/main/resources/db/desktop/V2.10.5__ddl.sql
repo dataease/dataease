a4f2bd7a43e27e6acafcf8f26368df9307afbc1d
wangjiahao <1522128093@qq.com>
2025-01-20 17:33:59 +0800

    refactor: 更新flyway

--- core/core-backend/src/main/resources/db/desktop/V2.10.5__ddl.sql
+++ core/core-backend/src/main/resources/db/desktop/V2.10.5__ddl.sql
@@ -1,6 +1,3 @@
-ALTER TABLE `xpack_threshold_info`
-    ADD COLUMN `reci_larksuite_groups` longtext NULL COMMENT '国际飞书群';
-
 UPDATE core_sys_setting
 SET pval = 'https://cdn0-templates-dataease-cn.fit2cloud.com'
 WHERE
