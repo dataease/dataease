7c28d1dfce40250ad1b007120481707b4f29106b
jianneng-fit2cloud <jianneng.pan@fit2cloud.com>
2025-03-07 11:11:01 +0800

    perf(API 文档): 优化数据源管理 API 描述信息

--- sdk/api/api-base/src/main/java/io/dataease/api/ds/DatasourceDriverApi.java
+++ sdk/api/api-base/src/main/java/io/dataease/api/ds/DatasourceDriverApi.java
@@ -5,6 +5,7 @@ import com.github.xiaoymin.knife4j.annotations.ApiSupport;
 import io.dataease.api.ds.vo.DriveDTO;
 import io.dataease.api.ds.vo.DriveJarDTO;
 import io.dataease.extensions.datasource.dto.DatasourceDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,30 +19,39 @@ public interface DatasourceDriverApi {
      * @param keyWord 过滤关键字
      * @return
      */
+    @Operation(summary = "查询数据源树", hidden = true)
     @GetMapping("/query/{keyWord}")
     List<DatasourceDTO> query(@PathVariable("keyWord") String keyWord);
 
+    @Operation(summary = "列表")
     @GetMapping("/list")
     List<DriveDTO> list();
 
+    @Operation(summary = "根据数据源类型获取")
     @GetMapping("/list/{dsType}")
     List<DriveDTO> listByDsType(@PathVariable("dsType") String dsType);
 
+    @Operation(summary = "保存")
     @PostMapping("/save")
     DriveDTO save(@RequestBody DriveDTO datasourceDrive);
 
+    @Operation(summary = "更新")
     @PostMapping("/update")
     DriveDTO update(@RequestBody DriveDTO datasourceDrive);
 
+    @Operation(summary = "删除")
     @PostMapping("/delete/{driverId}")
     void delete(@PathVariable("driverId") String driverId);
 
+    @Operation(summary = "获取驱动jar列表")
     @GetMapping("/listDriverJar/{driverId}")
     List<DriveJarDTO> listDriverJar(@PathVariable("driverId") String driverId);
 
+    @Operation(summary = "删除驱动jar")
     @PostMapping("/deleteDriverJar/{jarId}")
     void deleteDriverJar(@PathVariable("jarId") String jarId);
 
+    @Operation(summary = "上传驱动jar")
     @PostMapping("/uploadJar")
     DriveJarDTO uploadJar(@RequestParam("deDriverId") String deDriverId, @RequestParam("jarFile") MultipartFile jarFile) throws Exception;
 }
