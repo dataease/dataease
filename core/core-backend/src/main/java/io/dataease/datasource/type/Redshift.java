c2525a7148a6df40bc90f50e2fc0dd0df2292c92
taojinlong <jinlong@fit2cloud.com>
2025-04-23 11:52:27 +0800

    fix: 过滤 redshift 非法参数

--- core/core-backend/src/main/java/io/dataease/datasource/type/Redshift.java
+++ core/core-backend/src/main/java/io/dataease/datasource/type/Redshift.java
@@ -1,18 +1,29 @@
 package io.dataease.datasource.type;
 
+import io.dataease.exception.DEException;
 import io.dataease.extensions.datasource.vo.DatasourceConfiguration;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
 
 @Data
 @Component("redshift")
 public class Redshift extends DatasourceConfiguration {
     private String driver = "com.amazon.redshift.jdbc42.Driver";
     private String extraParams = "";
+    private List<String> illegalParameters = Arrays.asList("socketFactory", "socketFactoryArg");
 
     public String getJdbc() {
         if(StringUtils.isNoneEmpty(getUrlType()) && !getUrlType().equalsIgnoreCase("hostName")){
+            for (String illegalParameter : illegalParameters) {
+                if (URLDecoder.decode(getJdbcUrl()).contains(illegalParameter)) {
+                    DEException.throwException("Illegal parameter: " + illegalParameter);
+                }
+            }
             return getJdbcUrl();
         }
         return "jdbc:redshift://HOSTNAME:PORT/DATABASE"
