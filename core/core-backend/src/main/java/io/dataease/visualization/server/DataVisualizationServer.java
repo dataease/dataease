f5c75b68fd8d18f33f2c1cfb6c7c1ca79f67a265
wangjiahao <1522128093@qq.com>
2025-05-06 11:19:13 +0800

    refactor(仪表板、数据大屏): 增加仪表板大屏查询索引同时过滤非必要的图表信息以增加查询速度

--- core/core-backend/src/main/java/io/dataease/visualization/server/DataVisualizationServer.java
+++ core/core-backend/src/main/java/io/dataease/visualization/server/DataVisualizationServer.java
@@ -200,7 +200,8 @@ public class DataVisualizationServer implements DataVisualizationApi {
             //获取图表信息
             List<ChartViewDTO> chartViewDTOS = chartViewManege.listBySceneId(dvId, resourceTable);
             if (!CollectionUtils.isEmpty(chartViewDTOS)) {
-                Map<Long, ChartViewDTO> viewInfo = chartViewDTOS.stream().collect(Collectors.toMap(ChartViewDTO::getId, chartView -> chartView));
+                // 增加过滤当前使用的图表信息
+                Map<Long, ChartViewDTO> viewInfo = chartViewDTOS.stream().filter(item -> result.getComponentData().indexOf("\"id\":\"" + item.getId()) > 0).collect(Collectors.toMap(ChartViewDTO::getId, chartView -> chartView));
                 result.setCanvasViewInfo(viewInfo);
             }
             VisualizationWatermark watermark = watermarkMapper.selectById("system_default");
