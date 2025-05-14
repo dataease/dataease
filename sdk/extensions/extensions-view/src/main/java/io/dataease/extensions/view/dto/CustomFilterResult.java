7f32e4fa49583e3e176055cd061f9ef46b5fd2a1
ulleo <ulleo.wang@gmail.com>
2025-05-14 15:32:07 +0800

    fix(图表): 修复查询组件通过新建计算字段关联组合图报错的问题

--- sdk/extensions/extensions-view/src/main/java/io/dataease/extensions/view/dto/CustomFilterResult.java
+++ sdk/extensions/extensions-view/src/main/java/io/dataease/extensions/view/dto/CustomFilterResult.java
@@ -2,21 +2,13 @@ package io.dataease.extensions.view.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class CustomFilterResult {
     private List<ChartExtFilterDTO> filterList;
     private Map<String, Object> context;
-    private boolean isDrill;// 组合图右轴判断是否下钻字段，其余地方没有用
-
-    public CustomFilterResult(List<ChartExtFilterDTO> filterList, Map<String, Object> context) {
-        this.filterList = filterList;
-        this.context = context;
-    }
 }
