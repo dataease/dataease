c300137047ba6abbfee558816f745c6ede76a175
wisonic-s <shaochuan.wu@fit2cloud.com>
2025-05-20 11:15:04 +0800

    fix(图表): 修复富文本失效

--- core/core-frontend/src/views/chart/components/js/panel/index.ts
+++ core/core-frontend/src/views/chart/components/js/panel/index.ts
@@ -20,7 +20,13 @@ class ChartViewManager {
 const chartViewManager = new ChartViewManager()
 // 批量自动注册图表，只要是 AbstractChartView 的子类都初始化然后存起来
 const charts = import.meta.glob(
-  ['./charts/map/*.ts', './charts/table/*.ts', './charts/g2/**/*.ts', '!**/common.ts'],
+  [
+    './charts/map/*.ts',
+    './charts/table/*.ts',
+    './charts/g2/**/*.ts',
+    './charts/others/**/*.ts',
+    '!**/common.ts'
+  ],
   { eager: true }
 )
 for (const chart in charts) {
