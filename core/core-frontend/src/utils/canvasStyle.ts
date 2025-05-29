e9426e6acc78b6fea9ba24fa825c7f669cc18f0f
wangjiahao <1522128093@qq.com>
2025-05-29 18:20:42 +0800

    refactor(图表): 图形和图标支持设置内边距调整为0 #16143

--- core/core-frontend/src/utils/canvasStyle.ts
+++ core/core-frontend/src/utils/canvasStyle.ts
@@ -502,9 +502,16 @@ export function adaptCurThemeCommonStyle(component) {
   // 背景融合-Begin 如果是大屏['CanvasBoard', 'CanvasIcon', 'Picture']组件不需要设置背景
   if (
     dvMainStore.dvInfo.type === 'dataV' &&
-    ['CanvasBoard', 'CanvasIcon', 'Picture', 'Group', 'SvgTriangle', 'SvgStar'].includes(
-      component.component
-    )
+    [
+      'CanvasBoard',
+      'CanvasIcon',
+      'Picture',
+      'Group',
+      'SvgTriangle',
+      'SvgStar',
+      'RectShape',
+      'CircleShape'
+    ].includes(component.component)
   ) {
     component.commonBackground['backgroundColorSelect'] = false
     component.commonBackground['innerPadding'] = 0
