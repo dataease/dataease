ab1981ffa214d05398b50032edb305fba3d9cda9
dataeaseShu <tong.shu@fit2cloud.com>
2025-05-30 16:09:30 +0800

    fix: 样式优化

--- core/core-frontend/src/views/chart/components/editor/index.vue
+++ core/core-frontend/src/views/chart/components/editor/index.vue
@@ -5193,6 +5193,9 @@ span {
 </style>
 
 <style lang="less">
+.ed-dropdown__popper.ed-popper.is-dark:has(.dark-dimension-quota) {
+  border: none;
+}
 :deep(.ed-select-dropdown__item) {
   display: flex;
   align-items: center;
