6407ce9d1ad5da8557ee5de69a212ea687369249
taojinlong <jinlong@fit2cloud.com>
2025-06-11 17:05:23 +0800

    fix: [Bug] DM 数据源，schema 带 "-" 的情况下，数据集拖拽表报错 #16310

--- core/core-backend/src/main/java/io/dataease/dataset/manage/DatasetDataManage.java
+++ core/core-backend/src/main/java/io/dataease/dataset/manage/DatasetDataManage.java
@@ -60,6 +60,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static io.dataease.chart.manage.ChartDataManage.START_END_SEPARATOR;
+import static io.dataease.dataset.utils.TableUtils.format;
 
 /**
  * @Author Junjun
@@ -126,7 +127,7 @@ public class DatasetDataManage {
                 Map map = JsonUtil.parseObject(datasourceSchemaDTO.getConfiguration(), Map.class);
                 if (!datasourceRequest.getIsCross()) {
                     if (ObjectUtils.isNotEmpty(map.get("schema"))) {
-                        sql = sql.replaceAll(SqlPlaceholderConstants.KEYWORD_PREFIX_REGEX + datasourceSchemaDTO.getSchemaAlias() + SqlPlaceholderConstants.KEYWORD_SUFFIX_REGEX, map.get("schema").toString());
+                        sql = sql.replaceAll(SqlPlaceholderConstants.KEYWORD_PREFIX_REGEX + datasourceSchemaDTO.getSchemaAlias() + SqlPlaceholderConstants.KEYWORD_SUFFIX_REGEX, String.format(format, map.get("schema").toString()) );
                     } else {
                         sql = sql.replaceAll(SqlPlaceholderConstants.KEYWORD_PREFIX_REGEX + datasourceSchemaDTO.getSchemaAlias() + SqlPlaceholderConstants.KEYWORD_SUFFIX_REGEX + "\\.", "");
                     }
