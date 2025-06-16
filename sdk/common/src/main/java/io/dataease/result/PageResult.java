31a097e7d6548776a3c339260f84ecb38c1ae7d2
jianneng-fit2cloud <jianneng.pan@fit2cloud.com>
2025-06-16 15:38:05 +0800

    feat: jpa改造

--- sdk/common/src/main/java/io/dataease/result/PageResult.java
+++ sdk/common/src/main/java/io/dataease/result/PageResult.java
@@ -1,4 +1,4 @@
-package io.dataease.api.sync.commons;
+package io.dataease.result;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,13 +18,37 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class PageResult<T> {
+    /**
+     * 数据
+     */
     private List<T> records;
+    /**
+     * 总数
+     */
     private long total;
+    /**
+     * 当前页码
+     */
     private long current;
+    /**
+     * 总页数
+     */
     private long pages;
+    /**
+     * 每页大小
+     */
     private long size;
+    /**
+     * 原始分页对象
+     */
     private Page originalPage;
 
+    /**
+     * 自定义分页结果类
+     *
+     * @param page             分页对象 JPA原始分页对象
+     * @param showOriginalPage 是否显示原始分页对象
+     */
     public PageResult(Page page, boolean... showOriginalPage) {
         this.records = page.getContent();
         this.total = page.getTotalElements();
@@ -36,6 +60,13 @@ public class PageResult<T> {
         }
     }
 
+    /**
+     * 自定义分页结果类
+     *
+     * @param list  数据列表
+     * @param total 总数
+     * @param page  分页对象 分页信息
+     */
     public PageResult(List<T> list, Long total, Pageable page) {
         this.records = list;
         this.total = total;
