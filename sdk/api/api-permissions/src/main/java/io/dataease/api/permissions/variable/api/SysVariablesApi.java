29567dbd50c45c913ca21f533e968eb994c72ed9
jianneng-fit2cloud <jianneng.pan@fit2cloud.com>
2025-06-27 17:05:48 +0800

    feat: jpa改造

--- sdk/api/api-permissions/src/main/java/io/dataease/api/permissions/variable/api/SysVariablesApi.java
+++ sdk/api/api-permissions/src/main/java/io/dataease/api/permissions/variable/api/SysVariablesApi.java
@@ -5,10 +5,10 @@ import com.github.xiaoymin.knife4j.annotations.ApiSupport;
 import io.dataease.api.permissions.variable.dto.SysVariableDto;
 import io.dataease.api.permissions.variable.dto.SysVariableValueDto;
 import io.dataease.auth.DeApiPath;
+import io.dataease.result.PageResult;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,7 +62,7 @@ public interface SysVariablesApi {
 
     @Operation(summary = "系统变量值列表")
     @PostMapping("/value/selected/{goPage}/{pageSize}")
-    Page<SysVariableValueDto> selectPage(@PathVariable("goPage") int goPage, @PathVariable("pageSize") int pageSize, @RequestBody SysVariableValueDto sysVariableValueDto);
+    PageResult<SysVariableValueDto> selectPage(@PathVariable("goPage") int goPage, @PathVariable("pageSize") int pageSize, @RequestBody SysVariableValueDto sysVariableValueDto);
 
     @Operation(summary = "批量删除变量值")
     @PostMapping("/value/batchDel")
