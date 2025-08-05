ac28a73a3c4b4ea94f0f13f4d1c806705a9139fd
taojinlong <jinlong@fit2cloud.com>
2025-08-05 16:38:47 +0800

    fix: 【漏洞】桌面版Aspectjweaver反序列化任意文件写入漏洞

--- core/core-backend/src/main/java/io/dataease/datasource/type/Db2.java
+++ core/core-backend/src/main/java/io/dataease/datasource/type/Db2.java
@@ -1,21 +1,31 @@
 package io.dataease.datasource.type;
 
+import io.dataease.exception.DEException;
 import io.dataease.extensions.datasource.vo.DatasourceConfiguration;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Data
 @Component("db2")
 public class Db2 extends DatasourceConfiguration {
     private String driver = "com.ibm.db2.jcc.DB2Driver";
     private String extraParams = "";
+    private List<String> illegalParameters = Arrays.asList("rmi");
 
     public String getJdbc() {
-        if(StringUtils.isNoneEmpty(getUrlType()) && !getUrlType().equalsIgnoreCase("hostName")){
+        if (StringUtils.isNoneEmpty(getUrlType()) && !getUrlType().equalsIgnoreCase("hostName")) {
+            for (String illegalParameter : illegalParameters) {
+                if (getJdbcUrl().toLowerCase().contains(illegalParameter.toLowerCase())) {
+                    DEException.throwException("Illegal parameter: " + illegalParameter);
+                }
+            }
             return getJdbcUrl();
         }
-        if(StringUtils.isEmpty(extraParams.trim())){
+        if (StringUtils.isEmpty(extraParams.trim())) {
             if (StringUtils.isEmpty(getSchema())) {
                 return "jdbc:db2://HOSTNAME:PORT/DATABASE"
                         .replace("HOSTNAME", getLHost().trim())
@@ -26,14 +36,20 @@ public class Db2 extends DatasourceConfiguration {
                         .replace("HOSTNAME", getLHost().trim())
                         .replace("PORT", getLPort().toString().trim())
                         .replace("DATABASE", getDataBase().trim())
-                        .replace("SCHEMA",getSchema().trim());
+                        .replace("SCHEMA", getSchema().trim());
             }
-        }else {
-            return "jdbc:db2://HOSTNAME:PORT/DATABASE:EXTRA_PARAMS"
+        } else {
+            String url = "jdbc:db2://HOSTNAME:PORT/DATABASE:EXTRA_PARAMS"
                     .replace("HOSTNAME", getLHost().trim())
                     .replace("PORT", getLPort().toString().trim())
                     .replace("DATABASE", getDataBase().trim())
                     .replace("EXTRA_PARAMS", getExtraParams().trim());
+            for (String illegalParameter : illegalParameters) {
+                if (url.toLowerCase().contains(illegalParameter.toLowerCase())) {
+                    DEException.throwException("Illegal parameter: " + illegalParameter);
+                }
+            }
+            return url;
         }
     }
 }
