307b40970e4cc73fb1de3e2bb6e6de1afcda8c91
wisonic-s <shaochuan.wu@fit2cloud.com>
2025-09-02 15:54:21 +0800

    fix(图表): 修复线面图纵轴网格线实线不生效

--- core/core-frontend/src/views/chart/components/js/panel/charts/g2/mix/mix-stack.ts
+++ core/core-frontend/src/views/chart/components/js/panel/charts/g2/mix/mix-stack.ts
@@ -625,14 +625,14 @@ export class StackLineMix extends G2ChartView {
       defaultsDeep(view, axisHide)
       return options
     }
-    let lineLineDash = undefined
+    let lineLineDash = [0, 0]
     if (xAxis.axisLine.lineStyle.style === 'dashed') {
       lineLineDash = [10, 8]
     }
     if (xAxis.axisLine.lineStyle.style === 'dotted') {
       lineLineDash = [1, 2]
     }
-    let gridLineDash = undefined
+    let gridLineDash = [0, 0]
     if (xAxis.splitLine.lineStyle.style === 'dashed') {
       gridLineDash = [10, 8]
     }
