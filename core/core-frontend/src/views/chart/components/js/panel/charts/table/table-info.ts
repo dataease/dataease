1a94a745157524d1faa35bf694943f66235bbc10
wisonic-s <shaochuan.wu@fit2cloud.com>
2025-09-03 16:21:47 +0800

    fix(图表): 修复表格自适应模式出现横向滚动条

--- core/core-frontend/src/views/chart/components/js/panel/charts/table/table-info.ts
+++ core/core-frontend/src/views/chart/components/js/panel/charts/table/table-info.ts
@@ -181,7 +181,7 @@ export class TableInfo extends S2ChartView<TableSheet> {
 
     // options
     const s2Options: S2Options = {
-      width: containerDom.offsetWidth,
+      width: containerDom.getBoundingClientRect().width,
       height: containerDom.offsetHeight,
       seriesNumber: {
         enable: tableHeader.showIndex,
@@ -297,7 +297,7 @@ export class TableInfo extends S2ChartView<TableSheet> {
         const totalWidthWithImg = ev.colLeafNodes.reduce((p, n) => {
           return p + (urlFields.includes(n.field) ? 120 : n.width)
         }, 0)
-        const containerWidth = containerDom.offsetWidth
+        const containerWidth = containerDom.getBoundingClientRect().width - 1
         if (containerWidth <= totalWidthWithImg) {
           // 图库计算的布局宽度已经大于等于容器宽度，不需要再扩大，但是需要处理非整数宽度值，不然会出现透明细线
           ev.colLeafNodes.reduce((p, n) => {
@@ -325,11 +325,11 @@ export class TableInfo extends S2ChartView<TableSheet> {
             n.x = getStartPosition(n)
           }
         })
+        const lastNode = ev.colLeafNodes[ev.colLeafNodes.length - 1]
         if (totalWidth > containerWidth) {
-          const lastNode = ev.colLeafNodes[ev.colLeafNodes.length - 1]
           lastNode.width = Math.floor(lastNode.width - (totalWidth - containerWidth))
         }
-        ev.colsHierarchy.width = containerWidth - 2
+        ev.colsHierarchy.width = lastNode.x + lastNode.width
       })
     }
     // 空数据时表格样式
