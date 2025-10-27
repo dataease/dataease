ae3f2e1b1b2db5337a3c5e2403bda3afa161010d
wisonic-s <shaochuan.wu@fit2cloud.com>
2025-10-27 15:24:07 +0800

    fix(图表): 修复象限图背景透明度显示错误

--- core/core-frontend/src/views/chart/components/editor/editor-style/components/QuadrantSelector.vue
+++ core/core-frontend/src/views/chart/components/editor/editor-style/components/QuadrantSelector.vue
@@ -79,17 +79,19 @@ const fontSizeList = computed(() => {
   return arr
 })
 
-const fillOpacityList = computed(() => {
-  const arr = []
-  for (let i = 0; i <= 1; i = i + 0.1) {
-    let c = i.toFixed(1)
-    arr.push({
-      name: c,
-      value: i
-    })
-  }
-  return arr
-})
+const fillOpacityList = [
+  { name: '0', value: 0 },
+  { name: '0.1', value: 0.1 },
+  { name: '0.2', value: 0.2 },
+  { name: '0.3', value: 0.3 },
+  { name: '0.4', value: 0.4 },
+  { name: '0.5', value: 0.5 },
+  { name: '0.6', value: 0.6 },
+  { name: '0.7', value: 0.7 },
+  { name: '0.8', value: 0.8 },
+  { name: '0.9', value: 0.9 },
+  { name: '1.0', value: 1 }
+]
 
 const changeStyle = () => {
   emit('onChangeQuadrantForm', state.quadrantForm)
