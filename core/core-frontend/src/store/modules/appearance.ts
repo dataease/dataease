6f42d88eef569daae0b42a0face0bef68b9a3408
王嘉豪 <42510293+ziyujiahao@users.noreply.github.com>
2026-01-08 13:28:47 +0800

    fix: 修复系统字体不支持含有空格的名称问题 (#17761)

--- core/core-frontend/src/store/modules/appearance.ts
+++ core/core-frontend/src/store/modules/appearance.ts
@@ -170,7 +170,7 @@ export const useAppearanceStore = defineStore('appearanceStore', {
       const embeddedStore = useEmbedded()
       const currentFont = this.fontList.find(ele => ele.name === name)
       if (currentFont) {
-        let fontStyleElement = document.querySelector(`#de-custom_font${name}`)
+        let fontStyleElement = document.querySelector(`[id="de-custom_font${name}"]`)
         if (!fontStyleElement) {
           fontStyleElement = document.createElement('style')
           fontStyleElement.setAttribute('id', `de-custom_font${name}`)
