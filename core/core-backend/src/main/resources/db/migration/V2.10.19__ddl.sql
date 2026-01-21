6fe08636529e17f2c2a8ca1fe3f60bfa6002ed07
wangjiahao <1522128093@qq.com>
2026-01-21 18:32:53 +0800

    refactor: 变更flyway编号

--- core/core-backend/src/main/resources/db/migration/V2.10.19__ddl.sql
+++ core/core-backend/src/main/resources/db/migration/V2.10.19__ddl.sql
@@ -1,8 +1,2 @@
 ALTER TABLE `xpack_threshold_info`
     ADD COLUMN `show_field_value` tinyint(1) NOT NULL DEFAULT 0 COMMENT '显示字段值' AFTER `repeat_send`;
-
-ALTER TABLE `snapshot_visualization_outer_params_target_view_info`
-    ADD COLUMN `match_mode` varchar(255) NULL DEFAULT 'self' COMMENT '匹配方式' AFTER `target_ds_id`;
-
-ALTER TABLE `visualization_outer_params_target_view_info`
-    ADD COLUMN `match_mode` varchar(255) NULL DEFAULT 'self' COMMENT '匹配方式' AFTER `target_ds_id`;
