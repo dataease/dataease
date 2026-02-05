3e257a637523d10b7ce3a29097cb632afb24604a
wisonic <shaochuan.wu@fit2cloud.com>
2026-02-05 14:21:40 +0800

    fix(图表): 修复编辑状态下指标卡触发点击事件之后回到编辑页面会跟随鼠标的问题 #17704

--- core/core-frontend/src/custom-component/indicator/DeIndicator.vue
+++ core/core-frontend/src/custom-component/indicator/DeIndicator.vue
@@ -587,7 +587,7 @@ defineExpose({
     ref="indicatorRef"
     :class="{ 'menu-point': showCursor }"
     :style="contentStyle"
-    @mousedown="onPointClick"
+    @click="onPointClick"
   >
     <view-track-bar
       ref="viewTrack"
