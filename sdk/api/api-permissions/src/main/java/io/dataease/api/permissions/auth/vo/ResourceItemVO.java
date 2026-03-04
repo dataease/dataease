c2a22b918c384725a32d118143c632c71fadccbe
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2026-03-04 15:20:46 +0800

    feat: Add playwright v3

---
+++ sdk/api/api-permissions/src/main/java/io/dataease/api/permissions/auth/vo/ResourceItemVO.java
@@ -0,0 +1,14 @@
+package io.dataease.api.permissions.auth.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ResourceItemVO extends ResourceNodeVO {
+    private String account;
+}
