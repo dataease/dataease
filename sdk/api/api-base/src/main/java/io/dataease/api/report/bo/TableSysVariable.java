4a3114b9586ada1f859216dbfa1381629715e9c0
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2026-03-11 14:02:48 +0800

    Revert "perf(X-Pack): 定时报告错误信息"
    This reverts commit f9b08b59199e403ac64c90d353d4b574ca8dee1c.

--- sdk/api/api-base/src/main/java/io/dataease/api/report/bo/TableSysVariable.java
+++ sdk/api/api-base/src/main/java/io/dataease/api/report/bo/TableSysVariable.java
@@ -4,13 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class TableSysVariable implements Serializable {
+public class TableSysVariable {
 
     private Long tableId;
 
