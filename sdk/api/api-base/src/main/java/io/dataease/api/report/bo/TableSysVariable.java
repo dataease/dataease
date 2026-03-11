60d82d78c43b928612e48fa9c71810487ffddab9
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2026-03-11 13:57:46 +0800

    perf(X-Pack): 定时报告错误信息

--- sdk/api/api-base/src/main/java/io/dataease/api/report/bo/TableSysVariable.java
+++ sdk/api/api-base/src/main/java/io/dataease/api/report/bo/TableSysVariable.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class TableSysVariable {
+public class TableSysVariable implements Serializable {
 
     private Long tableId;
 
