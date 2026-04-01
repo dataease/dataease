85521f2a8c896835efd5dbe7cd38037c01d71fdd
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2026-04-01 10:33:00 +0800

    feat(X-Pack): 定时报告-新增推送渠道钉钉群

---
+++ sdk/api/api-base/src/main/java/io/dataease/api/dingtalk/vo/DingtalkChatItem.java
@@ -0,0 +1,13 @@
+package io.dataease.api.dingtalk.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class DingtalkChatItem implements Serializable {
+
+    private String id;
+
+    private String name;
+}
