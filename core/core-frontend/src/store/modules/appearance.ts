625f94445c54d4584a22e2629930a7be01ee62e9
dataeaseShu <tong.shu@fit2cloud.com>
2026-05-11 10:40:32 +0800

    fix: 样式优化

--- core/core-frontend/src/store/modules/appearance.ts
+++ core/core-frontend/src/store/modules/appearance.ts
@@ -292,6 +292,12 @@ export const useAppearanceStore = defineStore('appearanceStore', {
         document.documentElement.style.setProperty('--ed-color-primary', this.customColor)
         document.documentElement.style.setProperty('--van-blue', this.customColor)
         document.documentElement.style.setProperty(
+          '--ed-color-primary_b50',
+          colorFunctions
+            .mix(new colorTree('ffffff'), new colorTree(this.customColor.substr(1)), { value: 90 })
+            .toRGB()
+        )
+        document.documentElement.style.setProperty(
           '--ed-color-primary-light-5',
           mixColor('ffffff', this.customColor, 40)
         )
