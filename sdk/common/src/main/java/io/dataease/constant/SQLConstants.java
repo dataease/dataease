dc93325eac45ce52376616f4eb6a8cc293c1a162
ulleo <ulleo.wang@gmail.com>
2026-05-13 16:17:26 +0800

    fix(X-Pack): 修复[数据填报]时间格式字段查询

--- sdk/common/src/main/java/io/dataease/constant/SQLConstants.java
+++ sdk/common/src/main/java/io/dataease/constant/SQLConstants.java
@@ -64,6 +64,9 @@ public class SQLConstants {
     public static final String CAST = "CAST(%s AS %s)";
 
     public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DEFAULT_YEAR_FORMAT = "yyyy";
+    public static final String DEFAULT_YEAR_MONTH_FORMAT = "yyyy-MM";
+    public static final String DEFAULT_YEAR_MONTH_DAY_FORMAT = "yyyy-MM-dd";
 
     public static final String DEFAULT_INT_FORMAT = "DECIMAL(19,0)";
 
