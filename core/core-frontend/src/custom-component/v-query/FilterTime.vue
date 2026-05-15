1ed1ed508535beb3977cb25f0da54e2acb8bbd48
Carson <chuncheng.carson@gmail.com>
2026-05-16 07:02:22 +0800

    feat(query): add year to last month end range

--- core/core-frontend/src/custom-component/v-query/FilterTime.vue
+++ core/core-frontend/src/custom-component/v-query/FilterTime.vue
@@ -217,6 +217,10 @@ const relativeToCurrentListRange = computed(() => {
         {
           label: t('common.to_this_month'),
           value: 'YearToThisMonth'
+        },
+        {
+          label: t('v_query.year_to_last_month_end'),
+          value: 'YearToLastMonthEnd'
         }
       ]
       break
@@ -242,6 +246,10 @@ const relativeToCurrentListRange = computed(() => {
         {
           label: t('v_query.year_to_date'),
           value: 'yearBeginning'
+        },
+        {
+          label: t('v_query.year_to_last_month_end'),
+          value: 'YearToLastMonthEnd'
         },
         {
           label: t('common.month_to_yesterday'),
