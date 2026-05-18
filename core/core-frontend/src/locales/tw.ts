362e3bbec97d37484f7dafd891cf610c040f045d
wangjiahao <1522128093@qq.com>
2026-05-18 18:20:14 +0800

    refactor(仪表板、数据大屏): 表格无数据的提示，支持自定义字体大小和颜色 #18296

--- core/core-frontend/src/locales/tw.ts
+++ core/core-frontend/src/locales/tw.ts
@@ -1424,6 +1424,7 @@ export default {
     table_col_freeze_tip: '凍結前n 列',
     table_row_freeze_tip: '凍結前n 行',
     table_freeze: '凍結',
+    table_empty_font_color: '表格空值顏色',
     stripe: '斑馬紋',
     start_angle: '起始角度',
     end_angle: '結束角度',
