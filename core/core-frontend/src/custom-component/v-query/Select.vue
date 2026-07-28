38265cfc96b1047f5121237ae4d5da2452f779aa
dataeaseShu <tong.shu@fit2cloud.com>
2026-07-28 10:07:45 +0800

    fix(过滤组件): 级连过滤组件选择第一个后，图表内容被刷新了

--- core/core-frontend/src/custom-component/v-query/Select.vue
+++ core/core-frontend/src/custom-component/v-query/Select.vue
@@ -492,7 +492,7 @@ const setDefaultValueFirstItem = () => {
     setCascadeValueBack(config.value.mapValue)
     emitCascade()
     nextTick(() => {
-      isConfirmSearch(config.value.id, true)
+      isConfirmSearch(config.value.id, !isFromFromCascade)
     })
     return
   }
@@ -785,6 +785,7 @@ const getOptionFromCascade = () => {
     clearCascadeSelectValue()
     emitCascade()
   }
+  isFromFromCascade = true
   isFromRemote.value = true
   debounceOptions(1)
 }
@@ -797,7 +798,7 @@ const hideClick = id => {
   const vnode = single.value || mult.value
   vnode?.handleClickOutside?.()
 }
-
+let isFromFromCascade = false
 onBeforeMount(() => {
   init()
   useEmitt({
