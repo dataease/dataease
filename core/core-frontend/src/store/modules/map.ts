866102f3317190f0d6b6ba77017a3bb026e6c31d
jianneng-fit2cloud <jianneng.pan@fit2cloud.com>
2026-07-29 13:15:33 +0800

    feat(地图设置): 在线地图支持瓦片地图充当 DataEase 在线地图的底图进行使用

--- core/core-frontend/src/store/modules/map.ts
+++ core/core-frontend/src/store/modules/map.ts
@@ -1,29 +1,29 @@
 import { defineStore } from 'pinia'
 import { store } from '@/store'
 import { FeatureCollection } from '@antv/l7plot/dist/esm/plots/choropleth/types'
+import {
+  DEFAULT_ONLINE_MAP_CONFIG,
+  normalizeOnlineMapConfig,
+  type OnlineMapConfig
+} from '@/utils/onlineMap'
 interface MapStore {
   mapCache: Record<string, FeatureCollection>
-  mapKey: {
-    key: string
-    securityCode: string
-    mapType: string
-  }
+  mapKey: OnlineMapConfig
+  mapKeyLoaded: boolean
 }
 export const useMapStore = defineStore('map', {
   state: (): MapStore => ({
     mapCache: {},
-    mapKey: {
-      key: '',
-      securityCode: '',
-      mapType: ''
-    }
+    mapKey: { ...DEFAULT_ONLINE_MAP_CONFIG },
+    mapKeyLoaded: false
   }),
   actions: {
     setMap({ id, geoJson }) {
       this.mapCache[id] = geoJson
     },
     setKey(key) {
-      this.mapKey = key
+      this.mapKey = normalizeOnlineMapConfig(key)
+      this.mapKeyLoaded = true
     }
   }
 })
