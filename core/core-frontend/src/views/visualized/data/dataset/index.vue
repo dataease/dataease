317c3ded32c625a2215abc46c7dfe6e27f34181a
dataeaseShu <tong.shu@fit2cloud.com>
2026-08-10 18:36:36 +0800

    fix(数据源): 点击目录隐藏设置，随后上下滚动，当目录即将超出显示视窗弹窗发生偏移，偏移至左上角

--- core/core-frontend/src/views/visualized/data/dataset/index.vue
+++ core/core-frontend/src/views/visualized/data/dataset/index.vue
@@ -1450,13 +1450,17 @@ const proxyAllowDrop = throttle((arg1, arg2) => {
   padding-right: 4px;
 
   .label-tooltip {
-    width: 100%;
+    width: calc(100% - 40px);
     margin-left: 8.75px;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 18px;
   }
 
   .icon-more {
     margin-left: auto;
-    display: none;
+    opacity: 0;
   }
 
   &:hover {
@@ -1465,7 +1469,7 @@ const proxyAllowDrop = throttle((arg1, arg2) => {
     }
 
     .icon-more {
-      display: inline-flex;
+      opacity: 1;
     }
   }
 }
