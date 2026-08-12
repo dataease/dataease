2ad8fa853e561907a22857310df0002d3a74a180
junjun <junjie.xia@fit2cloud.com>
2026-08-12 10:32:50 +0800

    fix: 修复血缘分析新页面打开无权限的问题

--- core/core-backend/src/main/java/io/dataease/resource/ResourceService.java
+++ core/core-backend/src/main/java/io/dataease/resource/ResourceService.java
@@ -2,6 +2,7 @@ package io.dataease.resource;
 
 import io.dataease.api.permissions.auth.dto.BusiPerCheckDTO;
 import io.dataease.constant.AuthEnum;
+import io.dataease.constant.BusiResourceEnum;
 import io.dataease.system.manage.CorePermissionManage;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Component;
@@ -14,9 +15,10 @@ public class ResourceService {
     @Resource
     private CorePermissionManage corePermissionManage;
 
-    public boolean checkPermission(Long id) {
+    public boolean checkPermission(Long id, String type) {
         BusiPerCheckDTO dto = new BusiPerCheckDTO();
         dto.setId(id);
+        dto.setBusiEnum(getResourceTypeEnum(type));
         dto.setAuthEnum(AuthEnum.READ);
         boolean b;
         try {
@@ -25,5 +27,16 @@ public class ResourceService {
             b = false;
         }
         return b;
+    }
+
+    private BusiResourceEnum getResourceTypeEnum(String type) {
+        String t = type.toLowerCase();
+        return switch (t) {
+            case "datasource" -> BusiResourceEnum.DATASOURCE;
+            case "dataset" -> BusiResourceEnum.DATASET;
+            case "dashboard" -> BusiResourceEnum.PANEL;
+            case "datav" -> BusiResourceEnum.SCREEN;
+            default -> null;
+        };
     }
 }
