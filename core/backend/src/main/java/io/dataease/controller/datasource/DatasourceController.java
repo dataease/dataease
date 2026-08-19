0f4997a062bd27b3450204d214d6ddd0abb36d70
tjlygdx <tjlygdx@163.com>
2026-08-19 19:39:53 +0800

    fix: 【漏洞】垂直越权

--- core/backend/src/main/java/io/dataease/controller/datasource/DatasourceController.java
+++ core/backend/src/main/java/io/dataease/controller/datasource/DatasourceController.java
@@ -122,7 +122,6 @@ public class DatasourceController {
     @DePermission(type = DePermissionType.DATASOURCE, value = "id")
     @ApiOperation("查询数据源详情")
     @PostMapping("/get/{id}")
-    @DePermission(type = DePermissionType.DATASOURCE, value = "id", level = ResourceAuthLevel.DATASOURCE_LEVEL_USE)
     public DatasourceDTO getDatasource(@PathVariable String id) throws Exception {
         return datasourceService.getDataSourceDetails(id);
     }
