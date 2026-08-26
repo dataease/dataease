4edaf2705ddf5311b80e9b745c2a58361c8c1528
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2026-08-26 09:57:57 +0800

    fix(X-Pack): 邮件配置-增加发件人字段

--- core/core-frontend/src/locales/en.ts
+++ core/core-frontend/src/locales/en.ts
@@ -3978,6 +3978,7 @@ export default {
     port: 'SMTP port',
     account: 'SMTP account',
     pwd: 'SMTP password',
+    from: 'Sender address',
     reci: 'Test recipient',
     ssl: 'SSL',
     tsl: 'TSL'
