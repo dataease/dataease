5aceb797c7e779a6e69a2d338c2f3b4b6ba0a862
dataeaseShu <tong.shu@fit2cloud.com>
2026-08-28 14:13:30 +0800

    fix(系统设置): 样式优化

--- core/core-frontend/src/views/system/parameter/index.vue
+++ core/core-frontend/src/views/system/parameter/index.vue
@@ -3,7 +3,10 @@
     <el-tab-pane v-for="item in tabArray" :key="item.name" :label="item.label" :name="item.name" />
   </el-tabs>
   <div class="sys-setting-p">
-    <div class="container-sys_param" :class="{ 'setting-max-h': activeName === 'map' }">
+    <div
+      class="container-sys_param"
+      :class="{ 'setting-max-h': activeName === 'map', 'engine-bg': activeName === 'engine' }"
+    >
       <map-setting v-if="activeName === 'map'" />
       <basic-info v-if="activeName === 'basic'" />
       <engine-info v-if="activeName === 'engine'" />
@@ -61,6 +64,9 @@ const addTable = tab => {
   height: auto;
   overflow-y: auto;
   border-radius: 12px;
+  &.engine-bg {
+    background: #ffffff;
+  }
 }
 .setting-max-h {
   height: 100% !important;
