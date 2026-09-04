df9437d1cde9eaea2d87dd70e6f90def0ebe2d2c
dataeaseShu <tong.shu@fit2cloud.com>
2026-09-04 11:01:03 +0800

    fix(数据大屏): 日期字段-固定值使用过滤器过滤，鼠标hover值填写处会显示气泡图案

--- core/core-frontend/src/views/menu/spreadsheet/components/config-panel/filter/auth-tree-chart/FilterFiled.vue
+++ core/core-frontend/src/views/menu/spreadsheet/components/config-panel/filter/auth-tree-chart/FilterFiled.vue
@@ -615,6 +615,7 @@ const emits = defineEmits(['update:item', 'del'])
               effect="light"
               :content="item.timeValue"
               placement="top"
+              :disabled="!item.timeValue"
               ><el-input
                 @click="handleClick"
                 readonly
