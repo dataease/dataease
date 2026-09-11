19ff1b30dadbb65eefaa8649e6fd3d1ea8590cd1
junjun <junjie.xia@fit2cloud.com>
2026-09-11 10:39:34 +0800

    feat: 增加GaussDB数据源

--- core/core-frontend/src/views/visualized/data/datasource/form/EditorDetail.vue
+++ core/core-frontend/src/views/visualized/data/datasource/form/EditorDetail.vue
@@ -377,7 +377,11 @@ const setRules = () => {
     'configuration.sshPassword': [{ validator: validateSshPassword, trigger: 'blur' }],
     'configuration.sshKey': [{ validator: validateSshkey, trigger: 'blur' }]
   }
-  if (['oracle', 'sqlServer', 'pg', 'kingbase', 'gaussdb', 'redshift', 'db2'].includes(form.value.type)) {
+  if (
+    ['oracle', 'sqlServer', 'pg', 'kingbase', 'gaussdb', 'redshift', 'db2'].includes(
+      form.value.type
+    )
+  ) {
     configRules['configuration.schema'] = [
       {
         required: true,
@@ -1276,7 +1280,11 @@ defineExpose({
             </el-radio>
           </el-form-item>
           <el-form-item
-            v-if="['oracle', 'sqlServer', 'pg', 'kingbase', 'gaussdb', 'redshift', 'db2'].includes(form.type)"
+            v-if="
+              ['oracle', 'sqlServer', 'pg', 'kingbase', 'gaussdb', 'redshift', 'db2'].includes(
+                form.type
+              )
+            "
             class="schema-label"
             :prop="showSchema ? '' : 'configuration.schema'"
           >
