75aabdbc2383bba83f4d1a11a12181e23c860aa6
wisonic-s <shaochuan.wu@fit2cloud.com>
2026-09-15 16:19:13 +0800

    fix(图表): 表格分组导出异常

--- sdk/extensions/extensions-view/src/main/java/io/dataease/extensions/view/dto/TableHeader.java
+++ sdk/extensions/extensions-view/src/main/java/io/dataease/extensions/view/dto/TableHeader.java
@@ -24,6 +24,8 @@ public class TableHeader {
     public class ColumnInfo {
         @Getter
         private String key;
+        private String field;
+        private String title;
         private List<ColumnInfo> children = new ArrayList<>();
         private Integer width;
     }
