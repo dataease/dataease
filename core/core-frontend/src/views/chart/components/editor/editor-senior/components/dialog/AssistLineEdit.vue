bbcda126496f343a9d4830990a6af26511ca8777
jianneng-fit2cloud <jianneng.pan@fit2cloud.com>
2026-09-21 14:14:42 +0800

    style(图表): 优化辅助线下拉选项错位问题并统一下拉选项样式

--- core/core-frontend/src/views/chart/components/editor/editor-senior/components/dialog/AssistLineEdit.vue
+++ core/core-frontend/src/views/chart/components/editor/editor-senior/components/dialog/AssistLineEdit.vue
@@ -231,7 +231,7 @@ onMounted(() => {
           <el-select
             v-model="item.fieldId"
             class="select-item"
-            fit-input-width
+            :fit-input-width="false"
             :placeholder="t('chart.field')"
             @change="changeAssistLineField(item)"
           >
@@ -244,17 +244,15 @@ onMounted(() => {
               :label="quota.name"
               :value="quota.id"
             >
-              <span style="flex-shrink: 0">
-                <el-icon>
-                  <Icon :className="`field-icon-${fieldType[item.deType]}`"
-                    ><component
-                      class="svg-icon"
-                      :class="`field-icon-${fieldType[item.deType]}`"
-                      :is="iconFieldMap[fieldType[item.deType]]"
-                    ></component
-                  ></Icon>
-                </el-icon>
-              </span>
+              <el-icon style="flex-shrink: 0">
+                <Icon :className="`field-icon-${fieldType[quota.deType]}`"
+                  ><component
+                    class="svg-icon"
+                    :class="`field-icon-${fieldType[quota.deType]}`"
+                    :is="iconFieldMap[fieldType[quota.deType]]"
+                  ></component
+                ></Icon>
+              </el-icon>
               <span class="field-name ellipsis" :title="quota.name">
                 {{ quota.name }}
               </span>
@@ -292,7 +290,13 @@ onMounted(() => {
           </el-tooltip>
         </el-col>
         <el-col :span="useQuotaExt ? 2 : 4">
-          <el-select v-model="item.lineType" class="select-item" @change="changeAssistLine">
+          <!-- 下拉菜单按内容扩展，不限制为选择框宽度 -->
+          <el-select
+            v-model="item.lineType"
+            class="select-item"
+            :fit-input-width="false"
+            @change="changeAssistLine"
+          >
             <el-option
               v-for="opt in state.lineOptions"
               :key="opt.value"
@@ -377,21 +381,20 @@ span {
 }
 
 .ed-select-dropdown__item {
-  padding: 0 20px;
+  padding: 0 36px 0 12px;
   font-size: 14px;
 
   &.field-option {
     display: flex;
     align-items: center;
-    // 为右侧选中图标预留空间，避免与字段名重叠。
-    padding-right: 36px;
+    gap: 8px;
   }
 }
 
 .field-name {
   flex: 1;
   min-width: 0;
-  color: #8492a6;
-  font-size: 12px;
+  color: inherit;
+  font-size: inherit;
 }
 </style>
