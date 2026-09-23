9c7a40c9efc58394aac61bf054059eeafca905f0
wangjiahao <1522128093@qq.com>
2026-09-23 13:49:11 +0800

    refactor: 隐藏不使用功能

--- core/core-frontend/src/components/data-visualization/DvToolbar.vue
+++ core/core-frontend/src/components/data-visualization/DvToolbar.vue
@@ -453,7 +453,7 @@ const fullScreenPreview = () => {
           </component-group>
           <component-group
             is-label
-            :base-width="315"
+            :base-width="215"
             :icon-name="dvMoreCom"
             :title="t('visualization.more')"
           >
