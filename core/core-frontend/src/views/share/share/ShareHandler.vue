b27102e4cb483c687e0d3936158f703247a207ac
xuwei-fit2cloud <wei@fit2cloud.com>
2026-09-23 22:59:23 +0800

    fix(分享): 修复社区版公共链接状态与访客权限控制

--- core/core-frontend/src/views/share/share/ShareHandler.vue
+++ core/core-frontend/src/views/share/share/ShareHandler.vue
@@ -31,7 +31,12 @@
     <div ref="shareDialogRef" class="share-dialog-container">
       <div class="copy-link">
         <div class="open-share flex-align-center">
-          <el-switch size="small" v-model="shareEnable" @change="enableSwitcher" />
+          <el-switch
+            size="small"
+            v-model="shareEnable"
+            :disabled="!shareInfoReady"
+            @change="enableSwitcher"
+          />
           {{ shareTips }}
         </div>
         <div v-if="shareEnable" class="custom-link-line">
@@ -189,7 +194,7 @@
       </div>
     </div>
     <ShareVisitorPermissions
-      v-if="shareEnable"
+      v-if="shareEnable && !appearanceStore.getCommunity"
       v-model="visitorChoices"
       :allowed="state.detailInfo.allowedVisitorPermissions ?? 7"
     />
@@ -228,6 +233,7 @@
 
 <script lang="ts" setup>
 import ShareVisitorPermissions from './ShareVisitorPermissions.vue'
+import { useAppearanceStoreWithOut } from '@/store/modules/appearance'
 import dvShare from '@/assets/svg/dv-share.svg'
 import icon_shareLabel_outlined from '@/assets/svg/icon_share-label_outlined.svg'
 import deCopy from '@/assets/svg/de-copy.svg'
@@ -260,6 +266,8 @@ const props = defineProps({
   weight: propTypes.number.def(0),
   isButton: propTypes.bool.def(false)
 })
+const appearanceStore = useAppearanceStoreWithOut()
+const shareInfoReady = ref(false)
 const visitorChoices = ref<number[]>([])
 const originUuid = ref('')
 const customPwdRef = ref()
@@ -376,13 +384,15 @@ const copyInfo = async () => {
         }
       }
       savingPermissions.value = true
-      await request.post({
-        url: '/share/visitorPermissions',
-        data: {
-          resourceId: props.resourceId,
-          visitorPermissions: visitorChoices.value.reduce((a, b) => a | b, 0)
-        }
-      })
+      if (!appearanceStore.getCommunity) {
+        await request.post({
+          url: '/share/visitorPermissions',
+          data: {
+            resourceId: props.resourceId,
+            visitorPermissions: visitorChoices.value.reduce((a, b) => a | b, 0)
+          }
+        })
+      }
       formatLinkAddr()
       let info = linkAddr.value
       if (passwdEnable.value) {
@@ -429,6 +439,7 @@ const share = () => {
 }
 
 const loadShareInfo = (cb, resetPermissions = false) => {
+  shareInfoReady.value = false
   showLoading()
   const resourceId = props.resourceId
   const url = `/share/detail/${resourceId}`
@@ -452,6 +463,10 @@ const loadShareInfo = (cb, resetPermissions = false) => {
         originUuid.value = res.data.uuid
       }
       setPageInfo()
+      shareInfoReady.value = true
+    })
+    .catch(() => {
+      // The request interceptor reports the error; do not allow toggling an unknown state.
     })
     .finally(() => {
       closeLoading()
@@ -460,8 +475,8 @@ const loadShareInfo = (cb, resetPermissions = false) => {
 }
 
 const setPageInfo = () => {
-  if (state.detailInfo.id && state.detailInfo.uuid) {
-    shareEnable.value = true
+  shareEnable.value = !!(state.detailInfo.id && state.detailInfo.uuid)
+  if (shareEnable.value) {
     formatLinkAddr()
   }
   passwdEnable.value = !!state.detailInfo.pwd
@@ -469,12 +484,17 @@ const setPageInfo = () => {
 }
 
 const enableSwitcher = () => {
+  if (!shareInfoReady.value) return
+  shareInfoReady.value = false
   showLoading()
   const resourceId = props.resourceId
   const url = `/share/switcher/${resourceId}`
   request
     .post({ url })
     .then(() => {
+      loadShareInfo(null)
+    })
+    .catch(() => {
       loadShareInfo(null)
     })
     .finally(() => {
