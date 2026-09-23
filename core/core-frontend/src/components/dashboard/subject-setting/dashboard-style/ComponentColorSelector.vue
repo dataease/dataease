27fb72c4bbe67e78adeae2a10586a10f8cfec011
wangjiahao <1522128093@qq.com>
2026-09-23 14:45:46 +0800

    refactor: 修改大屏统一字体最小限制

--- core/core-frontend/src/components/dashboard/subject-setting/dashboard-style/ComponentColorSelector.vue
+++ core/core-frontend/src/components/dashboard/subject-setting/dashboard-style/ComponentColorSelector.vue
@@ -386,7 +386,7 @@ const colorFormRef = ref(null)
 
 const fontSizeList = computed(() => {
   const arr = []
-  for (let i = 6; i <= 40; i = i + 2) {
+  for (let i = 10; i <= 40; i = i + 2) {
     arr.push({
       name: i + '',
       value: i
