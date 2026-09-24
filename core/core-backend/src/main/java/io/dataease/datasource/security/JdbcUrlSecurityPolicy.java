90b75a5cbf6899966e7b825b84b5580141f0ec67
tjlygdx <tjlygdx@163.com>
2026-09-24 10:17:41 +0800

    fix:  【漏洞】DB2 JDBC validation accepts CORBA corbaloc URLs

--- core/core-backend/src/main/java/io/dataease/datasource/security/JdbcUrlSecurityPolicy.java
+++ core/core-backend/src/main/java/io/dataease/datasource/security/JdbcUrlSecurityPolicy.java
@@ -53,6 +53,7 @@ public final class JdbcUrlSecurityPolicy {
             "ftp:",
             "nis:",
             "corba:",
+            "corbaloc",
             "corbaname",
             "iiop",
             "iiopname",
